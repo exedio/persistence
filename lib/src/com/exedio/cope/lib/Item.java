d2fa39bcb7e3653df45102cad04ff7b53efb4f8c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-01-12 16:11:21 +0000

    forgot to check in
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@68 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ lib/src/com/exedio/cope/lib/Item.java
@@ -0,0 +1,20 @@
+
+package persistence;
+
+public class Item
+{
+	
+	public Item()
+	{}
+	
+	protected final Object getAttribute(final Attribute attribute)
+	{
+		return null;
+	}
+	
+	protected final void setAttribute(final Attribute attribute, final Object value)
+	throws UniqueViolationException
+	{
+	}
+	
+}
