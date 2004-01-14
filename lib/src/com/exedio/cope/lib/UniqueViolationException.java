46006785b15141d2566f8c8c6c4c160e688b5d46
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-01-14 14:37:33 +0000

    fully qualified UniqueViolationException
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@120 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/UniqueViolationException.java
+++ lib/src/com/exedio/cope/lib/UniqueViolationException.java
@@ -2,13 +2,35 @@
 package persistence;
 
 /**
- * Is thrown, when a persistent modifivation violates a unique-constraint.
+ * Is thrown, when a persistent modification violates a unique-constraint.
  */
 public class UniqueViolationException extends ConstraintViolationException
 {
 	
-	public UniqueViolationException()
+	/**
+	 * Creates a new UniqueViolationException with the neccessary information about the collision.
+	 * @param item initializes, what is returned by {@link #getItem()}.
+	 * @param constraint initializes, what is returned by {@link #getConstraint()}.
+	 */
+	public UniqueViolationException(final Item item, final UniqueConstraint constraint)
+	{}
+	
+	/**
+	 * Returns the item that was tried to be modified.
+	 * Is null, if the item has not yet been created,
+	 * e.g. the collision occured in the constructor.
+	 */
+	public final Item getItem()
 	{
+		return null;
+	}
+
+	/**
+	 * Return the violated constraint.
+	 */
+	public UniqueConstraint getConstraint()
+	{
+		return null;
 	}
 	
 }
