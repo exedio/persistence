562176565302229f2ba051adeffbf245a11c1ca3
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-02-25 16:45:46 +0000

    additional message and printStackTrace prints stack trace of causing exception
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@255 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/SystemException.java
+++ lib/src/com/exedio/cope/lib/SystemException.java
@@ -1,16 +1,51 @@
 
 package com.exedio.cope.lib;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 /**
  * Is thrown, when a fatal unspecified error occurs.
  */
 public class SystemException extends RuntimeException
 {
 	private final Exception cause;
+	private final String message;
 	
 	public SystemException(final Exception cause)
 	{
 		this.cause = cause;
+		this.message = null;
 	}
 	
+	public SystemException(final Exception cause, final String message)
+	{
+		this.cause = cause;
+		this.message = message;
+	}
+	
+	public void printStackTrace()
+	{
+		super.printStackTrace();
+		if(message!=null)
+			System.out.println(message);
+		cause.printStackTrace();
+	}
+
+	public void printStackTrace(final PrintStream s)
+	{
+		super.printStackTrace(s);
+		if(message!=null)
+			s.println(message);
+		cause.printStackTrace(s);
+	}
+
+	public void printStackTrace(final PrintWriter s)
+	{
+		super.printStackTrace(s);
+		if(message!=null)
+			s.println(message);
+		cause.printStackTrace(s);
+	}
+
 }
