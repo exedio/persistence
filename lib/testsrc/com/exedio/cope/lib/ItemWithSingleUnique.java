180e91407f0ece4e8638a62528d51c3783c85b24
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-02-27 14:35:48 +0000

    introduce unit tests for instrumentor and runtime library
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@272 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ lib/testsrc/com/exedio/cope/lib/ItemWithSingleUnique.java
@@ -0,0 +1,98 @@
+
+package com.exedio.cope.lib;
+
+import com.exedio.cope.lib.Attribute;
+import com.exedio.cope.lib.IntegerAttribute;
+import com.exedio.cope.lib.Item;
+import com.exedio.cope.lib.ItemAttribute;
+import com.exedio.cope.lib.MediaAttribute;
+import com.exedio.cope.lib.StringAttribute;
+
+/**
+ * An item having a unique attribute.
+ * @persistent
+ */
+public class ItemWithSingleUnique extends Item
+{
+	/**
+	 * An attribute that is unique.
+	 * @persistent
+	 * @unique
+	 */
+	public static final StringAttribute uniqueString = new StringAttribute();
+
+/**
+
+	 **
+	 * Constructs a new ItemWithSingleUnique with all the attributes initially needed.
+	 * @generated
+	 *
+ */public ItemWithSingleUnique()
+	{
+		super(new com.exedio.cope.lib.AttributeValue[]{
+		});
+	}/**
+
+	 **
+	 * Returns the value of the persistent attribute {@link #uniqueString}.
+	 * @generated
+	 *
+ */public final String getUniqueString()
+	{
+		return (String)getAttribute(this.uniqueString);
+	}/**
+
+	 **
+	 * Sets a new value for the persistent attribute {@link #uniqueString}.
+	 * @generated
+	 *
+ */public final void setUniqueString(final String uniqueString)
+			throws
+				com.exedio.cope.lib.UniqueViolationException
+	{
+		try
+		{
+			setAttribute(this.uniqueString,uniqueString);
+		}
+		catch(com.exedio.cope.lib.NotNullViolationException e)
+		{
+			throw new com.exedio.cope.lib.SystemException(e);
+		}
+		catch(com.exedio.cope.lib.ReadOnlyViolationException e)
+		{
+			throw new com.exedio.cope.lib.SystemException(e);
+		}
+	}/**
+
+	 **
+	 * Finds a itemWithSingleUnique by it's unique attributes
+	 * @param searchedUniqueString shall be equal to attribute {@link #uniqueString}.
+	 * @generated
+	 *
+ */public static final ItemWithSingleUnique findByUniqueString(final String searchedUniqueString)
+	{
+		return (ItemWithSingleUnique)searchUnique(TYPE,equal(uniqueString,searchedUniqueString));
+	}/**
+
+	 **
+	 * The persistent type information for itemWithSingleUnique.
+	 * @generated
+	 *
+ */public static final com.exedio.cope.lib.Type TYPE = 
+		new com.exedio.cope.lib.Type(
+			ItemWithSingleUnique.class,
+			new com.exedio.cope.lib.Attribute[]{
+				uniqueString,
+			},
+			new com.exedio.cope.lib.UniqueConstraint[]{
+				new com.exedio.cope.lib.UniqueConstraint(uniqueString),
+			},
+			new Runnable()
+			{
+				public void run()
+				{
+					uniqueString.initialize("uniqueString",false,false);
+				}
+			}
+		)
+;}
