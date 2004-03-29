5d4e26a2c1e2617c0a8614bb626c3a031a2cc1d1
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-03-29 17:15:57 +0000

    some more attributes
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@452 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- instrument/testsrc/com/exedio/cope/instrument/ExampleTest.java
+++ instrument/testsrc/com/exedio/cope/instrument/ExampleTest.java
@@ -23,18 +23,38 @@ public class ExampleTest extends InjectorTest
 	{
 		assertText("/*\nSome initial test comment. \n*/\n\npackage// hallo\n  com.exedio.cope.instrument");
 		assertPackage("com.exedio.cope.instrument");
+
 		assertText(";\n\nimport java.util.*");
 		assertImport("java.util.*");
+
 		assertText(";\nimport java.text.Format");
 		assertImport("java.text.Format");
 		assertText(";\n\n");
+
 		assertFileDocComment("/**\n	Represents an attribute or association partner of a class.\n	Note: type==Model.AMIGOUS means, the attribute cannot be used in OCL due to attribute ambiguities.\n	See OCL spec 5.4.1. for details.\n*/");
 		assertText("\npublic abstract class Example");
 		assertClass("Example");
 		assertText(" implements Runnable\n{\n  ");
+
 		assertAttributeHeader("name");
 		assertText("private String name;");
 		assertAttribute("name", null);
+		assertText("\n  ");
+
+		assertAttributeHeader("type");
+		assertText("private Integer type=new Integer(5);");
+		assertAttribute("type", null);
+		assertText("\n  ");
+
+		assertAttributeHeader("qualifiers");
+		assertText("private volatile Integer[] qualifiers;");
+		assertAttribute("qualifiers", null);
+		assertText("\n  ");
+
+		assertAttributeHeader("hallo");
+		assertText("String hallo=\"hallo\";");
+		assertAttribute("hallo", null);
+		assertText("\n  \n  \n  ");
 	}
 
 }
