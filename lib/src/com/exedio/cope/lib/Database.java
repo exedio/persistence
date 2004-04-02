7d16eb5fa3626e0b6a7c598c3de7700dd4f34732
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-04-02 14:57:14 +0000

    cleanup import
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@525 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/Database.java
+++ lib/src/com/exedio/cope/lib/Database.java
@@ -3,7 +3,6 @@ package com.exedio.cope.lib;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
