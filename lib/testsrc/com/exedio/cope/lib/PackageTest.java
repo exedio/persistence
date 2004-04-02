e473da49ee178f2085d361658703c2ef79a91564
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-04-02 09:47:14 +0000

    use a PackageTest for lib.test
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@505 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ lib/testsrc/com/exedio/cope/lib/PackageTest.java
@@ -0,0 +1,23 @@
+
+package com.exedio.cope.lib;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class PackageTest extends TestCase
+{
+
+	public PackageTest(String name)
+	{
+		super(name);
+	}
+
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite();
+		suite.addTest(new TestSuite(LibTest.class));
+		return suite;
+	}
+
+}
