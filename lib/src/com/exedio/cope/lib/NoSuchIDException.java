7a136c1456561a14fe55157789a80181fc5db78f
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-04-05 17:19:48 +0000

    apidoc
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@545 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/NoSuchIDException.java
+++ lib/src/com/exedio/cope/lib/NoSuchIDException.java
@@ -1,6 +1,13 @@
 
 package com.exedio.cope.lib;
 
+/**
+ * Signals, that an attempt to find an item by it's ID has been failed,
+ * because there is no item with such an ID.
+ *
+ * This exception will be thrown by {@link Search#findByID(String) Search.findByID},
+ * if there is no item with the given ID.
+ */
 public class NoSuchIDException extends Exception
 {
 	NoSuchIDException(final String id, final String detail)
