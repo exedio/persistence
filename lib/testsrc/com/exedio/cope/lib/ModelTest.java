12f9a2c06e49b34c47e89de6768f77de4556121e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-04-06 12:56:39 +0000

    move model tests for someEnumeration into separate test
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@550 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ lib/testsrc/com/exedio/cope/lib/ModelTest.java
@@ -0,0 +1,62 @@
+
+package com.exedio.cope.lib;
+
+/**
+ * Tests the model itself, without creating/using any persistent data.
+ */
+public class ModelTest extends AbstractLibTest
+{
+	public void testSomeEnumeration()
+	{
+		final ItemWithManyAttributes item = null;
+
+		assertEquals(item.TYPE, item.someEnumeration.getType());
+		assertEquals(
+			list(
+				ItemWithManyAttributes.SomeEnumeration.enumValue1,
+				ItemWithManyAttributes.SomeEnumeration.enumValue2,
+				ItemWithManyAttributes.SomeEnumeration.enumValue3),
+			item.someEnumeration.getValues());
+
+		assertEquals(
+			ItemWithManyAttributes.SomeEnumeration.enumValue1,
+			item.someEnumeration.getValue(
+				ItemWithManyAttributes.SomeEnumeration.enumValue1NUM));
+		assertEquals(
+			ItemWithManyAttributes.SomeEnumeration.enumValue2,
+			item.someEnumeration.getValue(
+				ItemWithManyAttributes.SomeEnumeration.enumValue2NUM));
+		assertEquals(
+			ItemWithManyAttributes.SomeEnumeration.enumValue3,
+			item.someEnumeration.getValue(
+				ItemWithManyAttributes.SomeEnumeration.enumValue3NUM));
+
+		assertTrue(!
+			ItemWithManyAttributes.SomeEnumeration.enumValue1.equals(
+			ItemWithManyAttributes.SomeEnumeration.enumValue2));
+		assertTrue(!
+			ItemWithManyAttributes.SomeEnumeration.enumValue2.equals(
+			ItemWithManyAttributes.SomeEnumeration.enumValue3));
+		assertTrue(!
+			ItemWithManyAttributes.SomeEnumeration.enumValue3.equals(
+			ItemWithManyAttributes.SomeEnumeration.enumValue1));
+
+		ItemWithManyAttributes.SomeEnumeration someEnumeration = ItemWithManyAttributes.SomeEnumeration.enumValue1;
+		switch (someEnumeration.number)
+		{
+			case ItemWithManyAttributes.SomeEnumeration.enumValue1NUM :
+				someEnumeration = ItemWithManyAttributes.SomeEnumeration.enumValue2;
+				break;
+			case ItemWithManyAttributes.SomeEnumeration.enumValue2NUM :
+				someEnumeration = ItemWithManyAttributes.SomeEnumeration.enumValue3;
+				break;
+			case ItemWithManyAttributes.SomeEnumeration.enumValue3NUM :
+				someEnumeration = ItemWithManyAttributes.SomeEnumeration.enumValue1;
+				break;
+			default :
+				throw new RuntimeException("Ooooops");
+		}
+		assertEquals(someEnumeration, ItemWithManyAttributes.SomeEnumeration.enumValue2);
+	}
+
+}
