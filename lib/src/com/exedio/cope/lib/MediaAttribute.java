f2e7b1b480c44b22ae710aaee9abbd99fc2f5a01
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-04-26 09:50:17 +0000

    add TODO
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@695 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/MediaAttribute.java
+++ lib/src/com/exedio/cope/lib/MediaAttribute.java
@@ -16,7 +16,9 @@ public final class MediaAttribute extends Attribute
 	
 	protected List createColumns(final String name, final boolean notNull)
 	{
+		// TODO: create column only, if major mime type is not fixed
 		mimeMajor = new StringColumn(getType(), name + "Major", notNull);
+		// TODO: create column only, if minor mime type is not fixed
 		mimeMinor = new StringColumn(getType(), name + "Minor", notNull);
 		return Arrays.asList(new StringColumn[]{mimeMajor, mimeMinor});
 	}
