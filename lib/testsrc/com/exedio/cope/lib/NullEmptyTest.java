f0ca78a13df5d1b4ba0c14bef5873ce812698adb
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-04-30 10:48:27 +0000

    added NullEmptyTest
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@708 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ lib/testsrc/com/exedio/cope/lib/NullEmptyTest.java
@@ -0,0 +1,51 @@
+package com.exedio.cope.lib;
+
+/**
+ * Test, whether database converts empty strings to null,
+ * and how the framework hides such behaviour from the user.
+ */ 
+public class NullEmptyTest extends DatabaseLibTest
+{
+	ItemWithoutAttributes someItem;
+	ItemWithManyAttributes item;
+	ItemWithManyAttributes item2;
+	String emptyString;
+
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		someItem = new ItemWithoutAttributes();
+		item = new ItemWithManyAttributes("someString", 5, true, someItem, ItemWithManyAttributes.SomeEnumeration.enumValue1);
+		item2 = new ItemWithManyAttributes("someString", 5, false, someItem, ItemWithManyAttributes.SomeEnumeration.enumValue2);
+		// TODO: database must hide this from the user
+		if(HsqldbDatabase.class.equals(Database.theInstance.getClass()))
+			emptyString = "";
+		else
+			emptyString = null;
+	}
+
+	public void tearDown() throws Exception
+	{
+		assertDelete(item);
+		assertDelete(item2);
+		assertDelete(someItem);
+		super.tearDown();
+	}
+
+	public void testNullEmpty()
+			throws IntegrityViolationException
+	{
+		assertEquals(null, item.getSomeString());
+
+		item.setSomeString("");
+		assertEquals("", item.getSomeString());
+		item.passivate();
+		assertEquals(emptyString, item.getSomeString());
+
+		item.setSomeString(null);
+		assertEquals(null, item.getSomeString());
+		item.passivate();
+		assertEquals(null, item.getSomeString());
+	}
+
+}
