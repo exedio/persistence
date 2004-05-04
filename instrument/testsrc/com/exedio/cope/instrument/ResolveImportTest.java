b2d4fb85046743a5bb560c4a03a2db26fdef97af
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-05-04 11:51:46 +0000

    import a bit
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@723 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- instrument/testsrc/com/exedio/cope/instrument/ResolveImportTest.java
+++ instrument/testsrc/com/exedio/cope/instrument/ResolveImportTest.java
@@ -1,5 +1,6 @@
 package com.exedio.cope.instrument;
 
+import com.exedio.cope.instrument.findtype.BothFindType;
 import com.exedio.cope.instrument.findtype.FindType;
 import com.exedio.cope.instrument.findtype.subfindtype.SubFindType;
 import com.exedio.cope.instrument.findtype.subfindtype2.SubFindType2;
@@ -30,8 +31,8 @@ public class ResolveImportTest extends InstrumentorTest
 		assertEquals(SubFindType2.class, file.findType("SubFindType2"));
 		assertEquals(SubFindType2.class, file.findType(SubFindType2.class.getName()));
 
-		assertEquals(com.exedio.cope.instrument.findtype.BothFindType.class, file.findType("BothFindType"));
-		assertEquals(com.exedio.cope.instrument.findtype.BothFindType.class, file.findType(com.exedio.cope.instrument.findtype.BothFindType.class.getName()));
+		assertEquals(BothFindType.class, file.findType("BothFindType"));
+		assertEquals(BothFindType.class, file.findType(BothFindType.class.getName()));
 
 		assertEquals(com.exedio.cope.instrument.findtype.subfindtype.BothFindType2.class, file.findType("BothFindType2"));
 		assertEquals(com.exedio.cope.instrument.findtype.subfindtype.BothFindType2.class, file.findType(com.exedio.cope.instrument.findtype.subfindtype.BothFindType2.class.getName()));
