7f732537da51c942458aac63b16f3b25154e6b2e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-05-06 16:58:38 +0000

    introduce CompositeCondition
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@749 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/search/AndCondition.java
+++ lib/src/com/exedio/cope/lib/search/AndCondition.java
@@ -1,50 +1,10 @@
 
 package com.exedio.cope.lib.search;
 
-import java.util.TreeSet;
-
-import com.exedio.cope.lib.Statement;
-
-public final class AndCondition extends Condition
+public final class AndCondition extends CompositeCondition
 {
-	public final Condition[] conditions;
-
 	public AndCondition(final Condition[] conditions)
 	{
-		this.conditions = conditions;
-	}
-
-	public final void appendStatement(final Statement bf)
-	{
-		bf.append('(');
-		conditions[0].appendStatement(bf);
-		for(int i = 1; i<conditions.length; i++)
-		{
-			bf.append(" and ");
-			conditions[i].appendStatement(bf);
-		}
-		bf.append(')');
-	}
-
-	public final void check(final TreeSet fromTypes)
-	{
-		for(int i = 0; i<conditions.length; i++)
-			conditions[i].check(fromTypes);
-	}
-
-	public final String toString()
-	{
-		final StringBuffer buf = new StringBuffer();
-		
-		buf.append('(');
-		buf.append(conditions[0].toString());
-		for(int i = 1; i<conditions.length; i++)
-		{
-			buf.append(" and ");
-			buf.append(conditions[i].toString());
-		}
-		buf.append(')');
-		
-		return buf.toString();
+		super(" and ", conditions);
 	}
 }
---
+++ lib/src/com/exedio/cope/lib/search/CompositeCondition.java
@@ -0,0 +1,52 @@
+
+package com.exedio.cope.lib.search;
+
+import java.util.TreeSet;
+
+import com.exedio.cope.lib.Statement;
+
+public abstract class CompositeCondition extends Condition
+{
+	private final String operator;
+	public final Condition[] conditions;
+
+	public CompositeCondition(final String operator, final Condition[] conditions)
+	{
+		this.operator = operator;
+		this.conditions = conditions;
+	}
+
+	public final void appendStatement(final Statement bf)
+	{
+		bf.append('(');
+		conditions[0].appendStatement(bf);
+		for(int i = 1; i<conditions.length; i++)
+		{
+			bf.append(operator);
+			conditions[i].appendStatement(bf);
+		}
+		bf.append(')');
+	}
+
+	public final void check(final TreeSet fromTypes)
+	{
+		for(int i = 0; i<conditions.length; i++)
+			conditions[i].check(fromTypes);
+	}
+
+	public final String toString()
+	{
+		final StringBuffer buf = new StringBuffer();
+		
+		buf.append('(');
+		buf.append(conditions[0].toString());
+		for(int i = 1; i<conditions.length; i++)
+		{
+			buf.append(operator);
+			buf.append(conditions[i].toString());
+		}
+		buf.append(')');
+		
+		return buf.toString();
+	}
+}
