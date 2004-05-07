5ecc72c37849f134141a9031433182c0c317ddbd
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-05-07 17:49:25 +0000

    add TODO
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@766 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- instrument/src/com/exedio/cope/instrument/Generator.java
+++ instrument/src/com/exedio/cope/instrument/Generator.java
@@ -600,6 +600,7 @@ final class Generator
 			if(persistentAttribute.qualifiers != null)
 				qualifiers.addAll(persistentAttribute.qualifiers);
 			o.write("final ");
+			// TODO: use boxed type
 			o.write(persistentAttribute.getPersistentType());
 			o.write(" searched");
 			o.write(persistentAttribute.getCamelCaseName());
