9efbfe96a7f74a0b2143fa3f3ebbf057223456fa
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-07-01 16:56:03 +0000

    add a first web test for copernica
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1110 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ copernica/webtestsrc/com/exedio/copernica/WebTest.java
@@ -0,0 +1,26 @@
+package com.exedio.copernica;
+
+import net.sourceforge.jwebunit.WebTestCase;
+
+public class WebTest extends WebTestCase
+{
+
+	public WebTest(String name)
+	{
+		super(name);
+	}
+
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		getTestContext().setBaseUrl("http://localhost:8080/copernicatest/");
+	}
+
+	public void testSearch()
+	{
+		beginAt("copernica.jsp");
+		assertTitleEquals("Copernica");
+		assertLinkPresentWithText("de");
+		assertLinkPresentWithText("Attribute Item");
+	}
+}
