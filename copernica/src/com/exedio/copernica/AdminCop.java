6471f6c7f6d48a5b757befee4738bc452a98e009
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-07-13 17:33:10 +0000

    narrow database report to tables
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1252 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- copernica/src/com/exedio/copernica/AdminCop.java
+++ copernica/src/com/exedio/copernica/AdminCop.java
@@ -2,29 +2,59 @@ package com.exedio.copernica;
 
 import java.util.Map;
 
+import com.exedio.cope.lib.ReportTable;
+
 final class AdminCop extends Cop
 {
 	static final String REPORT = "report";
 
 	final boolean report;
+	final String reportTable;
 
-	AdminCop(final boolean report)
+	AdminCop(final boolean report, final String reportTable)
 	{
 		super("admin.jsp");
 		this.report = report;
+		this.reportTable = reportTable;
+		if(!report && reportTable!=null)
+			throw new RuntimeException();
+		
 		if(report)
-			addParameter(REPORT, "true");
+			addParameter(REPORT, reportTable==null ? "" : reportTable);
 	}
 	
 	final AdminCop toggleReport()
 	{
-		return new AdminCop(!report);
+		return new AdminCop(!report, null);
+	}
+	
+	final AdminCop narrowReport(final ReportTable reportTable)
+	{
+		if(!report)
+			throw new RuntimeException();
+			
+		return new AdminCop(true, reportTable.name);
+	}
+	
+	final boolean skipTable(final ReportTable table)
+	{
+		return reportTable!=null && !reportTable.equals(table.name);
 	}
 
 	static final AdminCop getCop(final Map parameterMap)
 	{	
 		final String reportID = getParameter(parameterMap, REPORT);
-		return new AdminCop(reportID!=null);
+		if(reportID==null)
+		{
+			return new AdminCop(false, null);
+		}
+		else
+		{
+			if(reportID.length()==0)
+				return new AdminCop(true, null);
+			else
+				return new AdminCop(true, reportID);
+		}
 	}
 
 }
