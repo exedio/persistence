add7bc80bde617bc24112239ed142fbe44d5d0c6
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-08-16 07:15:49 +0000

    fix teardown.database with new Properties
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1289 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/testsrc/com/exedio/cope/lib/DatabaseLibTest.java
+++ lib/testsrc/com/exedio/cope/lib/DatabaseLibTest.java
@@ -228,6 +228,7 @@ public abstract class DatabaseLibTest extends AbstractLibTest
 
 	public static void main(String[] args)
 	{
+		model.setProperties(new Properties());
 		model.tearDownDatabase();
 	}
 
