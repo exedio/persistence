fba337edcadb6ba77d6cc31c396569cc4cfe64b9
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-08-24 18:26:11 +0000

    add constructors for native types
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1358 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/AttributeValue.java
+++ lib/src/com/exedio/cope/lib/AttributeValue.java
@@ -17,10 +17,22 @@ public final class AttributeValue
 		this.value = value;
 	}
 	
+	public AttributeValue(final BooleanAttribute attribute, final boolean value)
+	{
+		this.attribute = attribute;
+		this.value = value ? Boolean.TRUE : Boolean.FALSE;
+	}
+	
 	public AttributeValue(final IntegerAttribute attribute, final Integer value)
 	{
 		this.attribute = attribute;
 		this.value = value;
+	}
+	
+	public AttributeValue(final IntegerAttribute attribute, final int value)
+	{
+		this.attribute = attribute;
+		this.value = new Integer(value);
 	}
 	
 	public AttributeValue(final LongAttribute attribute, final Long value)
@@ -29,10 +41,22 @@ public final class AttributeValue
 		this.value = value;
 	}
 	
+	public AttributeValue(final LongAttribute attribute, final long value)
+	{
+		this.attribute = attribute;
+		this.value = new Long(value);
+	}
+	
 	public AttributeValue(final DoubleAttribute attribute, final Double value)
 	{
 		this.attribute = attribute;
 		this.value = value;
+	}
+	
+	public AttributeValue(final DoubleAttribute attribute, final double value)
+	{
+		this.attribute = attribute;
+		this.value = new Double(value);
 	}
 	
 	public AttributeValue(final ItemAttribute attribute, final Item value)
