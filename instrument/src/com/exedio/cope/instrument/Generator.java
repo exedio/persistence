6446deaf640132c5c43e08f2b444cdcd0f40f67c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-09-28 16:59:47 +0000

    cleanup imports
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1449 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- instrument/src/com/exedio/cope/instrument/Generator.java
+++ instrument/src/com/exedio/cope/instrument/Generator.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Modifier;
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
