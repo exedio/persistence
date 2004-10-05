acc36071fa3afb0780951da27d4330762e8db11a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-10-05 17:21:07 +0000

    made final
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1465 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- copernica/src/com/exedio/copernica/MalformedFieldException.java
+++ copernica/src/com/exedio/copernica/MalformedFieldException.java
@@ -1,7 +1,7 @@
 package com.exedio.copernica;
 
 
-class MalformedFieldException extends Exception
+final class MalformedFieldException extends Exception
 {
 	MalformedFieldException(final String message)
 	{
