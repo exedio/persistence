0049b5b72e5b22bc40f4cc28670b964d28ec8908
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-12-07 12:40:17 +0000

    use deleteOnTearDown
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1614 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/testsrc/com/exedio/cope/lib/HierarchyTest.java
+++ lib/testsrc/com/exedio/cope/lib/HierarchyTest.java
@@ -11,6 +11,7 @@ public class HierarchyTest extends DatabaseLibTest
 			throws IntegrityViolationException
 	{
 		final FirstSub firstItem = new FirstSub(0);
+		deleteOnTearDown(firstItem);
 		assertID(0, firstItem);
 		assertEquals(0, firstItem.getSuperInt());
 		assertEquals(null, firstItem.getFirstSubString());
@@ -28,23 +29,21 @@ public class HierarchyTest extends DatabaseLibTest
 		assertEquals("firstSubString", firstItem.getFirstSubString());
 		
 		final SecondSub secondItem = new SecondSub(2);
+		deleteOnTearDown(secondItem);
 		assertID(1, secondItem);
 		assertEquals(2, secondItem.getSuperInt());
 		assertEquals(null, secondItem.getFirstSubString());
 
 		final SecondSub secondItem2 = new SecondSub(3);
+		deleteOnTearDown(secondItem2);
 		assertID(2, secondItem2);
 
 		final FirstSub firstItem2 = new FirstSub(4);
+		deleteOnTearDown(firstItem2);
 		assertID(3, firstItem2);
 		
 		assertEquals(list(firstItem), firstItem.TYPE.search(Cope.equal(firstItem.firstSubString, "firstSubString")));
 		assertEquals(list(), firstItem.TYPE.search(Cope.equal(firstItem.firstSubString, "firstSubStringX")));
-
-		firstItem2.delete();
-		secondItem2.delete();
-		secondItem.delete();
-		firstItem.delete();
 	}
 
 }
