b0fac93619072a60ca864fe9f42c5bdb2eb20eb9
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2004-12-15 06:22:20 +0000

    remove some not-needed constructors
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1643 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/search/EqualCondition.java
+++ lib/src/com/exedio/cope/lib/search/EqualCondition.java
@@ -1,17 +1,10 @@
 
 package com.exedio.cope.lib.search;
 
-import java.util.Date;
 import java.util.TreeSet;
 
-import com.exedio.cope.lib.BooleanAttribute;
-import com.exedio.cope.lib.DateAttribute;
-import com.exedio.cope.lib.DoubleAttribute;
 import com.exedio.cope.lib.Function;
 import com.exedio.cope.lib.IntegerFunction;
-import com.exedio.cope.lib.Item;
-import com.exedio.cope.lib.ItemAttribute;
-import com.exedio.cope.lib.LongAttribute;
 import com.exedio.cope.lib.ObjectAttribute;
 import com.exedio.cope.lib.Statement;
 import com.exedio.cope.lib.StringFunction;
@@ -42,36 +35,6 @@ public final class EqualCondition extends Condition
 	public EqualCondition(final IntegerFunction function, final Integer value)
 	{
 		this.function = function;
-		this.value = value;
-	}
-	
-	public EqualCondition(final LongAttribute attribute, final Long value)
-	{
-		this.function = attribute;
-		this.value = value;
-	}
-	
-	public EqualCondition(final BooleanAttribute attribute, final Boolean value)
-	{
-		this.function = attribute;
-		this.value = value;
-	}
-	
-	public EqualCondition(final DoubleAttribute attribute, final Double value)
-	{
-		this.function = attribute;
-		this.value = value;
-	}
-	
-	public EqualCondition(final DateAttribute attribute, final Date value)
-	{
-		this.function = attribute;
-		this.value = value;
-	}
-	
-	public EqualCondition(final ItemAttribute attribute, final Item value)
-	{
-		this.function = attribute;
 		this.value = value;
 	}
 	
