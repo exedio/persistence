62c70694ed2e22d9873f73df4b08be903450c42f
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-02-11 10:29:39 +0000

    add check-all-drop button
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1753 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- copernica/jsp/admin-report.js
+++ copernica/jsp/admin-report.js
@@ -1,11 +1,11 @@
-function checkAllAnalyze()
+function checkAll(checkboxName)
 {
 	var checkboxes = document.all.tags("input");
 	for(j=0; j<checkboxes.length; j++)
 	{
 		var checkbox = checkboxes[j];
 		if(checkbox.type=="checkbox" &&
-			checkbox.name=="ANALYZE_TABLE")
+			checkbox.name==checkboxName)
 		{
 			checkbox.checked = true;
 		}
