43bffa40381809027f0b61f0123c3ec5230a63eb
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-02-25 17:49:31 +0000

    put error.jsp into CopernicaServlet
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1815 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- copernica/src/com/exedio/copernica/CopernicaServlet.java
+++ copernica/src/com/exedio/copernica/CopernicaServlet.java
@@ -3,16 +3,57 @@ package com.exedio.copernica;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CopernicaServlet extends HttpServlet
+public final class CopernicaServlet extends HttpServlet
 {
 	CopernicaProvider provider = null;
 	boolean checked;
+
+	private final Random random = new Random();
+	
+	final void printException(final PrintStream out, final Exception exception)
+	{
+		exception.printStackTrace(out);
+		if(exception instanceof ServletException)
+		{
+			final Throwable rootCause =
+				((ServletException)exception).getRootCause();
+			if(rootCause!=null)
+			{
+				out.println("root cause for ServletException:");
+				rootCause.printStackTrace(out);
+			}
+			else
+			{
+				out.println("no root cause for ServletException");
+			}
+		}
+		out.flush();
+	}
+	
+	/**
+	 * Returns the id under with the exception has been reported in the log.
+	 */
+	final String reportException(final Exception exception)
+	{
+		final long idLong;
+		synchronized(random)
+		{
+			idLong = random.nextLong();
+		}
+		final String id = String.valueOf(Math.abs(idLong));
+		System.out.println("--------I"+id+"-----");
+		printException(System.out, exception);
+		System.out.println("--------O"+id+"-----");
+		return id;
+	}
+
 	
 	public void init() throws ServletException
 	{
@@ -52,6 +93,11 @@ public class CopernicaServlet extends HttpServlet
 			response.addHeader("WWW-Authenticate", "Basic realm=\"Copernica\"");
 			response.setStatus(response.SC_UNAUTHORIZED);
 			Copernica_Jspm.writeAuthenticationError(out, e);
+		}
+		catch(Exception e)
+		{
+			final boolean onPage = "jo-man".equals(request.getParameter("display_error"));
+			Copernica_Jspm.writeException(out, this, e, onPage);
 		}
 	}
 
