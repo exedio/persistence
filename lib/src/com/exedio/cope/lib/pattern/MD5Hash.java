5ab1a9fa01e7d35afc3f56e5eb84a21fbb2d4b9a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-03-21 10:42:04 +0000

    introduce constant for hash algorithm name
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1902 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/lib/pattern/MD5Hash.java
+++ lib/src/com/exedio/cope/lib/pattern/MD5Hash.java
@@ -4,14 +4,16 @@ import com.exedio.cope.lib.StringAttribute;
 
 public final class MD5Hash extends JavaHash
 {
+	private static final String HASH = "MD5";
+
 	public MD5Hash(final StringAttribute storage)
 	{
-		super(storage, "MD5");
+		super(storage, HASH);
 	}
 
 	public MD5Hash(final StringAttribute storage, final String encoding)
 	{
-		super(storage, "MD5", encoding);
+		super(storage, HASH, encoding);
 	}
 
 }
