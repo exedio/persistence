9a338de10e7bbb145e6a8d0d07e94625633b69be
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-03-24 08:14:47 +0000

    replace init.jsp by jspm-based servlet
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@1930 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ copernica/testsrc/com/exedio/copernica/InitServlet.java
@@ -0,0 +1,33 @@
+package com.exedio.copernica;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.exedio.cops.CopsServlet;
+
+public class InitServlet extends CopsServlet
+{
+
+	protected void doRequest(
+			final HttpServletRequest request,
+			final HttpServletResponse response)
+		throws ServletException, IOException
+	{
+		response.setContentType("text/html");
+
+		final boolean initialize = (request.getParameter("INIT")!=null);
+		if(initialize)
+			CopernicaTestProvider.initializeExampleSystem();
+
+		PrintStream out = new PrintStream(response.getOutputStream());
+
+		Init_Jspm.write(out, initialize);
+
+		out.close();
+	}
+
+}
