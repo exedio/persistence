d4e9b59210e5f81a0fe7312eaab3a2ff5526f85c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-04-15 11:27:48 +0000

    put StringField in cops framework
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@2005 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- copernica/src/com/exedio/copernica/ItemForm.java
+++ copernica/src/com/exedio/copernica/ItemForm.java
@@ -43,6 +43,7 @@ import com.exedio.cops.Form;
 import com.exedio.cops.IntegerField;
 import com.exedio.cops.LongField;
 import com.exedio.cops.RadioField;
+import com.exedio.cops.StringField;
 import com.exedio.cops.TextField;
 
 final class ItemForm extends Form
@@ -249,9 +250,9 @@ final class ItemForm extends Form
 		else if(attribute instanceof StringAttribute)
 		{
 			if(post)
-				return new StringField((StringAttribute)attribute, name, readOnly, hidden);
+				return new StringField(this, attribute, name, readOnly, hidden);
 			else
-				return new StringField((StringAttribute)attribute, name, readOnly, (String)item.getAttribute(attribute), hidden);
+				return new StringField(this, attribute, name, readOnly, (String)item.getAttribute(attribute), hidden);
 		}
 		else if(attribute instanceof ItemAttribute)
 		{
@@ -437,39 +438,7 @@ final class ItemForm extends Form
 		}
 	}
 	
-	// TODO: put StringField in cops framework
-	final class StringField extends TextField
-	{
-		final String content;
 
-		/**
-		 * Constructs a form field with an initial value.
-		 */
-		StringField(final StringAttribute attribute, final String name, final boolean readOnly, final String value, final boolean hidden)
-		{
-			super(ItemForm.this, attribute, name, readOnly, (value==null) ? "" : value, hidden);
-			
-			this.content = value;
-		}
-	
-		/**
-		 * Constructs a form field with a value obtained from the submitted form.
-		 */
-		StringField(final StringAttribute attribute, final String name, final boolean readOnly, final boolean hidden)
-		{
-			super(ItemForm.this, attribute, name, readOnly, hidden);
-			
-			final String value = this.value;
-			content = value; // TODO: convert empty string to null
-		}
-		
-		public Object getContent()
-		{
-			return content;
-		}
-
-	}
-	
 	private void save()
 	{
 		for(Iterator i = getAllFields().iterator(); i.hasNext(); )
