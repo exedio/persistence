d4dbd8f020ec40dffe646723d1968ef35d7aa6de
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-05-20 15:16:55 +0000

    document.all.tags does not work with newer versions of Mozilla/Firefox/Konqueror
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@2260 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- copernica/jsp/admin-report.js
+++ copernica/jsp/admin-report.js
@@ -1,6 +1,6 @@
 function checkAll(checkboxName)
 {
-	var checkboxes = document.all.tags("input");
+	var checkboxes = document.body.getElementsByTagName("input");
 	for(j=0; j<checkboxes.length; j++)
 	{
 		var checkbox = checkboxes[j];
