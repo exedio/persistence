80be80870b5accd872cb2d132cb469c0d2783a3a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-08-16 16:08:10 +0000

    rename GETTER_DATA_MINOR to GETTER_HTTP_ENTITY_MINOR
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@2894 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- instrument/src/com/exedio/cope/instrument/Generator.java
+++ instrument/src/com/exedio/cope/instrument/Generator.java
@@ -60,7 +60,7 @@ final class Generator
 	private static final String SETTER_HTTP_ENTITY_IOEXCEPTION = "if accessing {0} throws an IOException.";
 	private static final String GETTER_HTTP_ENTITY_URL =     "Returns a URL the data of the data attribute {0} is available under.";
 	private static final String GETTER_HTTP_ENTITY_MAJOR = "Returns the major mime type of the data attribute {0}.";
-	private static final String GETTER_DATA_MINOR = "Returns the minor mime type of the data attribute {0}.";
+	private static final String GETTER_HTTP_ENTITY_MINOR = "Returns the minor mime type of the data attribute {0}.";
 	private static final String GETTER_DATA_DATA = "Returns the data of the data attribute {0}."; // TODO change texts to HttpEntity
 	private static final String TOUCHER = "Sets the current date for the date attribute {0}.";
 	private static final String FINDER_UNIQUE = "Finds a {0} by it''s unique attributes.";
@@ -520,7 +520,7 @@ final class Generator
 		// getters
 		writeDataGetterMethod(attribute, String.class,      "URL",       GETTER_HTTP_ENTITY_URL);
 		writeDataGetterMethod(attribute, String.class,      "MimeMajor", GETTER_HTTP_ENTITY_MAJOR);
-		writeDataGetterMethod(attribute, String.class,      "MimeMinor", GETTER_DATA_MINOR);
+		writeDataGetterMethod(attribute, String.class,      "MimeMinor", GETTER_HTTP_ENTITY_MINOR);
 		writeDataGetterMethod(attribute, InputStream.class, "Data",      GETTER_DATA_DATA);
 		// TODO generate isNull as well
 		
