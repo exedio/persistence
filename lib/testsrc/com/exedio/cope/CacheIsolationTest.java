e4aae32f374f3c75ec4b52276b30ed276a322992
Frank Finger <frank.finger@exedio.com>
2005-09-01 15:07:49 +0000

    added tests for reloading
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@3225 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/testsrc/com/exedio/cope/CacheIsolationTest.java
+++ lib/testsrc/com/exedio/cope/CacheIsolationTest.java
@@ -22,22 +22,53 @@ public class CacheIsolationTest extends AbstractLibTest
 	public void test() throws MandatoryViolationException
 	{
 		model.commit();
-		Transaction txChangeItem = model.startTransaction( "change item" );
-		if ( ! model.supportsReadCommitted() )
+		Transaction txChangeItem = null, txChangeCollisionItem = null;
+		try
 		{
-			// forced preload as work-around of hsql shortcoming:
+			txChangeItem = model.startTransaction( "change item" );
+			if ( ! model.supportsReadCommitted() )
+			{
+				// forced preload as work-around of hsql shortcoming:
+				assertEquals( "collision", collisionItem.getName() );
+			}
+			model.leaveTransaction();
+			txChangeCollisionItem = model.startTransaction( "change collision item" );
+			collisionItem.setName( "othercollision" );
+			assertEquals( "othercollision", collisionItem.getName() );
+			assertEquals( txChangeCollisionItem, model.leaveTransaction() );
+			model.joinTransaction( txChangeItem );
 			assertEquals( "collision", collisionItem.getName() );
+			try
+			{
+				item.setUniqueString( collisionItem.getUniqueString() );
+				fail();
+			}
+			catch ( UniqueViolationException e )
+			{
+				// fine
+			}
+			assertEquals( null, txChangeItem.getEntityIfActive(item.type, item.pk) );
+			model.getDatabase().expectLoad( txChangeItem, item );
+			assertEquals( "collision", collisionItem.getName() );
+			assertEquals( "blub", item.getName() );
+			model.getDatabase().verifyExpectations();
+			if ( model.supportsReadCommitted() )
+			{
+				assertEquals( null, item.getUniqueString() );
+			}
+			model.commit();
+			model.joinTransaction( txChangeCollisionItem );
+			model.commit();
 		}
-		model.leaveTransaction();
-		Transaction txChangeCollisionItem = model.startTransaction( "change collision item" );
-		collisionItem.setName( "othercollision" );
-		assertEquals( "othercollision", collisionItem.getName() );
-		assertEquals( txChangeCollisionItem, model.leaveTransaction() );
-		model.joinTransaction( txChangeItem );
-		assertEquals( "collision", collisionItem.getName() );
-		model.commit();
-		model.joinTransaction( txChangeCollisionItem );
-		model.commit();
+		finally
+		{
+			if ( model.hasCurrentTransaction() )
+			{
+				model.leaveTransaction();
+			}
+			if ( txChangeItem!=null && !txChangeItem.isClosed() ) txChangeItem.rollback();
+			if ( txChangeCollisionItem!=null && !txChangeCollisionItem.isClosed() ) txChangeCollisionItem.rollback();
+		}
 		model.startTransaction("just for tearDown");
 	}
 	
