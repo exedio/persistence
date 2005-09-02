2534507a998ba35ceaa7f6828086b1a67c783614
Frank Finger <frank.finger@exedio.com>
2005-09-02 09:02:35 +0000

    added lifecycle test
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@3235 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/testsrc/com/exedio/cope/TransactionOnlyTest.java
+++ lib/testsrc/com/exedio/cope/TransactionOnlyTest.java
@@ -40,4 +40,48 @@ public class TransactionOnlyTest extends AbstractLibTest
 		model.startTransaction("just for tearDown");
 		assertEquals( "just for tearDown", model.getCurrentTransaction().getName() );
 	}
+	
+	public void testTransactionLifecycle()
+	{
+		final Transaction copeTest = model.getCurrentTransaction();
+		assertContains( copeTest, model.getOpenTransactions() );
+		assertUnmodifiable( model.getOpenTransactions() );
+		assertEquals( false, copeTest.isClosed() );
+		
+		model.leaveTransaction();
+		assertContains( copeTest, model.getOpenTransactions() );
+		assertEquals( false, copeTest.isClosed() );
+		
+		final Transaction tx1 = model.startTransaction( "tx1" );
+		assertContains( copeTest, tx1, model.getOpenTransactions() );
+		assertEquals( false, copeTest.isClosed() );
+		assertEquals( false, tx1.isClosed() );
+		
+		model.leaveTransaction();
+		assertContains( copeTest, tx1, model.getOpenTransactions() );
+		assertEquals( false, copeTest.isClosed() );
+		assertEquals( false, tx1.isClosed() );
+		
+		model.joinTransaction( copeTest );
+		assertContains( copeTest, tx1, model.getOpenTransactions() );
+		assertEquals( false, copeTest.isClosed() );
+		assertEquals( false, tx1.isClosed() );
+		
+		model.commit();
+		assertContains( tx1, model.getOpenTransactions() );
+		assertEquals( true, copeTest.isClosed() );
+		assertEquals( false, tx1.isClosed() );
+		
+		model.joinTransaction( tx1 );
+		assertContains( tx1, model.getOpenTransactions() );
+		assertEquals( true, copeTest.isClosed() );
+		assertEquals( false, tx1.isClosed() );
+		
+		model.rollback();
+		assertContains( model.getOpenTransactions() );
+		assertEquals( true, copeTest.isClosed() );
+		assertEquals( true, tx1.isClosed() );
+		
+		model.startTransaction( "forTearDown" );
+	}
 }
