fd4fdd312a571e2efd7fbe1d37d38f5ad000b94b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-09-05 15:26:01 +0000

    make test methods public
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@3273 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/junitsrc/com/exedio/cope/junit/CopeAssert.java
+++ lib/junitsrc/com/exedio/cope/junit/CopeAssert.java
@@ -34,7 +34,7 @@ import com.exedio.cope.Query;
 
 public abstract class CopeAssert extends TestCase
 {
-	protected final static void assertContainsList(final List expected, final Collection actual)
+	public final static void assertContainsList(final List expected, final Collection actual)
 	{
 		if(expected.size()!=actual.size() ||
 				!expected.containsAll(actual) ||
@@ -42,110 +42,110 @@ public abstract class CopeAssert extends TestCase
 			fail("expected "+expected+", but was "+actual);
 	}
 
-	protected final static void assertContains(final Collection actual)
+	public final static void assertContains(final Collection actual)
 	{
 		assertContainsList(Collections.EMPTY_LIST, actual);
 	}
 
-	protected final static void assertContains(final Object o, final Collection actual)
+	public final static void assertContains(final Object o, final Collection actual)
 	{
 		assertContainsList(Collections.singletonList(o), actual);
 	}
 
-	protected final static void assertContains(final Object o1, final Object o2, final Collection actual)
+	public final static void assertContains(final Object o1, final Object o2, final Collection actual)
 	{
 		assertContainsList(Arrays.asList(new Object[]{o1, o2}), actual);
 	}
 
-	protected final static void assertContains(final Object o1, final Object o2, final Object o3, final Collection actual)
+	public final static void assertContains(final Object o1, final Object o2, final Object o3, final Collection actual)
 	{
 		assertContainsList(Arrays.asList(new Object[]{o1, o2, o3}), actual);
 	}
 
-	protected final static void assertContains(final Object o1, final Object o2, final Object o3, final Object o4, final Collection actual)
+	public final static void assertContains(final Object o1, final Object o2, final Object o3, final Object o4, final Collection actual)
 	{
 		assertContainsList(Arrays.asList(new Object[]{o1, o2, o3, o4}), actual);
 	}
 
-	protected final static void assertContains(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Collection actual)
+	public final static void assertContains(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Collection actual)
 	{
 		assertContainsList(Arrays.asList(new Object[]{o1, o2, o3, o4, o5}), actual);
 	}
 
-	protected final static void assertContainsUnmodifiable(final Collection actual)
+	public final static void assertContainsUnmodifiable(final Collection actual)
 	{
 		assertUnmodifiable(actual);
 		assertContains(actual);
 	}
 
-	protected final static void assertContainsUnmodifiable(final Object o, final Collection actual)
+	public final static void assertContainsUnmodifiable(final Object o, final Collection actual)
 	{
 		assertUnmodifiable(actual);
 		assertContains(o, actual);
 	}
 
-	protected final static void assertContainsUnmodifiable(final Object o1, final Object o2, final Collection actual)
+	public final static void assertContainsUnmodifiable(final Object o1, final Object o2, final Collection actual)
 	{
 		assertUnmodifiable(actual);
 		assertContains(o1, o2, actual);
 	}
 
-	protected final static List list()
+	public final static List list()
 	{
 		return Collections.EMPTY_LIST;
 	}
 
-	protected final static List list(final Object o)
+	public final static List list(final Object o)
 	{
 		return Collections.singletonList(o);
 	}
 	
-	protected final static List list(final Object o1, final Object o2)
+	public final static List list(final Object o1, final Object o2)
 	{
 		return Arrays.asList(new Object[]{o1, o2});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3)
+	public final static List list(final Object o1, final Object o2, final Object o3)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3, final Object o4)
+	public final static List list(final Object o1, final Object o2, final Object o3, final Object o4)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3, o4});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5)
+	public final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3, o4, o5});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6)
+	public final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3, o4, o5, o6});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7)
+	public final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3, o4, o5, o6, o7});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7, final Object o8)
+	public final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7, final Object o8)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3, o4, o5, o6, o7, o8});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7, final Object o8, final Object o9)
+	public final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7, final Object o8, final Object o9)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3, o4, o5, o6, o7, o8, o9});
 	}
 	
-	protected final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7, final Object o8, final Object o9, final Object o10)
+	public final static List list(final Object o1, final Object o2, final Object o3, final Object o4, final Object o5, final Object o6, final Object o7, final Object o8, final Object o9, final Object o10)
 	{
 		return Arrays.asList(new Object[]{o1, o2, o3, o4, o5, o6, o7, o8, o9, o10});
 	}
 	
-	protected final static void assertUnmodifiable(final Collection c)
+	public final static void assertUnmodifiable(final Collection c)
 	{
 		try
 		{
@@ -198,7 +198,7 @@ public abstract class CopeAssert extends TestCase
 		}
 	}
 	
-	protected final static void assertEqualsUnmodifiable(final List expected, final Collection actual)
+	public final static void assertEqualsUnmodifiable(final List expected, final Collection actual)
 	{
 		assertUnmodifiable(actual);
 		assertEquals(expected, actual);
@@ -209,7 +209,7 @@ public abstract class CopeAssert extends TestCase
 	/**
 	 * @param resolution the expected resolution of the timestamp saved in param <code>actual</code>, in milliseonds
 	 */
-	protected final static void assertWithin(final long resolution, final Date expectedBefore, final Date expectedAfter, final Date actual)
+	public final static void assertWithin(final long resolution, final Date expectedBefore, final Date expectedAfter, final Date actual)
 	{
 		final Date expectedBeforeFloor = new Date((expectedBefore.getTime() / resolution) * resolution);
 		final Date expectedAfterCeil   = new Date(((expectedAfter.getTime() / resolution) * resolution) + resolution);
@@ -229,7 +229,7 @@ public abstract class CopeAssert extends TestCase
 		assertTrue(message, !expectedAfterCeil.before(actual));
 	}
 
-	protected final static void assertWithin(final Date expectedBefore, final Date expectedAfter, final Date actual)
+	public final static void assertWithin(final Date expectedBefore, final Date expectedAfter, final Date actual)
 	{
 		final SimpleDateFormat df = new SimpleDateFormat(DATE_FORMAT_FULL);
 		final String message =
@@ -241,7 +241,7 @@ public abstract class CopeAssert extends TestCase
 		assertTrue(message, !expectedAfter.before(actual));
 	}
 	
-	protected final static Object waitForKey(final Object o)
+	public final static Object waitForKey(final Object o)
 	{
 		System.out.println("WAITING FOR KEY");
 		try
@@ -255,7 +255,7 @@ public abstract class CopeAssert extends TestCase
 		return o;
 	}
 
-	protected final static void waitForKey()
+	public final static void waitForKey()
 	{
 		waitForKey(null);
 	}
@@ -264,7 +264,7 @@ public abstract class CopeAssert extends TestCase
 	 * Calls {@link Query#search()} on the given query and returns the result.
 	 * Prints the statement info to standard out.
 	 */
-	protected static final Collection infoSearch(final Query query)
+	public static final Collection infoSearch(final Query query)
 	{
 		query.enableMakeStatementInfo();
 		final Collection result = query.search();
