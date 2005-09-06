327a95a3b6fa979d401649a8ab67adbd1944dd02
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-09-06 08:34:09 +0000

    test MEDIA_ROOT_URL
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@3288 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/testsrc/com/exedio/cope/ModelTest.java
+++ lib/testsrc/com/exedio/cope/ModelTest.java
@@ -151,6 +151,9 @@ public class ModelTest extends AbstractLibTest
 			final java.util.Properties props = (java.util.Properties)newProps.clone();
 			props.remove(Properties.MEDIA_ROOT_URL);
 			assertEquals(Properties.MEDIA_ROOT_URL_DEFAULT, new Properties(props, "source").getMediaRootUrl());
+
+			props.setProperty(Properties.MEDIA_ROOT_URL, "klack");
+			assertEquals("klack", new Properties(props, "source").getMediaRootUrl());
 		}
 	}
 	
