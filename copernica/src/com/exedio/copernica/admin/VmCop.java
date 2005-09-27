7d7f5e72da6692b4b401605ec06664d340c7807d
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-09-27 14:20:53 +0000

    print cope package information
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@3521 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- copernica/src/com/exedio/copernica/admin/VmCop.java
+++ copernica/src/com/exedio/copernica/admin/VmCop.java
@@ -48,7 +48,7 @@ final class VmCop extends AdminCop
 
 	final void writeBody(final PrintStream out, final Model model, final HttpServletRequest request) throws IOException
 	{
-		Properties_Jspm.writeVm(out);
+		Properties_Jspm.writeVm(out, Cope.class.getPackage(), VmCop.class.getPackage());
 		
 		final java.util.Properties current = model.getDatabaseInfo();
 		for(Iterator i = current.keySet().iterator(); i.hasNext(); )
