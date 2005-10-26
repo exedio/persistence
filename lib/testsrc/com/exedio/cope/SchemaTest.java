2f0ff8d3db5b19c3812a960a29cdbf62217f7376
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-10-26 16:34:28 +0000

    small refactoring
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@3754 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/testsrc/com/exedio/cope/SchemaTest.java
+++ lib/testsrc/com/exedio/cope/SchemaTest.java
@@ -297,17 +297,19 @@ public class SchemaTest extends TestmodelTest
 			assertEquals(null, min4Max8.getError());
 			assertEquals(Schema.COLOR_OK, min4Max8.getParticularColor());
 			
+			final String string8;
 			if(hsqldb)
-				assertEquals("varchar(8)", min4Max8.getType());
+				string8 = "varchar(8)";
 			else if(mysql)
-				assertEquals("varchar(8) binary", min4Max8.getType());
+				string8 = "varchar(8) binary";
 			else
 			{
 				if(model.getProperties().getDatabaseCustomProperty("varchar")!=null)
-					assertEquals("VARCHAR2(8)", min4Max8.getType());
+					string8 = "VARCHAR2(8)";
 				else
-					assertEquals("NVARCHAR2(8)", min4Max8.getType());
+					string8 = "NVARCHAR2(8)";
 			}
+			assertEquals(string8, min4Max8.getType());
 
 			assertCheckConstraint(stringItem, "STRINGITEMS_MIN_4_Ck",     "(LENGTH("+protect("MIN_4")+")>=4) OR ("+protect("MIN_4")+" IS NULL)");
 			assertCheckConstraint(stringItem, "STRINGITEMS_MAX_4_Ck",     "(LENGTH("+protect("MAX_4")+")<=4) OR ("+protect("MAX_4")+" IS NULL)");
