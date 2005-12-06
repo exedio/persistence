ca66ccbfbf41bd54d441b65fbc63520979dba370
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-12-06 14:00:58 +0000

    ConstraintViolationException uses nesting of exceptions introduced in JDK 1.4
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4102 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/ConstraintViolationException.java
+++ lib/src/com/exedio/cope/ConstraintViolationException.java
@@ -18,8 +18,6 @@
 
 package com.exedio.cope;
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 
 /**
@@ -29,32 +27,10 @@ import java.sql.SQLException;
  */
 public abstract class ConstraintViolationException extends Exception
 {
-	private final SQLException cause;
 
 	public ConstraintViolationException(final SQLException cause)
 	{
-		this.cause = cause;
-	}
-	
-	public void printStackTrace()
-	{
-		super.printStackTrace();
-		if(cause!=null)
-			cause.printStackTrace();
-	}
-
-	public void printStackTrace(final PrintStream s)
-	{
-		super.printStackTrace(s);
-		if(cause!=null)
-			cause.printStackTrace(s);
-	}
-
-	public void printStackTrace(final PrintWriter s)
-	{
-		super.printStackTrace(s);
-		if(cause!=null)
-			cause.printStackTrace(s);
+		super(cause);
 	}
 	
 	// force subclasses to implement a getMessage()
