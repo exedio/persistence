56f952e22233d586ce19c3a5c353fb8fbef964fe
Frank Finger <frank.finger@exedio.com>
2005-12-06 09:25:57 +0000

    use jdk1.4 RuntimeException nesting capabilities
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4086 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/NestingRuntimeException.java
+++ lib/src/com/exedio/cope/NestingRuntimeException.java
@@ -31,66 +31,40 @@ import java.io.PrintWriter;
  */
 public final class NestingRuntimeException extends RuntimeException
 {
-	private final Exception cause;
 	private final String message;
 	
 	public NestingRuntimeException(final Exception cause)
 	{
-		this.cause = cause;
+		super( cause );
 		this.message = null;
 	}
 	
 	public NestingRuntimeException(final Exception cause, final String message)
 	{
-		this.cause = cause;
+		super( message, cause );
 		this.message = message;
 	}
 	
 	public Exception getNestedCause()
 	{
-		return cause;
+		return (Exception)super.getCause();
 	}
 	
 	public String getMessage()
 	{
 		if(message!=null)
 		{
-			if(cause!=null)
-				return message + ":" + cause.getMessage();
+			if(super.getCause()!=null)
+				return message + ":" + super.getCause().getMessage();
 			else
 				return message;
 		}
 		else
 		{
-			if(cause!=null)
-				return cause.getMessage();
+			if(super.getCause()!=null)
+				return super.getCause().getMessage();
 			else
 				return "";
 		}
 	}
-	
-	public void printStackTrace()
-	{
-		super.printStackTrace();
-		if(message!=null)
-			System.out.println(message);
-		cause.printStackTrace();
-	}
-
-	public void printStackTrace(final PrintStream s)
-	{
-		super.printStackTrace(s);
-		if(message!=null)
-			s.println(message);
-		cause.printStackTrace(s);
-	}
-
-	public void printStackTrace(final PrintWriter s)
-	{
-		super.printStackTrace(s);
-		if(message!=null)
-			s.println(message);
-		cause.printStackTrace(s);
-	}
-
 }
