07df8c5814b2225dba219cd0921a25152913616c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-12-06 10:00:02 +0000

    remove workaround for JDK1.3 jdbc drivers
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4088 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- lib/src/com/exedio/cope/TimestampColumn.java
+++ lib/src/com/exedio/cope/TimestampColumn.java
@@ -43,54 +43,12 @@ final class TimestampColumn extends Column
 		return null;
 	}
 	
-	private static final boolean skipWorkAround =
-		!System.getProperty("java.runtime.version").startsWith("1.3");
-
-	/**
-	 * Returns what should be returned by
-	 * {@link Date#getTime()}.
-	 * For both oracle and hsqldb the dates returned by ResultSets
-	 * return zero milliseconds with getTime().
-	 * 
-	 * This method provides a workaround.
-	 * 
-	 * Probably has to be adjusted for different jdbc drivers
-	 * and jdk versions. 
-	 */
-	private static final long getTime(final Date date)
-	{
-		//System.out.println("TimeColumn.getTime");
-		
-		long result = date.getTime();
-		
-		if(skipWorkAround)
-			return result;
-	
-		//System.out.println("          result "+result+" before");
-		if((result%1000)!=0)
-			throw new RuntimeException(String.valueOf(result));
-		
-		//final Calendar cal = Calendar.getInstance();
-		//cal.setTime(date);
-		//System.out.println("          cal "+cal);
-		
-		final String dateString = date.toString();
-		//System.out.println("          toString "+dateString);
-		final int dotPos = dateString.lastIndexOf('.');
-		final String millisString = dateString.substring(dotPos+1);
-		final int millis = Integer.parseInt(millisString);
-		result += millis;
-		
-		//System.out.println("          result "+result);
-		return result;
-	}
-	
 	final void load(final ResultSet resultSet, final int columnIndex, final Row row)
 			throws SQLException
 	{
 		final Object loadedTimestamp = resultSet.getObject(columnIndex);
 		//System.out.println("TimestampColumn.load "+columnIndex+" "+loadedTimestamp);
-		row.put(this, (loadedTimestamp!=null) ? new Long(getTime(((Date)loadedTimestamp))) : null);
+		row.put(this, (loadedTimestamp!=null) ? new Long(((Date)loadedTimestamp).getTime()) : null);
 	}
 	
 	final String cacheToDatabase(final Object cache)
