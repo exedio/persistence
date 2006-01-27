98fe77674e3ebfaf7dab65b37ddb119cb9c6d156
Frank Finger <frank.finger@exedio.com>
2006-01-27 10:18:20 +0000

    better error message
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4704 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/AbstractDatabase.java
+++ runtime/src/com/exedio/cope/AbstractDatabase.java
@@ -411,6 +411,10 @@ abstract class AbstractDatabase implements Database
 				final boolean[] orderAscending = query.orderAscending;
 				for(int i = 0; i<orderBy.length; i++)
 				{
+					if ( orderBy[i]==null )
+					{
+						throw new NullPointerException( "orderBy contains null at index "+i );
+					}
 					bf.appendOrderBy().
 						append(orderBy[i], (Join)null);
 					if(!orderAscending[i])
