b552754765308b387a079b520feac00abca299ca
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-03-08 20:17:40 +0000

    remove unused imports
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4862 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/pattern/Media.java
+++ runtime/servletsrc/com/exedio/cope/pattern/Media.java
@@ -26,7 +26,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
