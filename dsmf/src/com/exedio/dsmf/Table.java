50915f8e1435cee652b9ee349e6a071bf19b6a1b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-03-08 10:18:21 +0000

    remove unused imports
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4840 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- dsmf/src/com/exedio/dsmf/Table.java
+++ dsmf/src/com/exedio/dsmf/Table.java
@@ -22,9 +22,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
-
-
 
 public final class Table extends Node
 {
