fb23e9a8c523e9140ede305cda6d4945bbd11c0b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-03-09 15:22:54 +0000

    remove unused import
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4870 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- console/src/com/exedio/cope/console/PropertiesCop.java
+++ console/src/com/exedio/cope/console/PropertiesCop.java
@@ -23,7 +23,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 
