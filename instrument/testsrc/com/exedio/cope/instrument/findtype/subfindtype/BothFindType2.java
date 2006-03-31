b38a6001197f6736750fca63225ac6c800b3d747
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-03-31 09:01:31 +0000

    avoid warnings because of empty classes
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4975 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- instrument/testsrc/com/exedio/cope/instrument/findtype/subfindtype/BothFindType2.java
+++ instrument/testsrc/com/exedio/cope/instrument/findtype/subfindtype/BothFindType2.java
@@ -15,9 +15,10 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 package com.exedio.cope.instrument.findtype.subfindtype;
 
 public class BothFindType2
 {
-
+	// just a class for testing imports
 }
