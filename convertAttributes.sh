dd029a1bc6cc612d80eaa97a21fe34e09c30a109
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-05-02 08:53:39 +0000

    fix if classes are fully qualified with package
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@5323 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- convertAttributes.sh
+++ convertAttributes.sh
@@ -1,7 +1,7 @@
 for i in `find . -name *.java`
 do
 	#echo $i
-	sed -e 's/new *\(Enum\|Item\)Attribute *< *\([0-9,A-Z,a-z]*\) *>\( *\)(\( *\)\([A-Z,_]*\)\( *\(, *[A-Z]* *\)\?\))/new\1Attribute\3(\4\5, \2.class\6)/g' \
+	sed -e 's/new *\(com.exedio.cope.\)\?\(Enum\|Item\)Attribute *< *\([0-9,A-Z,a-z,.]*\) *>\( *\)(\( *\)\([A-Z,_]*\)\( *\(, *[A-Z]* *\)\?\))/new\2Attribute\4(\5\6, \3.class\7)/g' \
 		< $i \
 		> $i.sed
 	diff $i $i.sed
