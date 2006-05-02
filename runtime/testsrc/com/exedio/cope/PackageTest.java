f3cc2a3ef6614fa01287c7c22d6dec24f6131c57
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-05-02 13:52:24 +0000

    remove DeleteRelationTest, obsoleted by r5326
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@5328 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/PackageTest.java
+++ runtime/testsrc/com/exedio/cope/PackageTest.java
@@ -48,8 +48,6 @@ public class PackageTest extends TestCase
 		suite.addTestSuite( AttributeQualifiedTest.class );
 		
 		suite.addTestSuite( DeleteTest.class );
-		// TODO SOON remove
-		suite.addTestSuite( DeleteRelationTest.class );
 		suite.addTestSuite( DeleteHierarchyTest.class );
 		suite.addTestSuite( NameTest.class );
 		suite.addTestSuite( StringTest.class );
