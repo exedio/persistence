f86606f4c42c9b72223a9ea7eb439157ccf15f1a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-05-31 09:20:15 +0000

    source format
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@5715 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/IntegerColumn.java
+++ runtime/src/com/exedio/cope/IntegerColumn.java
@@ -30,8 +30,9 @@ class IntegerColumn extends Column
 	final boolean longInsteadOfInt;
 	final int[] allowedValues;
 
-	IntegerColumn(final Table table, final String id, 
-					  final boolean optional, final int precision,
+	IntegerColumn(final Table table, final String id,
+					  final boolean optional,
+					  final int precision,
 					  final boolean longInsteadOfInt, final int[] allowedValues)
 	{
 		super(table, id, false, optional, longInsteadOfInt ? JDBC_TYPE_LONG : JDBC_TYPE_INT);
