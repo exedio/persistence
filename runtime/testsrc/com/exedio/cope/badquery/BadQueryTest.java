eec5676e36c57d434b7fb1df2c8e76a32f7f3443
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-06-14 13:22:47 +0000

    fix identation (2)
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@5880 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/badquery/BadQueryTest.java
+++ runtime/testsrc/com/exedio/cope/badquery/BadQueryTest.java
@@ -45,7 +45,8 @@ public class BadQueryTest extends AbstractLibTest
 		{
 			// with specifying join
 			final Query<QueryItem> query = QueryItem.TYPE.newQuery(null);
-			final Join superJoin = query.join(SuperContainer.TYPE); superJoin.setCondition(new JoinedItemFunction<QueryItem>(SuperContainer.queryItem, superJoin).equalTarget());
+			final Join superJoin = query.join(SuperContainer.TYPE);
+			superJoin.setCondition(new JoinedItemFunction<QueryItem>(SuperContainer.queryItem, superJoin).equalTarget());
 			query.join(SubContainer.TYPE, SubContainer.superContainer.equalTarget(superJoin));
 			query.setCondition(new JoinedItemFunction<SuperContainer>(SuperContainer.TYPE.getThis(), superJoin).typeNotIn(SubContainer.TYPE));
 			assertContains(query.search());
@@ -54,7 +55,8 @@ public class BadQueryTest extends AbstractLibTest
 		{
 			// without specifying join
 			final Query<QueryItem> query = QueryItem.TYPE.newQuery(null);
-			final Join superJoin = query.join(SuperContainer.TYPE); superJoin.setCondition(new JoinedItemFunction<QueryItem>(SuperContainer.queryItem, superJoin).equalTarget());
+			final Join superJoin = query.join(SuperContainer.TYPE);
+			superJoin.setCondition(new JoinedItemFunction<QueryItem>(SuperContainer.queryItem, superJoin).equalTarget());
 			query.join(SubContainer.TYPE, SubContainer.superContainer.equalTarget(superJoin));
 			query.setCondition(SuperContainer.TYPE.getThis().typeNotIn(SubContainer.TYPE));
 			try
