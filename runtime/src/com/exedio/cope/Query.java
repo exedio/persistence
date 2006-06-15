exedio/persistence

commit db9095957abb357b5d45c30c739eb25a8155cb6d

support order by with multiple functions
git-svn-id: https://svn.exedio.com/cope/trunk/persistence@4682 e7d4fc99-c606-0410-b9bf-843393a9eab7

runtime/src/com/exedio/cope/Query.java
@@ -35,9 +35,9 @@ public final class Query
 	ArrayList joins = null;
 	Condition condition;
 
-	Function orderBy = null;
+	Function[] orderBy = null;
+	boolean[] orderAscending;
 	
-	boolean orderAscending;
 	boolean deterministicOrder = false;
 
 	int limitStart = 0;
@@ -135,6 +135,18 @@ public final class Query
 	
 	public void setOrderBy(final Function orderBy, final boolean ascending)
 	{
+		this.orderBy = new Function[]{orderBy};
+		this.orderAscending = new boolean[]{ascending};
+	}
+	
+	/**
+	 * @throws RuntimeException if <code>orderBy.length!=ascending.length</code>
+	 */
+	public void setOrderBy(final Function[] orderBy, final boolean[] ascending)
+	{
+		if(orderBy.length!=ascending.length)
+			throw new RuntimeException("orderBy and ascending must have same length, but was "+orderBy.length+" and "+ascending.length);
+		
 		this.orderBy = orderBy;
 		this.orderAscending = ascending;
 	}
@@ -389,11 +401,16 @@ public final class Query
 
 		if(orderBy!=null)
 		{
-			bf.append(" order by ").
-				append(orderBy);
-			
-			if(orderAscending)
-				bf.append(" desc");
+			bf.append(" order by ");
+			for(int i = 0; i<orderBy.length; i++)
+			{
+				if(i>0)
+					bf.append(", ");
+				
+				bf.append(orderBy[i]);
+				if(!orderAscending[i])
+					bf.append(" desc");
+			}
 		}
 		
 		if(deterministicOrder)
@@ -418,9 +435,9 @@ public final class Query
 		final ArrayList joins;
 		final Condition condition;
 
-		final Function orderBy;
+		final Function[] orderBy;
 
-		final boolean orderAscending;
+		final boolean[] orderAscending;
 		final boolean deterministicOrder;
 
 		final int limitStart;
@@ -481,7 +498,7 @@ public final class Query
 			return b ? 1 : 0;
 		}
 		
-		private static int hashCode( Selectable[] selectables )
+		private static int hashCode( Object[] selectables )
 		{
 			if ( selectables==null )
 			{
@@ -506,7 +523,7 @@ public final class Query
 					^ hashCode(joins)
 					^ hashCode(condition)
 					^ hashCode(orderBy)
-					^ hashCode(orderAscending)
+					^ Arrays.hashCode(orderAscending)
 					^ hashCode(deterministicOrder)
 					^ limitStart
 					^ limitCount

runtime/src/com/exedio/cope/AbstractDatabase.java
@@ -402,24 +402,23 @@ abstract class AbstractDatabase implements Database
 		
 		if(!doCountOnly)
 		{
-			boolean firstOrderBy = true;		
-			if(query.orderBy!=null || query.deterministicOrder)
-				bf.append(" order by ");
-	
-			if(query.orderBy!=null)
+			final Function[] orderBy = query.orderBy;
+			
+			if(orderBy!=null)
 			{
-				firstOrderBy = false;
-	
-				bf.append(query.orderBy, (Join)null);
-				if(!query.orderAscending)
-					bf.append(" desc");
+				final boolean[] orderAscending = query.orderAscending;
+				for(int i = 0; i<orderBy.length; i++)
+				{
+					bf.appendOrderBy().
+						append(orderBy[i], (Join)null);
+					if(!orderAscending[i])
+						bf.append(" desc");
+				}
 			}
 			
-			if(query.deterministicOrder) // TODO skip this, if orderBy already orders by some unique condition
+			if(query.deterministicOrder) // TODO skip this, if orderBy already orders by some unique function
 			{
-				if(!firstOrderBy)
-					bf.append(',');
-				
+				bf.appendOrderBy();
 				query.type.getPkSource().appendDeterministicOrderByExpression(bf, query.type);
 			}
 

runtime/src/com/exedio/cope/Statement.java
@@ -134,6 +134,21 @@ final class Statement
 		return this;
 	}
 	
+	private boolean firstOrderBy = true;
+	
+	Statement appendOrderBy()
+	{
+		if(firstOrderBy)
+		{
+			text.append(" order by ");
+			firstOrderBy = false;
+		}
+		else
+			text.append(',');
+		
+		return this;
+	}
+		
 	Statement append(final Function function, final Join join)
 	{
 		function.append(this, join);

runtime/testsrc/com/exedio/cope/OrderByTest.java
@@ -56,6 +56,7 @@ public class OrderByTest extends TestmodelTest
 			final Query query = new Query(item1.TYPE, null);
 			query.setDeterministicOrder(true);
 			assertEquals(list(item1, item2, item3, item4, item5), query.search());
+			assertEquals("select AttributeItem.PK from AttributeItem order deterministically", query.toString());
 		}
 		
 		// simple order
@@ -63,6 +64,31 @@ public class OrderByTest extends TestmodelTest
 		assertOrder(list(item1, item5, item2, item4, item3), item.someNotNullInteger);
 		assertOrder(list(item1, item5, item2, item4, item3), item.someNotNullInteger);
 		assertOrder(list(item1, item3, item5, item2, item4), item.someNotNullDouble);
+		
+		// order with multiple functions
+		{
+			final Query query = new Query(item1.TYPE, null);
+			query.setOrderBy(new Function[]{item1.someNotNullBoolean,item1.someNotNullInteger}, new boolean[]{true, true});
+			assertEquals(list(item5, item4, item3, item1, item2), query.search());
+			assertEquals("select AttributeItem.PK from AttributeItem order by AttributeItem#someNotNullBoolean, AttributeItem#someNotNullInteger", query.toString());
+			
+			query.setOrderBy(new Function[]{item1.someNotNullBoolean,item1.someNotNullInteger}, new boolean[]{false, true});
+			assertEquals(list(item1, item2, item5, item4, item3), query.search());
+			assertEquals("select AttributeItem.PK from AttributeItem order by AttributeItem#someNotNullBoolean desc, AttributeItem#someNotNullInteger", query.toString());
+			
+			query.setOrderBy(new Function[]{item1.someNotNullEnum,item1.someNotNullString}, new boolean[]{true, true});
+			assertEquals(list(item1, item4, item2, item5, item3), query.search());
+			
+			try
+			{
+				query.setOrderBy(new Function[]{item1.someNotNullBoolean,item1.someNotNullInteger}, new boolean[]{true});
+				fail();
+			}
+			catch(RuntimeException e)
+			{
+				assertEquals("orderBy and ascending must have same length, but was 2 and 1", e.getMessage());
+			}
+		}
 
 		// limit
 		{

commit e479a922ee2973d55ec650d3c19d9b96c4d7256d

fix bug, which disabled query caching for all queries using order by, bug introduced in r4682: support order by with multiple functions
git-svn-id: https://svn.exedio.com/cope/trunk/persistence@5909 e7d4fc99-c606-0410-b9bf-843393a9eab7

runtime/src/com/exedio/cope/Query.java
@@ -541,15 +541,15 @@ public final class Query<R>
 			{
 				throw new NullPointerException( "must not compare Query.Key to null" );
 			}
-			Key other = (Key)obj;
+			final Key other = (Key)obj;
 			return equals( model, other.model )
 				&& Arrays.equals( selects, other.selects )
 				&& distinct == other.distinct
 				&& equals( type, other.type )
 				&& equals( joins, other.joins )
 				&& equals( condition, other.condition )
-				&& equals( orderBy, other.orderBy )
-				&& orderAscending==other.orderAscending
+				&& Arrays.equals( orderBy, other.orderBy )
+				&& Arrays.equals( orderAscending, other.orderAscending )
 				&& limitStart == other.limitStart
 				&& limitCount == other.limitCount
 				&& makeStatementInfo == other.makeStatementInfo;
@@ -619,6 +619,12 @@ public final class Query<R>
 					^ limitStart
 					^ limitCount
 					^ hashCode(makeStatementInfo);
-		}		
+		}
+		
+		@Override
+		public String toString()
+		{
+			return name!=null ? name : super.toString();
+		}
 	}
 }