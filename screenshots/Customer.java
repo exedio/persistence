72bfea005b0158b81dc9201f274bbd5ae32d533a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-07-27 10:30:11 +0000

    make definition.png fit into 640x480
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@6250 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- screenshots/Customer.java
+++ screenshots/Customer.java
@@ -1,21 +1,21 @@
 import com.exedio.cope.*;
-import com.exedio.cope.StringAttribute;
-import com.exedio.cope.pattern.MD5Hash;
+import com.exedio.cope.pattern.*;
 
 /**
- * Defines a persistent class customer within your application.
+ * Defines a persistent class customer within
+ * your application.
  */
 public class Customer extends Item
 {
-	/**
-	 * The unique email address of the customer.
-	 */
-	public static final StringAttribute email =
-		new StringAttribute(UNIQUE);
-	
-	/**
-	 * The password of the customer, transparently md5-encoded.
-	 */
-	public static final MD5Hash password = new MD5Hash();
+   /**
+    * The unique email address of the customer.
+    */
+   public static final StringAttribute email =
+       new StringAttribute(UNIQUE);
 
+   /**
+    * The password of the customer, transparently
+    * md5-encoded.
+    */
+   public static final MD5Hash password = new MD5Hash();
 }
