aae482ca4d3fe5b58797c7bd70ac7f41a4c05b84
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-10-09 11:37:58 +0000

    rename attribute to field
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@6482 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/UniqueConstraint.java
+++ runtime/src/com/exedio/cope/UniqueConstraint.java
@@ -153,10 +153,10 @@ public final class UniqueConstraint extends Feature
 		if(fields.size()!=values.length)
 			throw new RuntimeException("-"+fields.size()+'-'+values.length);
 
-		final Iterator<FunctionField<?>> attributeIterator = fields.iterator();
+		final Iterator<FunctionField<?>> fieldIter = fields.iterator();
 		final Condition[] conditions = new Condition[fields.size()];
-		for(int j = 0; attributeIterator.hasNext(); j++)
-			conditions[j] = Cope.equalAndCast(attributeIterator.next(), values[j]);
+		for(int j = 0; fieldIter.hasNext(); j++)
+			conditions[j] = Cope.equalAndCast(fieldIter.next(), values[j]);
 
 		return getType().searchSingleton(new CompositeCondition(CompositeCondition.Operator.AND, conditions));
 	}
