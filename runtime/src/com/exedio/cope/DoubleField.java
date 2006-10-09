65539c7c38589b57ca8ab130ae8bc2d72528aec3
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-10-09 10:05:55 +0000

    rename attribute to field
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@6475 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/DoubleField.java
+++ runtime/src/com/exedio/cope/DoubleField.java
@@ -69,12 +69,12 @@ public final class DoubleField extends FunctionField<Double>
 	}
 	
 	/**
-	 * @throws RuntimeException if this attribute is not {@link #isMandatory() mandatory}.
+	 * @throws RuntimeException if this field is not {@link #isMandatory() mandatory}.
 	 */
 	public final double getMandatory(final Item item)
 	{
 		if(optional)
-			throw new RuntimeException("attribute " + toString() + " is not mandatory");
+			throw new RuntimeException("field " + toString() + " is not mandatory");
 		
 		return get(item).doubleValue();
 	}
