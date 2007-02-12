exedio/persistence

commit 9bccc84c8e67c22685fd12d730614d994fd9068f

introduce Field#optional(), further step towards deprecating Field#Option
git-svn-id: https://svn.exedio.com/cope/trunk/persistence@7521 e7d4fc99-c606-0410-b9bf-843393a9eab7

runtime/patternsrc/com/exedio/cope/pattern/MD5Hash.java
@@ -45,6 +45,10 @@ public final class MD5Hash extends JavaSecurityHash
 		this(Item.MANDATORY);
 	}
 	
+	/**
+	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 */
+	@Deprecated
 	public MD5Hash(final Option storageOption)
 	{
 		this(new StringField(storageOption).lengthExact(LENGTH));
@@ -60,4 +64,9 @@ public final class MD5Hash extends JavaSecurityHash
 		return result;
 	}
 	
+	@Override
+	public MD5Hash optional()
+	{
+		return new MD5Hash(getStorage().optional(), getEncoding());
+	}
 }

runtime/src/com/exedio/cope/DataField.java
@@ -46,6 +46,10 @@ public final class DataField extends Field<byte[]>
 		this(false, false, DEFAULT_LENGTH);
 	}
 	
+	/**
+	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 */
+	@Deprecated
 	public DataField(final Option option)
 	{
 		this(option.isFinal, option.optional, DEFAULT_LENGTH);
@@ -58,6 +62,12 @@ public final class DataField extends Field<byte[]>
 	public DataField toFinal()
 	{
 		return new DataField(true, optional, maximumLength);
+	}
+	
+	@Override
+	public DataField optional()
+	{
+		return new DataField(isfinal, true, maximumLength);
 	}
 	
 	public DataField lengthMax(final long maximumLength)

runtime/src/com/exedio/cope/Item.java
@@ -553,6 +553,10 @@ public abstract class Item extends Cope
 		return newEnumField(option, valueClass);
 	}
 	
+	/**
+	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 */
+	@Deprecated
 	@SuppressWarnings("deprecation") // OK: constructor is deprecated for public use only
 	public static final <E extends Enum<E>> EnumField<E> newEnumField(final Option option, final Class<E> valueClass)
 	{
@@ -583,6 +587,10 @@ public abstract class Item extends Cope
 		return newItemField(option, valueClass);
 	}
 	
+	/**
+	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 */
+	@Deprecated
 	@SuppressWarnings("deprecation") // OK: constructor is deprecated for public use only
 	public static final <E extends Item> ItemField<E> newItemField(final Option option, final Class<E> valueClass)
 	{
@@ -613,6 +621,10 @@ public abstract class Item extends Cope
 		return newItemField(option, valueClass, policy);
 	}
 	
+	/**
+	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 */
+	@Deprecated
 	@SuppressWarnings("deprecation") // OK: constructor is deprecated for public use only
 	public static final <E extends Item> ItemField<E> newItemField(final Option option, final Class<E> valueClass, final DeletePolicy policy)
 	{

runtime/src/com/exedio/cope/Type.java
@@ -815,9 +815,13 @@ public final class Type<C extends Item>
 	
 	public ItemField<C> newItemField(final DeletePolicy policy)
 	{
-		return new ItemField<C>(Item.MANDATORY, this, policy);
+		return new ItemField<C>(this, policy);
 	}
 	
+	/**
+	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 */
+	@Deprecated
 	public ItemField<C> newItemField(final Option option, final DeletePolicy policy)
 	{
 		return new ItemField<C>(option, this, policy);

commit 4709ad48188815d92378f83e4587e79c51d2b955

fix javadoc links broken in r7521: introduce Field#optional()
git-svn-id: https://svn.exedio.com/cope/trunk/persistence@7524 e7d4fc99-c606-0410-b9bf-843393a9eab7

runtime/patternsrc/com/exedio/cope/pattern/MD5Hash.java
@@ -46,7 +46,7 @@ public final class MD5Hash extends JavaSecurityHash
 	}
 	
 	/**
-	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 * @deprecated use {@link com.exedio.cope.Field#toFinal()}, {@link com.exedio.cope.FunctionField#unique()} and {@link com.exedio.cope.Field#optional()} instead. 
 	 */
 	@Deprecated
 	public MD5Hash(final Option storageOption)

runtime/src/com/exedio/cope/DataField.java
@@ -47,7 +47,7 @@ public final class DataField extends Field<byte[]>
 	}
 	
 	/**
-	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 * @deprecated use {@link #toFinal()} and {@link #optional()} instead. 
 	 */
 	@Deprecated
 	public DataField(final Option option)

runtime/src/com/exedio/cope/Item.java
@@ -554,7 +554,7 @@ public abstract class Item extends Cope
 	}
 	
 	/**
-	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 * @deprecated use {@link Field#toFinal()}, {@link FunctionField#unique()} and {@link Field#optional()} instead. 
 	 */
 	@Deprecated
 	@SuppressWarnings("deprecation") // OK: constructor is deprecated for public use only
@@ -588,7 +588,7 @@ public abstract class Item extends Cope
 	}
 	
 	/**
-	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 * @deprecated use {@link Field#toFinal()}, {@link FunctionField#unique()} and {@link Field#optional()} instead. 
 	 */
 	@Deprecated
 	@SuppressWarnings("deprecation") // OK: constructor is deprecated for public use only
@@ -622,7 +622,7 @@ public abstract class Item extends Cope
 	}
 	
 	/**
-	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 * @deprecated use {@link Field#toFinal()}, {@link FunctionField#unique()} and {@link Field#optional()} instead. 
 	 */
 	@Deprecated
 	@SuppressWarnings("deprecation") // OK: constructor is deprecated for public use only

runtime/src/com/exedio/cope/Type.java
@@ -819,7 +819,7 @@ public final class Type<C extends Item>
 	}
 	
 	/**
-	 * @deprecated use {@link #toFinal()}, {@link #unique()} and {@link #optional()} instead. 
+	 * @deprecated use {@link Field#toFinal()}, {@link FunctionField#unique()} and {@link Field#optional()} instead. 
 	 */
 	@Deprecated
 	public ItemField<C> newItemField(final Option option, final DeletePolicy policy)