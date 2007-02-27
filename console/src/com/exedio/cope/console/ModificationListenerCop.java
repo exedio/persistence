edbd677f6c2ce866702e4fe3f1904aa53a1ca38a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2007-02-27 18:19:44 +0000

    add instrospection listener
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@7631 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- console/src/com/exedio/cope/console/ModificationListenerCop.java
+++ console/src/com/exedio/cope/console/ModificationListenerCop.java
@@ -19,16 +19,22 @@
 package com.exedio.cope.console;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import com.exedio.cope.Item;
 import com.exedio.cope.Model;
 import com.exedio.cope.util.ModificationListener;
 
 final class ModificationListenerCop extends ConsoleCop
 {
+	static final String ADD_INTROSPECTOR = "addIntrospector";
 	static final String REMOVE_SELECTED = "removeSelected";
 	static final String REMOVE_CHECKBOX = "rm";
 
@@ -57,6 +63,10 @@ final class ModificationListenerCop extends ConsoleCop
 				}
 			}
 		}
+		if("POST".equals(request.getMethod()) && (request.getParameter(ADD_INTROSPECTOR)!=null))
+		{
+			model.addModificationListener(new Introspector(request.getSession()));
+		}
 		
 		ModificationListener_Jspm.writeBody(this, out,
 				model.getModificationListenersRemoved(),
@@ -65,7 +75,7 @@ final class ModificationListenerCop extends ConsoleCop
 
 		/*model.addModificationListener(new ModificationListener()
 		{
-			int count = debugNumber++;
+			private int count = debugNumber++;
 			
 			public void onModifyingCommit(final Collection<Item> modifiedItems)
 			{
@@ -80,7 +90,7 @@ final class ModificationListenerCop extends ConsoleCop
 		});
 		model.addModificationListener(new ModificationListener()
 		{
-			int count = debugNumber++;
+			private int count = debugNumber++;
 
 			public void onModifyingCommit(final Collection<Item> modifiedItems)
 			{
@@ -98,5 +108,90 @@ final class ModificationListenerCop extends ConsoleCop
 	final String toID(final ModificationListener listener)
 	{
 		return listener.getClass().getName() + '@' + System.identityHashCode(listener);
+	}
+	
+	static final class Introspector implements ModificationListener
+	{
+		private static final String SESSION_KEY = "com.exedio.cope.console.introspector";
+		private final long start = System.currentTimeMillis();
+		private final HttpSession session;
+		private final ArrayList<Commit> commits = new ArrayList<Commit>();
+		
+		Introspector(final HttpSession session)
+		{
+			this.session = session;
+			session.setAttribute(SESSION_KEY, this);
+		}
+		
+		public void onModifyingCommit(final Collection<Item> modifiedItems)
+		{
+			final Commit commit = new Commit(modifiedItems);
+			synchronized(commits)
+			{
+				commits.add(commit);
+			}
+		}
+		
+		Date getStart()
+		{
+			return new Date(start);
+		}
+		
+		String getSessionId()
+		{
+			return session.getId();
+		}
+		
+		boolean isSessionAttached()
+		{
+			try
+			{
+				return session.getAttribute(SESSION_KEY)==this;
+			}
+			catch(RuntimeException e)
+			{
+				return false;
+			}
+		}
+		
+		ArrayList<Commit> getCommits()
+		{
+			synchronized(commits)
+			{
+				return new ArrayList<Commit>(commits); // prevent ConcurrentModificationException
+			}
+		}
+	}
+	
+	static final class Commit
+	{
+		private final long timestamp = System.currentTimeMillis();
+		private final String modifiedItems;
+		
+		Commit(final Collection<Item> modifiedItems)
+		{
+			final StringBuffer bf = new StringBuffer();
+			boolean first = true;
+			for(final Item item : modifiedItems)
+			{
+				if(first)
+					first = false;
+				else
+					bf.append(',').append(' ');
+				
+				bf.append(item.getCopeID());
+			}
+			this.modifiedItems = bf.toString();
+		}
+		
+		Date getTimeStamp()
+		{
+			return new Date(timestamp);
+		}
+		
+		String getModifiedItems()
+		{
+			return modifiedItems;
+		}
 	}
 }
