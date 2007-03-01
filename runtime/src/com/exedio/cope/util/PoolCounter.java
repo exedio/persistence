97f9ec19434c2a091fa4586100f15870583535ab
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2007-03-01 06:37:27 +0000

    default constructor for PoolCounter with typical sizes
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@7669 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/util/PoolCounter.java
+++ runtime/src/com/exedio/cope/util/PoolCounter.java
@@ -45,6 +45,11 @@ public final class PoolCounter
 	private int get = 0;
 	private int put = 0;
 
+	public PoolCounter()
+	{
+		this(1,2,4,6,8,10,15,20,25,30,40,50,60,70,80,90,100);
+	}
+	
 	public PoolCounter(final int... idleLimits)
 	{
 		if(idleLimits.length<1)
