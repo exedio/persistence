175f03105b51a66da7074a3f11b2c9e41da8645b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2007-04-15 20:28:52 +0000

    remove feature checkAll, not needed anymore
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@7853 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- console/src/com/exedio/cope/console/schema.js
+++ console/src/com/exedio/cope/console/schema.js
@@ -1,17 +1,3 @@
-function checkAll(checkboxName)
-{
-	var checkboxes = document.body.getElementsByTagName("input");
-	for(j=0; j<checkboxes.length; j++)
-	{
-		var checkbox = checkboxes[j];
-		if(checkbox.type=="checkbox" &&
-			checkbox.name==checkboxName)
-		{
-			checkbox.checked = true;
-		}
-	}
-}
-
 function togSch(switchElement)
 {
 	var bodyElement = document.getElementById("schBdy");
