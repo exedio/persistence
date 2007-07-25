26ced15955f9f07fadaeaed3b364f7bb53f59633
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2007-07-25 08:32:27 +0000

    test for broken toString of query with unbound view function
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@8161 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/JoinMultipleTest.java
+++ runtime/testsrc/com/exedio/cope/JoinMultipleTest.java
@@ -88,6 +88,8 @@ public class JoinMultipleTest extends TestmodelTest
 		{
 			final Query query = source.TYPE.newQuery(null);
 			final Join join1 = query.join(target1.TYPE);
+			query.setCondition((target1.num1.plus(target1.num2)).greater(2));
+			assertEquals("select this from PointerItem join PointerTargetItem p1 where null>'2'", query.toString()); // TODO this is a bug
 			query.setCondition((target1.num1.plus(target1.num2)).bind(join1).greater(2));
 			assertEquals("select this from PointerItem join PointerTargetItem p1 where p1.null>'2'", query.toString()); // TODO this is a bug
 		}
