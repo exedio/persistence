f67d50adb5e8fd3ff631e390f298473f067c715e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2007-10-22 16:47:50 +0000

    format source
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@8965 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/TimestampColumn.java
+++ runtime/src/com/exedio/cope/TimestampColumn.java
@@ -15,6 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 package com.exedio.cope;
 
 import java.sql.ResultSet;
@@ -81,5 +82,4 @@ final class TimestampColumn extends Column
 	{
 		return Long.valueOf(System.currentTimeMillis());
 	}
-	
 }
