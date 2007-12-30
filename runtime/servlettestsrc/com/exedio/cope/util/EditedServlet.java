fec3c489a019e982d7acf7a9fbd75c42ab248198
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2007-12-30 13:38:01 +0000

    add test environment for content editor
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@9380 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/servlettestsrc/com/exedio/cope/util/EditedServlet.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2004-2007  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.util;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.exedio.cope.Model;
+
+public final class EditedServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1l;
+	
+	static final String ENCODING = "utf-8";
+	
+	public static final Model model = new Model(EditedItem.TYPE);
+	
+	private ConnectToken connectToken = null;
+	
+	@Override
+	public void init() throws ServletException
+	{
+		super.init();
+		
+		connectToken = ServletUtil.connect(model, getServletContext(), "EditedServlet#init");
+		model.createDatabase();
+		try
+		{
+			model.startTransaction("create sample data");
+			createItem(0);
+			createItem(1);
+			createItem(2);
+			model.commit();
+		}
+		finally
+		{
+			model.rollbackIfNotCommitted();
+		}
+	}
+	
+	private static final EditedItem createItem(final int number)
+	{
+		final EditedItem item1 = new EditedItem("item" + number);
+		item1.setMap(1, "item" + number + "map1");
+		item1.setMap(2, "item" + number + "map2");
+		item1.setMap(3, "item" + number + "map3Block");
+		return item1;
+	}
+
+	@Override
+	public void destroy()
+	{
+		model.dropDatabase();
+		connectToken.returnIt();
+		connectToken = null;
+		super.destroy();
+	}
+	
+	@Override
+	protected void doGet(
+			final HttpServletRequest request,
+			final HttpServletResponse response)
+	throws IOException
+	{
+		request.setCharacterEncoding(ENCODING);
+		response.setContentType("text/html; charset="+ENCODING);
+
+		response.addHeader("Cache-Control", "no-cache");
+		response.addHeader("Cache-Control", "no-store");
+		response.addHeader("Cache-Control", "max-age=0");
+		response.addHeader("Cache-Control", "must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", System.currentTimeMillis());
+		
+		final PrintStream out = new PrintStream(response.getOutputStream(), false, ENCODING);
+		try
+		{
+			model.startTransaction("EditedServlet");
+			EditedServlet_Jspm.write(out, EditedItem.TYPE.search());
+			model.commit();
+			response.setStatus(HttpServletResponse.SC_OK);
+		}
+		finally
+		{
+			model.rollbackIfNotCommitted();
+			out.close();
+		}
+	}
+	
+	@Override
+	protected void doPost(
+			final HttpServletRequest request,
+			final HttpServletResponse response) throws IOException
+	{
+		doGet(request, response);
+	}
+}
