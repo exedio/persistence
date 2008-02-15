ac790f19eacc0f60042bed6c256d253a7bc884f1
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2008-02-15 20:21:27 +0000

    test @CopeSchemaName
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@9662 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/NameTest.java
+++ runtime/testsrc/com/exedio/cope/NameTest.java
@@ -131,6 +131,16 @@ public class NameTest extends AbstractLibTest
 			assertCheckConstraint (nameSub, "NameSubItemX_unique_Ck",    "("+p("unique")+" IS NOT NULL) AND (("+p("unique")+">=-2147483648) AND ("+p("unique")+"<=2147483647))");
 			assertCheckConstraint (nameSub, "NameSubItemX_integer_Ck",   "("+p("integer")+" IS NOT NULL) AND (("+p("integer")+">=-2147483648) AND ("+p("integer")+"<=2147483647))");
 			assertCheckConstraint (nameSub, "NameSubItemX_item_Ck",      "("+p("item")+" IS NOT NULL) AND (("+p("item")+">=0) AND ("+p("item")+"<=2147483647))");
+			
+			assertEquals("uniqueY",  nameSub.getColumn("uniqueY") .getName());
+			assertEquals("integerY", nameSub.getColumn("integerY").getName());
+			assertEquals("itemY",    nameSub.getColumn("itemY")   .getName());
+			assertUniqueConstraint(nameSub, "NameSubItemX_uniqueX_Unq",  "("+p("uniqueY")+")"); // TODO bug, should be NameSubItemX_uniqueY_Unq
+			assertFkConstraint    (nameSub, "NameSubItemX_itemY_Fk",     "itemY", mysqlLower("NameSubItemX"), getPrimaryKeyColumnName(NameSubItem.TYPE));
+			assertUniqueConstraint(nameSub, "NameSubItemX_integerX_Unq", "("+p("integerY")+","+p("itemY")+")"); // TODO bug, should be NameSubItemX_integersY_Unq
+			assertCheckConstraint (nameSub, "NameSubItemX_uniqueY_Ck",   "("+p("uniqueY")+" IS NOT NULL) AND (("+p("uniqueY")+">=-2147483648) AND ("+p("uniqueY")+"<=2147483647))");
+			assertCheckConstraint (nameSub, "NameSubItemX_integerY_Ck",  "("+p("integerY")+" IS NOT NULL) AND (("+p("integerY")+">=-2147483648) AND ("+p("integerY")+"<=2147483647))");
+			assertCheckConstraint (nameSub, "NameSubItemX_itemY_Ck",     "("+p("itemY")+" IS NOT NULL) AND (("+p("itemY")+">=0) AND ("+p("itemY")+"<=2147483647))");
 		}
 		
 		// test persistence
