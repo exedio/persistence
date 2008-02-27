08fd4df0e39ff1d8609dd8cda86aee68659f2d8b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2008-02-27 18:13:51 +0000

    methods for up to 5 contentTypes
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@9765 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/pattern/Media.java
+++ runtime/servletsrc/com/exedio/cope/pattern/Media.java
@@ -116,6 +116,30 @@ public final class Media extends CachedMedia implements Settable<Media.Value>
 		return contentTypes(contentType1, contentType2);
 	}
 	
+	/**
+	 * Creates a new media, that must contain one of the given content types only.
+	 */
+	public Media contentType(final String contentType1, final String contentType2, final String contentType3)
+	{
+		return contentTypes(contentType1, contentType2, contentType3);
+	}
+	
+	/**
+	 * Creates a new media, that must contain one of the given content types only.
+	 */
+	public Media contentType(final String contentType1, final String contentType2, final String contentType3, final String contentType4)
+	{
+		return contentTypes(contentType1, contentType2, contentType3, contentType4);
+	}
+	
+	/**
+	 * Creates a new media, that must contain one of the given content types only.
+	 */
+	public Media contentType(final String contentType1, final String contentType2, final String contentType3, final String contentType4, final String contentType5)
+	{
+		return contentTypes(contentType1, contentType2, contentType3, contentType4, contentType5);
+	}
+	
 	private Media contentTypes(final String... types)
 	{
 		return new Media(optional, body.getMaximumLength(), new EnumContentType(types, optional));
