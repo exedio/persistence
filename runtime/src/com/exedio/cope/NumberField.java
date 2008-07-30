fca9d6a1639475c0f8477a63b17694daeafa12f0
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2008-07-30 09:44:15 +0000

    format java code
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@10706 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/NumberField.java
+++ runtime/src/com/exedio/cope/NumberField.java
@@ -22,7 +22,6 @@ import com.exedio.cope.search.SumAggregate;
 
 public abstract class NumberField<E extends Number> extends FunctionField<E> implements NumberFunction<E>
 {
-
 	protected NumberField(final boolean isfinal, final boolean optional, final boolean unique, final Class<E> valueClass, final E defaultConstant)
 	{
 		super(isfinal, optional, unique, valueClass, defaultConstant);
