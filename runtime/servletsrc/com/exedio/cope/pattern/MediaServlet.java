7210cf4f0289f7dbf3fa479d53cf4a3104474e8f
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2008-11-14 08:19:39 +0000

    inline serveError
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@11338 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/pattern/MediaServlet.java
+++ runtime/servletsrc/com/exedio/cope/pattern/MediaServlet.java
@@ -104,17 +104,8 @@ public final class MediaServlet extends HttpServlet
 		throws IOException
 	{
 		final Media.Log log = serveContent(request, response);
+		
 		log.increment();
-		serveError(response, log);
-		
-		// TODO make 500 error page without stack trace
-	}
-		
-	private final void serveError(
-			final HttpServletResponse response,
-			final Media.Log log)
-		throws IOException
-	{
 		if(log.responseStatus==HttpServletResponse.SC_OK)
 			return;
 		
