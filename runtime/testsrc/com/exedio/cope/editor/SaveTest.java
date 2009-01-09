934ac01758d2f525f7359502c329f45db8ec1830
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2009-01-09 14:01:30 +0000

    implement saving modifications (2)
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@11928 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/testsrc/com/exedio/cope/editor/SaveTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2004-2008  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.editor;
+
+import java.util.LinkedHashMap;
+
+import com.exedio.cope.AbstractRuntimeTest;
+
+public class SaveTest extends AbstractRuntimeTest
+{
+	public SaveTest()
+	{
+		super(DraftTest.MODEL);
+	}
+	
+	DraftedItem i;
+	Draft d;
+	LinkedHashMap<Modification, String> mods;
+	
+	@Override
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		i = deleteOnTearDown(new DraftedItem());
+		d = deleteOnTearDown(new Draft("user", "name", "comment"));
+		i.setString("oldString1");
+		final Modification mod1 = new Modification(DraftedItem.string, i);
+		mods = new LinkedHashMap<Modification, String>();
+		mods.put(mod1, "newString1");
+	}
+	
+	public void testDraft()
+	{
+		assertEquals(list(), d.getItems());
+		d.save(mods);
+		assertEquals(1, d.getItemsCount());
+		final DraftItem di1 = d.getItems().get(0);
+		assertEquals("DraftedItem.string", di1.getFeature());
+		assertEquals(i.getCopeID(), di1.getItem());
+		assertEquals("oldString1", di1.getOldValue());
+		assertEquals("newString1", di1.getNewValue());
+	}
+	
+	public void testLive()
+	{
+		assertEquals("oldString1", i.getString());
+		LiveSite.INSTANCE.save(mods);
+		assertEquals("newString1", i.getString());
+	}
+}
