548829590d3fd7535f33c83638b530d6d2eaa0d7
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2009-03-09 10:51:06 +0000

    test revise
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@12706 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- console/testsrc/com/exedio/cope/console/RevisionLineTest.java
+++ console/testsrc/com/exedio/cope/console/RevisionLineTest.java
@@ -25,6 +25,8 @@ import java.util.HashMap;
 
 import com.exedio.cope.Revision;
 import com.exedio.cope.RevisionInfoCreate;
+import com.exedio.cope.RevisionInfoRevise;
+import com.exedio.cope.RevisionInfoRevise.Body;
 import com.exedio.cope.junit.CopeAssert;
 
 public class RevisionLineTest extends CopeAssert
@@ -78,6 +80,63 @@ public class RevisionLineTest extends CopeAssert
 		assertEquals(0, l.getBody().get(1).getElapsed());
 		assertEquals(-1, l.getRows());
 		assertEquals(-1, l.getElapsed());
+	}
+	
+	public void testRevise()
+	{
+		final RevisionLine l = new RevisionLine(55);
+		assertEquals(55, l.number);
+		assertEquals(null, l.getContent());
+		assertEquals(null, l.getLogString());
+		assertEquals(null, l.getLogProperties());
+		assertEquals(null, l.getDate());
+		assertEquals(0, l.getBodyCount());
+		assertEquals(list(), l.getBody());
+		assertEquals(-1, l.getRows());
+		assertEquals(-1, l.getElapsed());
+		
+		final Revision r = new Revision(55, "comment55", "sql55.1", "sql55.2");
+		l.setRevision(r);
+		assertEquals("comment55", l.getContent());
+		assertEquals(null, l.getLogString());
+		assertEquals(null, l.getLogProperties());
+		assertEquals(null, l.getDate());
+		assertEquals(2, l.getBodyCount());
+		assertEquals("sql55.1", l.getBody().get(0).getSQL());
+		assertEquals(0, l.getBody().get(0).getRows());
+		assertEquals(0, l.getBody().get(0).getElapsed());
+		assertEquals("sql55.2", l.getBody().get(1).getSQL());
+		assertEquals(0, l.getBody().get(1).getRows());
+		assertEquals(0, l.getBody().get(1).getElapsed());
+		assertEquals(-1, l.getRows());
+		assertEquals(-1, l.getElapsed());
+		
+		l.setInfo(new RevisionInfoRevise(55, DATE, Collections.<String, String>emptyMap(), "comment55",
+				new Body("sql55.1", 126, 567),
+				new Body("sql55.2", 127, 568)).toBytes());
+		assertTrue(l.getLogString(), l.getLogString().startsWith("#migrationlogv01\n"));
+		final HashMap<String, String> map = new HashMap<String, String>();
+		map.put("comment", "comment55");
+		map.put("dateUTC", DATE_STRING);
+		map.put("revision", "55");
+		map.put("body0.elapsed", "567");
+		map.put("body0.rows", "126");
+		map.put("body0.sql", "sql55.1");
+		map.put("body1.elapsed", "568");
+		map.put("body1.rows", "127");
+		map.put("body1.sql", "sql55.2");
+		assertEquals("comment55", l.getContent());
+		assertEquals(map, l.getLogProperties());
+		assertEquals(DATE, l.getDate());
+		assertEquals(2, l.getBodyCount());
+		assertEquals("sql55.1", l.getBody().get(0).getSQL());
+		assertEquals(126, l.getBody().get(0).getRows());
+		assertEquals(567, l.getBody().get(0).getElapsed());
+		assertEquals("sql55.2", l.getBody().get(1).getSQL());
+		assertEquals(127, l.getBody().get(1).getRows());
+		assertEquals(568, l.getBody().get(1).getElapsed());
+		assertEquals(253, l.getRows());
+		assertEquals(1135, l.getElapsed());
 	}
 	
 	public void testCreate()
