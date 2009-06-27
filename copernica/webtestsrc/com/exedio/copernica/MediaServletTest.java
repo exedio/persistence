exedio/persistence

commit f5c3ef7d08ed53bf999a66c30928f465a323922b

mediapath gets new method getNamedURL
git-svn-id: https://svn.exedio.com/cope/trunk/persistence@13030 e7d4fc99-c606-0410-b9bf-843393a9eab7

nbproject/project.xml
@@ -33,6 +33,36 @@
                     <location>runtime/patternsrc</location>
                     <encoding>UTF-8</encoding>
                 </source-folder>
+                <source-folder>
+                    <label>runtime/testsrc</label>
+                    <type>java</type>
+                    <location>runtime/testsrc</location>
+                    <encoding>UTF-8</encoding>
+                </source-folder>
+                <source-folder>
+                    <label>runtime/servletsrc</label>
+                    <type>java</type>
+                    <location>runtime/servletsrc</location>
+                    <encoding>UTF-8</encoding>
+                </source-folder>
+                <source-folder>
+                    <label>runtime/servlettestsrc</label>
+                    <type>java</type>
+                    <location>runtime/servlettestsrc</location>
+                    <encoding>UTF-8</encoding>
+                </source-folder>
+                <source-folder>
+                    <label>runtime/servlettestweb</label>
+                    <type>java</type>
+                    <location>runtime/servlettestweb</location>
+                    <encoding>UTF-8</encoding>
+                </source-folder>
+                <source-folder>
+                    <label>runtime/junitsrc</label>
+                    <type>java</type>
+                    <location>runtime/junitsrc</location>
+                    <encoding>UTF-8</encoding>
+                </source-folder>
             </folders>
             <ide-actions>
                 <action name="build">
@@ -66,13 +96,33 @@
                         <label>runtime/patternsrc</label>
                         <location>runtime/patternsrc</location>
                     </source-folder>
+                    <source-folder style="packages">
+                        <label>runtime/testsrc</label>
+                        <location>runtime/testsrc</location>
+                    </source-folder>
+                    <source-folder style="packages">
+                        <label>runtime/servletsrc</label>
+                        <location>runtime/servletsrc</location>
+                    </source-folder>
+                    <source-folder style="packages">
+                        <label>runtime/servlettestsrc</label>
+                        <location>runtime/servlettestsrc</location>
+                    </source-folder>
+                    <source-folder style="packages">
+                        <label>runtime/servlettestweb</label>
+                        <location>runtime/servlettestweb</location>
+                    </source-folder>
+                    <source-folder style="packages">
+                        <label>runtime/junitsrc</label>
+                        <location>runtime/junitsrc</location>
+                    </source-folder>
                     <source-file>
                         <location>build.xml</location>
                     </source-file>
-		    <source-file>
+                    <source-file>
                         <location>getlib.xml</location>
                     </source-file>
- 		    <source-file>
+                    <source-file>
                         <location>getlib.properties</location>
                     </source-file>
                 </items>
@@ -91,7 +141,12 @@
                 <package-root>instrument/src</package-root>
                 <package-root>runtime/src</package-root>
                 <package-root>runtime/patternsrc</package-root>
-                <classpath mode="compile">lib/bsh-src.jar:lib/commons-fileupload.jar:lib/commons-io.jar:lib/cojen-sources.jar:lib/bsh-core.jar:lib/cojen.jar</classpath>
+                <package-root>runtime/testsrc</package-root>
+                <package-root>runtime/servletsrc</package-root>
+                <package-root>runtime/servlettestsrc</package-root>
+                <package-root>runtime/servlettestweb</package-root>
+                <package-root>runtime/junitsrc</package-root>
+                <classpath mode="compile">lib/bsh-src.jar:lib/commons-fileupload.jar:lib/commons-io.jar:lib/cojen-sources.jar:lib/bsh-core.jar:lib/cojen.jar:lib/servlet-api.jar:lib/exedio-cope-util.jar:lib/exedio-cope-util.jar-src.zip:lib/junit.jar:lib/jdbc/hsqldb.jar:lib/jdbc/mysql-connector-java-5.0.3-bin.jar:lib/jdbc/postgresql-8.1-404.jdbc3.jar</classpath>
                 <source-level>1.5</source-level>
             </compilation-unit>
         </java-data>

runtime/servletsrc/com/exedio/cope/pattern/MediaPath.java
@@ -100,13 +100,22 @@ public abstract class MediaPath extends Pattern
 		
 		return Collections.unmodifiableList(result);
 	}
-	
+
 	/**
 	 * Returns a URL the content of this media path is available under,
 	 * if a {@link MediaServlet} is properly installed.
 	 * Returns null, if there is no such content.
 	 */
 	public final String getURL(final Item item)
+	{
+		return getNamedURL(item, null);
+	}
+	/**
+	 * Returns a URL the content of this media path is available under,
+	 * if a {@link MediaServlet} is properly installed.
+	 * Returns null, if there is no such content.
+	 */
+	public final String getNamedURL(final Item item, final String name)
 	{
 		final String contentType = getContentType(item);
 
@@ -118,12 +127,62 @@ public abstract class MediaPath extends Pattern
 		bf.append(getUrlPath()).
 			append(item.getCopeID());
 
+		if (name!=null && name.length()>0)
+		{
+			final String nameNatural = encodeNaturalLanguageSegment(name);
+			if (nameNatural.length()>0)
+			bf.append("/").append(nameNatural);
+		}
+
 		final String extension = contentTypeToExtension.get(contentType);
 		if(extension!=null)
 			bf.append(extension);
 		
 		return bf.toString();
 	}
+
+	private static final char NATURAL_PLACE_HOLDER = '-';
+
+	public static final String encodeNaturalLanguageSegment(final String s)
+	{
+		if(s==null)
+			return null;
+
+		final int l = s.length();
+		for(int i = 0; i<l; i++)
+		{
+			final char c = s.charAt(i);
+			if(!(('0'<=c&&c<='9')||('a'<=c&&c<='z')||('A'<=c&&c<='Z')))
+			{
+				final StringBuilder bf = new StringBuilder(l);
+				if(i>0)
+					bf.append(s.substring(0, i));
+				boolean skipped = false;
+				for(; i<l; i++)
+				{
+					final char c2 = s.charAt(i);
+					if(('0'<=c2&&c2<='9')||('a'<=c2&&c2<='z')||('A'<=c2&&c2<='Z'))
+					{
+						if(skipped)
+						{
+							bf.append(NATURAL_PLACE_HOLDER);
+							skipped = false;
+						}
+						bf.append(c2);
+					}
+					else
+					{
+						skipped = true;
+					}
+				}
+				if(bf.length()==1 && bf.charAt(0)==NATURAL_PLACE_HOLDER)
+					return "";
+				return bf.toString();
+			}
+		}
+		return s;
+	}
+
 
 	public static final Log noSuchPath = new Log("no such path", HttpServletResponse.SC_NOT_FOUND);
 	public final Log exception = new Log("exception", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);

runtime/servletsrc/com/exedio/cope/pattern/MediaServlet.java
@@ -170,21 +170,40 @@ public final class MediaServlet extends HttpServlet
 		if(pathInfo==null)
 			return MediaPath.noSuchPath;
 
-		final int trailingSlash = pathInfo.lastIndexOf('/');
+		int trailingSlash = pathInfo.lastIndexOf('/');
 		if(trailingSlash<=0 && // null is leading slash, which is not allowed
 			trailingSlash>=pathInfo.length()-1)
 			return MediaPath.noSuchPath;
 
-		final String featureString = pathInfo.substring(0, trailingSlash+1);
+		String featureString = pathInfo.substring(0, trailingSlash+1);
 		//System.out.println("featureString="+featureString);
 
-		final MediaPath path = pathes.get(featureString);
-		if(path==null)
-			return MediaPath.noSuchPath;
-		
+		MediaPath path = pathes.get(featureString);
+		if(path==null && featureString.length()>1)
+		{
+			featureString = featureString.substring(0, featureString.length()-1);
+			trailingSlash = featureString.lastIndexOf('/');
+			featureString = featureString.substring(0, trailingSlash+1);
+			if (trailingSlash>0)
+			{
+				featureString = pathInfo.substring(0, trailingSlash+1);
+				path = pathes.get(featureString);
+				if (path==null)
+				{
+					return MediaPath.noSuchPath;
+				}
+			}
+			else
+			{
+				return MediaPath.noSuchPath;
+			}
+		}
 		try
 		{
-			final String subPath = pathInfo.substring(trailingSlash+1);
+			String subPath = pathInfo.substring(trailingSlash+1);
+			trailingSlash = subPath.lastIndexOf('/');
+			if (trailingSlash>0)
+				subPath = subPath.substring(0,trailingSlash);
 			return path.doGet(request, response, subPath);
 		}
 		catch(RuntimeException e)

runtime/testsrc/com/exedio/cope/pattern/MediaItem.java
@@ -27,6 +27,7 @@ public class MediaItem extends Item
 
 	static final Media file = new Media().optional().lengthMax(20);
 
+	/** @cope.getURL none */
 	static final Media image = new Media().optional().contentTypeSub("image");
 	
 	static final Media photo = new Media().optional().lengthMax(2000).contentType("image/jpeg");
@@ -70,6 +71,12 @@ public class MediaItem extends Item
 	{
 		return custom.getURL(this);
 	}
+
+	final java.lang.String getImageURL()
+	{
+		return MediaItem.image.getNamedURL(this, getName());
+	}
+
 	
 	/**
 
@@ -262,16 +269,6 @@ public class MediaItem extends Item
 				java.io.IOException
 	{
 		MediaItem.file.set(this,body,contentType);
-	}/**
-
-	 **
-	 * Returns a URL the content of {@link #image} is available under.
-	 * @cope.generated This feature has been generated by the cope instrumentor and will be overwritten by the build process.
-	 *       It can be customized with the tag <tt>@cope.getURL public|package|protected|private|none|non-final</tt> in the comment of the field.
-	 */
-	final java.lang.String getImageURL()
-	{
-		return MediaItem.image.getURL(this);
 	}/**
 
 	 **

runtime/testsrc/com/exedio/cope/pattern/MediaSubTest.java
@@ -156,6 +156,6 @@ public class MediaSubTest extends AbstractRuntimeTest
 		assertData(expectedData, item.getImageBody());
 		assertEquals(expectedData.length, item.getImageLength());
 		assertEquals(expectedContentType, item.getImageContentType());
-		assertEquals(mediaRootUrl + "MediaItem/image/" + item.getCopeID() + expectedExtension, item.getImageURL());
+		assertEquals(mediaRootUrl + "MediaItem/image/" + item.getCopeID() +"/" +MediaPath.encodeNaturalLanguageSegment(item.getName())+ expectedExtension, item.getImageURL());
 	}
 }

commit d48d100a0ef33b6e3226090f87e3973a3745ec94

more tests, do show bug introduced in r13030: new method getNamedURL
git-svn-id: https://svn.exedio.com/cope/trunk/persistence@13076 e7d4fc99-c606-0410-b9bf-843393a9eab7

copernica/webtestsrc/com/exedio/copernica/MediaServletTest.java
@@ -61,7 +61,29 @@ public class MediaServletTest extends TestCase
 		assertEquals(textLastModified, assertURL(new URL(prefix + "content/MediaServletItem.0/zick")));
 		assertEquals(textLastModified, assertURL(new URL(prefix + "content/MediaServletItem.0/.")));
 		assertEquals(textLastModified, assertURL(new URL(prefix + "content/MediaServletItem.0/")));
-		assertNotFound(new URL(prefix + "kontent/MediaServletItem.0.txt"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MeDiaServletItem/content/MediaServletItem.0/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MediaServletItem/conTent/MediaServletItem.0/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media//content/MediaServletItem.0/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MediaServletItem//MediaServletItem.0/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media///MediaServletItem.0/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media////"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media///"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media//"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MediaServletItem/content"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MediaServletItem/c"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MediaServletItem/"), NO_SUCH_PATH);
+		assertInternalErrorUncaught(new URL(app + "media/MediaServletItem")); // TODO, should be NO_SUCH_PATH
+		assertInternalErrorUncaught(new URL(app + "media/M")); // TODO should be NO_SUCH_PATH
+		assertNotFound(new URL(prefix + "c"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MediaServletItem/c"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/MediaServletItem/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media////"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media///"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media//"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media/"), NO_SUCH_PATH);
+		assertNotFound(new URL(app + "media"), NO_SUCH_PATH);
 		assertNotFound(new URL(prefix + "content/MediaServletItem.150.txt"), NO_SUCH_ITEM);
 		assertNotFound(new URL(prefix + "content/MediaServletItem.150.zick"), NO_SUCH_ITEM);
 		assertNotFound(new URL(prefix + "content/MediaServletItem.150."), NO_SUCH_ITEM);
@@ -294,6 +316,24 @@ public class MediaServletTest extends TestCase
 		assertEquals(null, is.readLine());
 		is.close();
 
+		final long date = conn.getDate();
+		final Date after = new Date();
+		//System.out.println("Date: "+new Date(date));
+		assertWithinHttpDate(before, after, new Date(date));
+	}
+
+	private void assertInternalErrorUncaught(final URL url) throws IOException // TODO remove
+	{
+		final Date before = new Date();
+		final HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.setFollowRedirects(false);
+		conn.connect();
+		if(conn.HTTP_INTERNAL_ERROR!=conn.getResponseCode())
+			print(conn, url);
+		assertEquals(conn.HTTP_INTERNAL_ERROR, conn.getResponseCode());
+		assertEquals("Internal Server Error", conn.getResponseMessage());
+		assertEquals("text/html;charset=utf-8", conn.getContentType());
+		
 		final long date = conn.getDate();
 		final Date after = new Date();
 		//System.out.println("Date: "+new Date(date));