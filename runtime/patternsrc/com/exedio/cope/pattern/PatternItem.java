2c1485bfdc9c18ce4a1641881788b72620458c89
Torsten Bürger <torsten.buerger@exedio.com>
2009-09-15 13:49:49 +0000

    Since I want to create a pattern in a project (which I later want to migrate to cope) I also want to use dummy item class. Therefore I propose to make this class public.
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@13567 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/patternsrc/com/exedio/cope/pattern/PatternItem.java
+++ runtime/patternsrc/com/exedio/cope/pattern/PatternItem.java
@@ -28,7 +28,7 @@ import com.exedio.cope.SetValue;
 import com.exedio.cope.Type;
 import com.exedio.cope.util.ReactivationConstructorDummy;
 
-final class PatternItem extends Item
+public final class PatternItem extends Item
 {
 	private static final long serialVersionUID = 1l;
 
