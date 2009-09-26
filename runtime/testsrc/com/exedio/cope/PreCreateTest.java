1349016f55a0d0670a91a0aae099dada47e37dca
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2009-09-26 17:55:54 +0000

    add PreCreateTest
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@13737 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/testsrc/com/exedio/cope/PreCreateTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+public class PreCreateTest extends AbstractRuntimeTest
+{
+	private static final Model MODEL = new Model(PreCreateItem.TYPE);
+	
+	public PreCreateTest()
+	{
+		super(MODEL);
+	}
+	
+	public void test()
+	{
+		final PreCreateItem normal = deleteOnTearDown(new PreCreateItem("normal"));
+		assertEquals("normal.preCreate", normal.getText());
+		
+		final PreCreateItem generic = deleteOnTearDown(new PreCreateItem(PreCreateItem.text.map("generic")));
+		assertEquals("generic.preCreate", generic.getText());
+		
+		final PreCreateItem type = deleteOnTearDown(PreCreateItem.TYPE.newItem(PreCreateItem.text.map("type")));
+		assertEquals("type.preCreate", type.getText());
+	}
+}
