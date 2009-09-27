8e31c115a81bab80c615a53d828011ffe0d3106e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2009-09-27 11:15:14 +0000

    local rename
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@13746 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/PreCreateTest.java
+++ runtime/testsrc/com/exedio/cope/PreCreateTest.java
@@ -30,16 +30,16 @@ public class PreCreateTest extends AbstractRuntimeTest
 	public void test()
 	{
 		{
-			final PreCreateItem normal = deleteOnTearDown(new PreCreateItem("normal"));
-			assertEquals("normal.preCreate.preCreateSuper", normal.getText());
+			final PreCreateItem item = deleteOnTearDown(new PreCreateItem("normal"));
+			assertEquals("normal.preCreate.preCreateSuper", item.getText());
 		}
 		{
-			final PreCreateItem generic = deleteOnTearDown(new PreCreateItem(PreCreateSuperItem.text.map("generic")));
-			assertEquals("generic.preCreate.preCreateSuper", generic.getText());
+			final PreCreateItem item = deleteOnTearDown(new PreCreateItem(PreCreateSuperItem.text.map("generic")));
+			assertEquals("generic.preCreate.preCreateSuper", item.getText());
 		}
 		{
-			final PreCreateItem type = deleteOnTearDown(PreCreateItem.TYPE.newItem(PreCreateSuperItem.text.map("type")));
-			assertEquals("type.preCreate.preCreateSuper", type.getText());
+			final PreCreateItem item = deleteOnTearDown(PreCreateItem.TYPE.newItem(PreCreateSuperItem.text.map("type")));
+			assertEquals("type.preCreate.preCreateSuper", item.getText());
 		}
 	}
 }
