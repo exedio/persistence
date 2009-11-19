097d06633d37d228b7566af28a889bc2beafb123
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2009-11-19 18:20:10 +0000

    split EnumFieldType off EnumField
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@14227 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/EnumField.java
+++ runtime/src/com/exedio/cope/EnumField.java
@@ -20,9 +20,6 @@ package com.exedio.cope;
 
 import gnu.trove.TIntObjectHashMap;
 
-import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.exedio.cope.instrument.Wrapper;
@@ -37,74 +34,13 @@ public final class EnumField<E extends Enum<E>> extends FunctionField<E>
 	{
 		super(isfinal, optional, unique, valueClass, defaultConstant);
 		checkValueClass(Enum.class);
-		if(!valueClass.isEnum())
-			throw new RuntimeException("must be an enum: " + valueClass);
 
-		// TODO compute all this once for an Enum class, as in Composite.Type
-		final ArrayList<E> values = new ArrayList<E>();
-		final TIntObjectHashMap<E> numbersToValues = new TIntObjectHashMap<E>();
-		
-		final E[] enumConstants = valueClass.getEnumConstants();
-		if(enumConstants==null)
-			throw new RuntimeException("must have at least one enum value: " + valueClass);
-		final int[] ordinalsToNumbers = new int[enumConstants.length];
-		
-		int schemaValue = 0;
-		for(final E e : enumConstants)
-		{
-			final CopeSchemaValue annotation = getAnnotation(e, CopeSchemaValue.class);
-			final int number = annotation!=null ? annotation.value() : (schemaValue+=10);
-			values.add(e);
-			numbersToValues.put(number, e);
-			ordinalsToNumbers[e.ordinal()] = number;
-		}
-		final int l = ordinalsToNumbers.length-1;
-		int i = 0;
-		for(final E e : values)
-		{
-			if(getAnnotation(e, CopeSchemaValue.class)!=null)
-			{
-				if((i>0 && ordinalsToNumbers[i]<=ordinalsToNumbers[i-1]) ||
-					(i<l && ordinalsToNumbers[i]>=ordinalsToNumbers[i+1]))
-				{
-					final StringBuilder bf = new StringBuilder();
-					bf.append(valueClass.getName()).
-						append(": @CopeSchemaValue for ").
-						append(e.name()).
-						append(" must be");
-					if(i>0)
-						bf.append(" greater than ").append(ordinalsToNumbers[i-1]);
-					if(i>0 && i<ordinalsToNumbers.length-1)
-						bf.append(" and");
-					if(i<ordinalsToNumbers.length-1)
-						bf.append(" less than ").append(ordinalsToNumbers[i+1]);
-					bf.append(", but was ").
-						append(ordinalsToNumbers[i]).
-						append('.');
-					throw new IllegalArgumentException(bf.toString());
-				}
-			}
-			i++;
-		}
-		values.trimToSize();
-		numbersToValues.trimToSize();
-		this.values = Collections.unmodifiableList(values);
-		this.numbersToValues = numbersToValues;
-		this.ordinalsToNumbers = ordinalsToNumbers;
+		final EnumFieldType<E> valueType = new EnumFieldType<E>(valueClass);
+		this.values = valueType.values;
+		this.numbersToValues = valueType.numbersToValues;
+		this.ordinalsToNumbers = valueType.ordinalsToNumbers;
 		
 		checkDefaultConstant();
-	}
-	
-	private static final <T extends Annotation> T getAnnotation(final Enum e, Class<T> annotationClass)
-	{
-		try
-		{
-			return e.getDeclaringClass().getField(e.name()).getAnnotation(annotationClass);
-		}
-		catch(NoSuchFieldException ex)
-		{
-			throw new RuntimeException(ex);
-		}
 	}
 	
 	EnumField(final Class<E> valueClass)
---
+++ runtime/src/com/exedio/cope/EnumFieldType.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+import gnu.trove.TIntObjectHashMap;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+final class EnumFieldType<E extends Enum<E>>
+{
+	final List<E> values;
+	final TIntObjectHashMap<E> numbersToValues;
+	final int[] ordinalsToNumbers;
+	
+	EnumFieldType(final Class<E> valueClass)
+	{
+		if(!valueClass.isEnum())
+			throw new RuntimeException("must be an enum: " + valueClass);
+
+		// TODO compute all this once for an Enum class, as in Composite.Type
+		final ArrayList<E> values = new ArrayList<E>();
+		final TIntObjectHashMap<E> numbersToValues = new TIntObjectHashMap<E>();
+		
+		final E[] enumConstants = valueClass.getEnumConstants();
+		if(enumConstants==null)
+			throw new RuntimeException("must have at least one enum value: " + valueClass);
+		final int[] ordinalsToNumbers = new int[enumConstants.length];
+		
+		int schemaValue = 0;
+		for(final E e : enumConstants)
+		{
+			final CopeSchemaValue annotation = getAnnotation(e, CopeSchemaValue.class);
+			final int number = annotation!=null ? annotation.value() : (schemaValue+=10);
+			values.add(e);
+			numbersToValues.put(number, e);
+			ordinalsToNumbers[e.ordinal()] = number;
+		}
+		final int l = ordinalsToNumbers.length-1;
+		int i = 0;
+		for(final E e : values)
+		{
+			if(getAnnotation(e, CopeSchemaValue.class)!=null)
+			{
+				if((i>0 && ordinalsToNumbers[i]<=ordinalsToNumbers[i-1]) ||
+					(i<l && ordinalsToNumbers[i]>=ordinalsToNumbers[i+1]))
+				{
+					final StringBuilder bf = new StringBuilder();
+					bf.append(valueClass.getName()).
+						append(": @CopeSchemaValue for ").
+						append(e.name()).
+						append(" must be");
+					if(i>0)
+						bf.append(" greater than ").append(ordinalsToNumbers[i-1]);
+					if(i>0 && i<ordinalsToNumbers.length-1)
+						bf.append(" and");
+					if(i<ordinalsToNumbers.length-1)
+						bf.append(" less than ").append(ordinalsToNumbers[i+1]);
+					bf.append(", but was ").
+						append(ordinalsToNumbers[i]).
+						append('.');
+					throw new IllegalArgumentException(bf.toString());
+				}
+			}
+			i++;
+		}
+		values.trimToSize();
+		numbersToValues.trimToSize();
+		this.values = Collections.unmodifiableList(values);
+		this.numbersToValues = numbersToValues;
+		this.ordinalsToNumbers = ordinalsToNumbers;
+	}
+	
+	private static final <T extends Annotation> T getAnnotation(final Enum e, Class<T> annotationClass)
+	{
+		try
+		{
+			return e.getDeclaringClass().getField(e.name()).getAnnotation(annotationClass);
+		}
+		catch(NoSuchFieldException ex)
+		{
+			throw new RuntimeException(ex);
+		}
+	}
+}
