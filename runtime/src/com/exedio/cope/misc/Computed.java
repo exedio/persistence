a0c7f63ad6d0ab96f0eb95c6a29ef394b4c9256b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2009-12-04 12:16:15 +0000

    more javadoc
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@14430 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/misc/Computed.java
+++ runtime/src/com/exedio/cope/misc/Computed.java
@@ -26,11 +26,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation marks a feature or type as "computed".
+ * This annotation marks a feature or type as <i>computed</i>.
  * This means, that the persistent contents of that feature or type
- * are typically not set by human users, but by some computational process.
+ * are typically not modified by human users, but by some computational process.
  * So any generic editing facility is advised to not let a human user
  * modify this type or feature.
+ * 
+ * For types, this means, that items of the type shall no be created or deleted
+ * by human users. For features this means, that the state of the feature shall
+ * not be modified by a homan user. A <i>computed</i> type does not imply, that the type's
+ * features are <i>computed</i> as well. 
  * 
  * This annotation does not specify, whether this feature or type shall be
  * shown to a human user.
