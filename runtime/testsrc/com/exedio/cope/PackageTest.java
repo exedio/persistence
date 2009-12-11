99549a6c94c56611edd840a19be04ce472d93e1e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2009-12-11 20:09:36 +0000

    revert r14514, fails on cruise
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@14515 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/PackageTest.java
+++ runtime/testsrc/com/exedio/cope/PackageTest.java
@@ -35,8 +35,6 @@ public class PackageTest extends TestCase
 	{
 		final TestSuite suite = new TestSuite();
 		
-		suite.addTest( com.exedio.cope.junit.PackageTest.suite() );
-		
 		suite.addTestSuite( TrimTest.class );
 		suite.addTestSuite( ClusterIntTest.class );
 		suite.addTestSuite( ClusterTest.class );
