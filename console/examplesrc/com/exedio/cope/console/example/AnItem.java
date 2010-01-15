11680f88ae194be26e5c383561b484be4c4d2a99
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-01-15 14:01:21 +0000

    add dedicated example environment for console
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@14842 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ console/examplesrc/com/exedio/cope/console/example/AnItem.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.console.example;
+
+import com.exedio.cope.Item;
+import com.exedio.cope.StringField;
+
+final class AnItem extends Item
+{
+	static final StringField aField = new StringField();
+	
+	private AnItem(com.exedio.cope.ActivationParameters ap)
+	{
+		super(ap);
+	}
+	
+	private static final long serialVersionUID = 1l;
+	
+	static final com.exedio.cope.Type<AnItem> TYPE = com.exedio.cope.TypesBound.newType(AnItem.class);
+}
