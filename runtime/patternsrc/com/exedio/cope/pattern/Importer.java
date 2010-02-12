fd1aa39ea842d6fbc64bc4b2e1cc205180a8da1c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-02-12 15:53:13 +0000

    add Importer, not yet public
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@15292 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/patternsrc/com/exedio/cope/pattern/Importer.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.pattern;
+
+import com.exedio.cope.FunctionField;
+import com.exedio.cope.Item;
+import com.exedio.cope.Pattern;
+import com.exedio.cope.SetValue;
+import com.exedio.cope.util.Cast;
+
+final class Importer<E extends Object> extends Pattern
+{
+	private static final long serialVersionUID = 1l;
+	
+	private final FunctionField<E> key;
+	
+	private Importer(final FunctionField<E> key)
+	{
+		if(key==null)
+			throw new NullPointerException("key");
+		if(!key.isFinal())
+			throw new IllegalArgumentException();
+		if(!key.isMandatory())
+			throw new IllegalArgumentException();
+		if(key.getImplicitUniqueConstraint()==null)
+			throw new IllegalArgumentException();
+		
+		this.key = key;
+	}
+	
+	public static final <E> Importer<E> newImporter(final FunctionField<E> key)
+	{
+		return new Importer<E>(key);
+	}
+	
+	public <P extends Item> P doImport(
+			final Class<P> parentClass,
+			final E keyValue,
+			final SetValue... setValues)
+	{
+		if(keyValue==null)
+			throw new NullPointerException("keyValue");
+		if(setValues==null)
+			throw new NullPointerException("setValues");
+		
+		final P existent = Cast.verboseCast(parentClass, key.searchUnique(keyValue));
+		if(existent!=null)
+		{
+			existent.set(setValues);
+			return existent;
+		}
+		else
+		{
+			final SetValue[] setValuesNew = new SetValue[setValues.length + 1];
+			setValuesNew[0] = key.map(keyValue);
+			System.arraycopy(setValues, 0, setValuesNew, 1, setValues.length);
+			return getType().as(parentClass).newItem(setValuesNew);
+		}
+	}
+}
