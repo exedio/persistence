91e78ac1a3ea65327ad59b942d3c3691c0f870e3
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-03-03 14:59:43 +0000

    rename PolymorphicBoundSelectTest items
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@15554 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/PolymorphicBoundSelectTest.java
+++ runtime/testsrc/com/exedio/cope/PolymorphicBoundSelectTest.java
@@ -28,7 +28,7 @@ import com.exedio.cope.Selectable;
 
 public class PolymorphicBoundSelectTest extends AbstractRuntimeTest
 {
-	private static final Model MODEL = new Model(PolymorphicBoundSelectTestSuperItem.TYPE, PolymorphicBoundSelectTestSubItem.TYPE);
+	private static final Model MODEL = new Model(PolymorphicBoundSelectSuperItem.TYPE, PolymorphicBoundSelectSubItem.TYPE);
 	
 	public PolymorphicBoundSelectTest()
 	{
@@ -37,18 +37,18 @@ public class PolymorphicBoundSelectTest extends AbstractRuntimeTest
 	
 	public void testItemField()
 	{
-		final PolymorphicBoundSelectTestSuperItem superItem = deleteOnTearDown(new PolymorphicBoundSelectTestSuperItem((PolymorphicBoundSelectTestSuperItem)null));
-		final PolymorphicBoundSelectTestSubItem subItem = deleteOnTearDown(new PolymorphicBoundSelectTestSubItem(superItem));
+		final PolymorphicBoundSelectSuperItem superItem = deleteOnTearDown(new PolymorphicBoundSelectSuperItem((PolymorphicBoundSelectSuperItem)null));
+		final PolymorphicBoundSelectSubItem subItem = deleteOnTearDown(new PolymorphicBoundSelectSubItem(superItem));
 
 		final Query<List<Object>> q = Query.newQuery(
-			new Selectable[]{PolymorphicBoundSelectTestSubItem.TYPE.getThis(), PolymorphicBoundSelectTestSuperItem.parent},
-			PolymorphicBoundSelectTestSubItem.TYPE,
+			new Selectable[]{PolymorphicBoundSelectSubItem.TYPE.getThis(), PolymorphicBoundSelectSuperItem.parent},
+			PolymorphicBoundSelectSubItem.TYPE,
 			null
 		);
-		final Join j = q.joinOuterLeft(PolymorphicBoundSelectTestSuperItem.TYPE, null);
-		j.setCondition(PolymorphicBoundSelectTestSubItem.parent.equalTarget(j));
-		q.setSelects(PolymorphicBoundSelectTestSubItem.TYPE.getThis(), PolymorphicBoundSelectTestSuperItem.parent.bind(j));
-		q.addOrderBy(PolymorphicBoundSelectTestSuperItem.parent.bind(j));
+		final Join j = q.joinOuterLeft(PolymorphicBoundSelectSuperItem.TYPE, null);
+		j.setCondition(PolymorphicBoundSelectSubItem.parent.equalTarget(j));
+		q.setSelects(PolymorphicBoundSelectSubItem.TYPE.getThis(), PolymorphicBoundSelectSuperItem.parent.bind(j));
+		q.addOrderBy(PolymorphicBoundSelectSuperItem.parent.bind(j));
 		if(noJoinParentheses)
 			return;
 		assertEquals(list(list(subItem, null)), q.search());
@@ -56,18 +56,18 @@ public class PolymorphicBoundSelectTest extends AbstractRuntimeTest
 	
 	public void testThis()
 	{
-		final PolymorphicBoundSelectTestSuperItem superItem = deleteOnTearDown(new PolymorphicBoundSelectTestSuperItem((PolymorphicBoundSelectTestSuperItem)null));
-		deleteOnTearDown(new PolymorphicBoundSelectTestSubItem(superItem));
+		final PolymorphicBoundSelectSuperItem superItem = deleteOnTearDown(new PolymorphicBoundSelectSuperItem((PolymorphicBoundSelectSuperItem)null));
+		deleteOnTearDown(new PolymorphicBoundSelectSubItem(superItem));
 
 		final Query<List<Object>> q = Query.newQuery(
-			new Selectable[]{PolymorphicBoundSelectTestSuperItem.TYPE.getThis(), PolymorphicBoundSelectTestSubItem.parent},
-			PolymorphicBoundSelectTestSuperItem.TYPE,
+			new Selectable[]{PolymorphicBoundSelectSuperItem.TYPE.getThis(), PolymorphicBoundSelectSubItem.parent},
+			PolymorphicBoundSelectSuperItem.TYPE,
 			null
 		);
-		final Join j = q.joinOuterLeft(PolymorphicBoundSelectTestSuperItem.TYPE, null);
-		j.setCondition(PolymorphicBoundSelectTestSubItem.parent.equalTarget(j));
-		q.setSelects(PolymorphicBoundSelectTestSuperItem.TYPE.getThis().bind(j), PolymorphicBoundSelectTestSubItem.parent);
-		q.addOrderBy(PolymorphicBoundSelectTestSuperItem.parent.bind(j));
+		final Join j = q.joinOuterLeft(PolymorphicBoundSelectSuperItem.TYPE, null);
+		j.setCondition(PolymorphicBoundSelectSubItem.parent.equalTarget(j));
+		q.setSelects(PolymorphicBoundSelectSuperItem.TYPE.getThis().bind(j), PolymorphicBoundSelectSubItem.parent);
+		q.addOrderBy(PolymorphicBoundSelectSuperItem.parent.bind(j));
 		if(noJoinParentheses)
 			return;
 		assertEquals(list(list(null, null), list(superItem, superItem)), q.search());
