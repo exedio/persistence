f43f492a1b30117dcdba329b9a9f79f79c3f0e95
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-03-10 09:28:42 +0000

    format code
    git-svn-id: https://svn.exedio.com/cope/trunk/persistence@15631 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/SchemaTest.java
+++ runtime/testsrc/com/exedio/cope/SchemaTest.java
@@ -36,66 +36,64 @@ public class SchemaTest extends AbstractRuntimeTest
 
 	public void testSchema()
 	{
-		{
-			assertEquals(!mysql, model.supportsCheckConstraints());
-			final Schema schema = model.getVerifiedSchema();
+		assertEquals(!mysql, model.supportsCheckConstraints());
+		final Schema schema = model.getVerifiedSchema();
 
-			final com.exedio.dsmf.Table table = schema.getTable(getTableName(SchemaItem.TYPE));
-			assertNotNull(table);
-			assertEquals(null, table.getError());
-			assertEquals(Schema.Color.OK, table.getParticularColor());
+		final com.exedio.dsmf.Table table = schema.getTable(getTableName(SchemaItem.TYPE));
+		assertNotNull(table);
+		assertEquals(null, table.getError());
+		assertEquals(Schema.Color.OK, table.getParticularColor());
 
-			String mediaContentTypeCharSet = null;
-			if(mysql)
-				mediaContentTypeCharSet = " AND (`someData_contentType` regexp '^[-,/,0-9,a-z]*$')";
-			assertCheckConstraint(table, "ScheItem_somNotNullStr_Ck", "("+p(SchemaItem.someNotNullString)+" IS NOT NULL) AND ("+l(SchemaItem.someNotNullString)+"<="+StringField.DEFAULT_LENGTH+")");
-			assertCheckConstraint(table, "SchemaItem_someBoolean_Ck", "(("+p(SchemaItem.someBoolean)+" IS NOT NULL) AND ("+p(SchemaItem.someBoolean)+" IN (0,1))) OR ("+p(SchemaItem.someBoolean)+" IS NULL)");
-			assertCheckConstraint(table, "ScheItem_somNotNullBoo_Ck", "("+p(SchemaItem.someNotNullBoolean)+" IS NOT NULL) AND ("+p(SchemaItem.someNotNullBoolean)+" IN (0,1))");
-			assertCheckConstraint(table, "SchemaItem_someEnum_Ck"   , "(("+p(SchemaItem.someEnum)+" IS NOT NULL) AND ("+p(SchemaItem.someEnum)+" IN (10,20,30))) OR ("+p(SchemaItem.someEnum)+" IS NULL)");
-			assertCheckConstraint(table, "ScheItem_somNotNullEnu_Ck", "("+p(SchemaItem.someNotNullEnum)+" IS NOT NULL) AND ("+p(SchemaItem.someNotNullEnum)+" IN (10,20,30))");
-			assertCheckConstraint(table, "ScheItem_somData_coTyp_Ck", "(("+p(SchemaItem.someData.getContentType())+" IS NOT NULL) AND (("+l(SchemaItem.someData.getContentType())+">=1) AND ("+l(SchemaItem.someData.getContentType())+"<=61)" + (mediaContentTypeCharSet!=null ? mediaContentTypeCharSet : "") + ")) OR ("+p(SchemaItem.someData.getContentType())+" IS NULL)");
+		String mediaContentTypeCharSet = null;
+		if(mysql)
+			mediaContentTypeCharSet = " AND (`someData_contentType` regexp '^[-,/,0-9,a-z]*$')";
+		assertCheckConstraint(table, "ScheItem_somNotNullStr_Ck", "("+p(SchemaItem.someNotNullString)+" IS NOT NULL) AND ("+l(SchemaItem.someNotNullString)+"<="+StringField.DEFAULT_LENGTH+")");
+		assertCheckConstraint(table, "SchemaItem_someBoolean_Ck", "(("+p(SchemaItem.someBoolean)+" IS NOT NULL) AND ("+p(SchemaItem.someBoolean)+" IN (0,1))) OR ("+p(SchemaItem.someBoolean)+" IS NULL)");
+		assertCheckConstraint(table, "ScheItem_somNotNullBoo_Ck", "("+p(SchemaItem.someNotNullBoolean)+" IS NOT NULL) AND ("+p(SchemaItem.someNotNullBoolean)+" IN (0,1))");
+		assertCheckConstraint(table, "SchemaItem_someEnum_Ck"   , "(("+p(SchemaItem.someEnum)+" IS NOT NULL) AND ("+p(SchemaItem.someEnum)+" IN (10,20,30))) OR ("+p(SchemaItem.someEnum)+" IS NULL)");
+		assertCheckConstraint(table, "ScheItem_somNotNullEnu_Ck", "("+p(SchemaItem.someNotNullEnum)+" IS NOT NULL) AND ("+p(SchemaItem.someNotNullEnum)+" IN (10,20,30))");
+		assertCheckConstraint(table, "ScheItem_somData_coTyp_Ck", "(("+p(SchemaItem.someData.getContentType())+" IS NOT NULL) AND (("+l(SchemaItem.someData.getContentType())+">=1) AND ("+l(SchemaItem.someData.getContentType())+"<=61)" + (mediaContentTypeCharSet!=null ? mediaContentTypeCharSet : "") + ")) OR ("+p(SchemaItem.someData.getContentType())+" IS NULL)");
 
-			assertPkConstraint(table, "SchemaItem_Pk", null, getPrimaryKeyColumnName(SchemaItem.TYPE));
+		assertPkConstraint(table, "SchemaItem_Pk", null, getPrimaryKeyColumnName(SchemaItem.TYPE));
 
-			assertFkConstraint(table, "SchemaItem_someItem_Fk", "someItem", filterTableName("SchemaTargetItem"), getPrimaryKeyColumnName(SchemaTargetItem.TYPE));
+		assertFkConstraint(table, "SchemaItem_someItem_Fk", "someItem", filterTableName("SchemaTargetItem"), getPrimaryKeyColumnName(SchemaTargetItem.TYPE));
 
-			
-			assertNotNull(table);
-			assertEquals(null, table.getError());
-			assertEquals(Schema.Color.OK, table.getParticularColor());
-			
-			assertUniqueConstraint(table, "SchemaItem_UNIQUE_S_Unq", "("+p("UNIQUE_S")+")");
-			
-			
-			assertNotNull(table);
-			assertEquals(null, table.getError());
-			assertEquals(Schema.Color.OK, table.getParticularColor());
-			
-			assertUniqueConstraint(table, "SchemaItem_doublUniqu_Unq", "("+p("string")+","+p("integer")+")");
-			
-			
-			assertNotNull(table);
-			assertEquals(null, table.getError());
-			assertEquals(Schema.Color.OK, table.getParticularColor());
+		
+		assertNotNull(table);
+		assertEquals(null, table.getError());
+		assertEquals(Schema.Color.OK, table.getParticularColor());
+		
+		assertUniqueConstraint(table, "SchemaItem_UNIQUE_S_Unq", "("+p("UNIQUE_S")+")");
+		
+		
+		assertNotNull(table);
+		assertEquals(null, table.getError());
+		assertEquals(Schema.Color.OK, table.getParticularColor());
+		
+		assertUniqueConstraint(table, "SchemaItem_doublUniqu_Unq", "("+p("string")+","+p("integer")+")");
+		
+		
+		assertNotNull(table);
+		assertEquals(null, table.getError());
+		assertEquals(Schema.Color.OK, table.getParticularColor());
 
-			final Column min4Max8 = table.getColumn("MIN4_MAX8");
-			assertEquals(null, min4Max8.getError());
-			assertEquals(Schema.Color.OK, min4Max8.getParticularColor());
-			
-			final String string8;
-			if(hsqldb)
-				string8 = "varchar(8)";
-			else if(mysql)
-				string8 = "varchar(8) character set utf8 binary";
-			else
-				string8 = "VARCHAR2(24 BYTE)"; // varchar specifies bytes
-			assertEquals(string8, min4Max8.getType());
+		final Column min4Max8 = table.getColumn("MIN4_MAX8");
+		assertEquals(null, min4Max8.getError());
+		assertEquals(Schema.Color.OK, min4Max8.getParticularColor());
+		
+		final String string8;
+		if(hsqldb)
+			string8 = "varchar(8)";
+		else if(mysql)
+			string8 = "varchar(8) character set utf8 binary";
+		else
+			string8 = "VARCHAR2(24 BYTE)"; // varchar specifies bytes
+		assertEquals(string8, min4Max8.getType());
 
-			assertCheckConstraint(table, "SchemaItem_MIN_4_Ck",     "(("+p("MIN_4")+" IS NOT NULL) AND (("+l("MIN_4")+">=4) AND ("+l("MIN_4")+"<="+StringField.DEFAULT_LENGTH+"))) OR ("+p("MIN_4")+" IS NULL)");
-			assertCheckConstraint(table, "SchemaItem_MAX_4_Ck",     "(("+p("MAX_4")+" IS NOT NULL) AND ("+l("MAX_4")+"<=4)) OR ("+p("MAX_4")+" IS NULL)");
-			assertCheckConstraint(table, "SchemaItem_MIN4_MAX8_Ck", "(("+p("MIN4_MAX8")+" IS NOT NULL) AND (("+l("MIN4_MAX8")+">=4) AND ("+l("MIN4_MAX8")+"<=8))) OR ("+p("MIN4_MAX8")+" IS NULL)");
-			assertCheckConstraint(table, "SchemaItem_EXACT_6_Ck",   "(("+p("EXACT_6")+" IS NOT NULL) AND ("+l("EXACT_6")+"=6)) OR ("+p("EXACT_6")+" IS NULL)");
-		}
+		assertCheckConstraint(table, "SchemaItem_MIN_4_Ck",     "(("+p("MIN_4")+" IS NOT NULL) AND (("+l("MIN_4")+">=4) AND ("+l("MIN_4")+"<="+StringField.DEFAULT_LENGTH+"))) OR ("+p("MIN_4")+" IS NULL)");
+		assertCheckConstraint(table, "SchemaItem_MAX_4_Ck",     "(("+p("MAX_4")+" IS NOT NULL) AND ("+l("MAX_4")+"<=4)) OR ("+p("MAX_4")+" IS NULL)");
+		assertCheckConstraint(table, "SchemaItem_MIN4_MAX8_Ck", "(("+p("MIN4_MAX8")+" IS NOT NULL) AND (("+l("MIN4_MAX8")+">=4) AND ("+l("MIN4_MAX8")+"<=8))) OR ("+p("MIN4_MAX8")+" IS NULL)");
+		assertCheckConstraint(table, "SchemaItem_EXACT_6_Ck",   "(("+p("EXACT_6")+" IS NOT NULL) AND ("+l("EXACT_6")+"=6)) OR ("+p("EXACT_6")+" IS NULL)");
 	}
 	
 	private final String p(final Field attribute)
