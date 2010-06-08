0390599b088cbde02b888033fd0ba8a72062075a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-06-08 04:00:51 +0000

    create SaltedSHAHash as duplicate of SHAHash
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/saltedHash@16488 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/patternsrc/com/exedio/cope/pattern/SaltedSHAHash.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.pattern;
+
+public final class SaltedSHAHash extends MessageDigestHash
+{
+	private static final long serialVersionUID = 1l;
+	
+	private static final String HASH = "SHA-512";
+
+	private SaltedSHAHash(final boolean optional, final String encoding)
+	{
+		super(optional, HASH, encoding);
+	}
+
+	public SaltedSHAHash(final String encoding)
+	{
+		super(false, HASH, encoding);
+	}
+
+	public SaltedSHAHash()
+	{
+		super(false, HASH);
+	}
+	
+	@Override
+	public SaltedSHAHash optional()
+	{
+		return new SaltedSHAHash(true, getEncoding());
+	}
+}
--- runtime/testsrc/com/exedio/cope/pattern/PackageTest.java
+++ runtime/testsrc/com/exedio/cope/pattern/PackageTest.java
@@ -31,6 +31,7 @@ public class PackageTest extends TestCase
 		suite.addTestSuite(HashTest.class);
 		suite.addTestSuite(MD5Test.class);
 		suite.addTestSuite(SHATest.class);
+		suite.addTestSuite(SaltedSHATest.class);
 		suite.addTestSuite(PartOfTest.class);
 		suite.addTestSuite(LimitedListFieldTest.class);
 		suite.addTestSuite(ListFieldTest.class);
