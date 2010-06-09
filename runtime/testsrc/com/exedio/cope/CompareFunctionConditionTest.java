06cb3f43da68978a452b10c1b8b93483dad5589e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-06-09 09:02:19 +0000

    local rename
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/checkCondition@16515 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/CompareFunctionConditionTest.java
+++ runtime/testsrc/com/exedio/cope/CompareFunctionConditionTest.java
@@ -83,70 +83,70 @@ public class CompareFunctionConditionTest extends AbstractRuntimeTest
 		
 		
 		// equal
-		assertIt(item3, item1.leftString.equal(item1.rightString));
-		assertIt(item3, item1.leftInt.equal(item1.rightInt));
-		assertIt(item3, item1.leftLong.equal(item1.rightLong));
-		assertIt(item3, item1.leftDouble.equal(item1.rightDouble));
-		assertIt(item3, item1.leftDate.equal(item1.rightDate));
-		assertIt(item3, item1.leftDay.equal(item1.rightDay));
-		assertIt(item3, item1.leftEnum.equal(item1.rightEnum));
-		assertIt(item3, item1.leftItem.equal(item1.rightItem));
-		assertIt(item3, item1.TYPE.getThis().equal(item1.rightItem));
+		assertCondition(item3, item1.leftString.equal(item1.rightString));
+		assertCondition(item3, item1.leftInt.equal(item1.rightInt));
+		assertCondition(item3, item1.leftLong.equal(item1.rightLong));
+		assertCondition(item3, item1.leftDouble.equal(item1.rightDouble));
+		assertCondition(item3, item1.leftDate.equal(item1.rightDate));
+		assertCondition(item3, item1.leftDay.equal(item1.rightDay));
+		assertCondition(item3, item1.leftEnum.equal(item1.rightEnum));
+		assertCondition(item3, item1.leftItem.equal(item1.rightItem));
+		assertCondition(item3, item1.TYPE.getThis().equal(item1.rightItem));
 
 		// notEqual
-		assertIt(item1, item2, item4, item5, item1.leftString.notEqual(item1.rightString));
-		assertIt(item1, item2, item4, item5, item1.leftInt.notEqual(item1.rightInt));
-		assertIt(item1, item2, item4, item5, item1.leftLong.notEqual(item1.rightLong));
-		assertIt(item1, item2, item4, item5, item1.leftDouble.notEqual(item1.rightDouble));
-		assertIt(item1, item2, item4, item5, item1.leftDate.notEqual(item1.rightDate));
-		assertIt(item1, item2, item4, item5, item1.leftDay.notEqual(item1.rightDay));
-		assertIt(item1, item2, item4, item5, item1.leftEnum.notEqual(item1.rightEnum));
-		assertIt(item1, item2, item4, item5, item1.leftItem.notEqual(item1.rightItem));
-		assertIt(item1, item2, item4, item5, item1.TYPE.getThis().notEqual(item1.rightItem));
+		assertCondition(item1, item2, item4, item5, item1.leftString.notEqual(item1.rightString));
+		assertCondition(item1, item2, item4, item5, item1.leftInt.notEqual(item1.rightInt));
+		assertCondition(item1, item2, item4, item5, item1.leftLong.notEqual(item1.rightLong));
+		assertCondition(item1, item2, item4, item5, item1.leftDouble.notEqual(item1.rightDouble));
+		assertCondition(item1, item2, item4, item5, item1.leftDate.notEqual(item1.rightDate));
+		assertCondition(item1, item2, item4, item5, item1.leftDay.notEqual(item1.rightDay));
+		assertCondition(item1, item2, item4, item5, item1.leftEnum.notEqual(item1.rightEnum));
+		assertCondition(item1, item2, item4, item5, item1.leftItem.notEqual(item1.rightItem));
+		assertCondition(item1, item2, item4, item5, item1.TYPE.getThis().notEqual(item1.rightItem));
 
 		// less
-		assertIt(item1, item2, item1.leftString.less(item1.rightString));
-		assertIt(item1, item2, item1.leftInt.less(item1.rightInt));
-		assertIt(item1, item2, item1.leftLong.less(item1.rightLong));
-		assertIt(item1, item2, item1.leftDouble.less(item1.rightDouble));
-		assertIt(item1, item2, item1.leftDate.less(item1.rightDate));
-		assertIt(item1, item2, item1.leftDay.less(item1.rightDay));
-		assertIt(item1, item2, item1.leftEnum.less(item1.rightEnum));
-		assertIt(item1, item2, item1.leftItem.less(item1.rightItem));
-		assertIt(item1, item2, item1.TYPE.getThis().less(item1.rightItem));
+		assertCondition(item1, item2, item1.leftString.less(item1.rightString));
+		assertCondition(item1, item2, item1.leftInt.less(item1.rightInt));
+		assertCondition(item1, item2, item1.leftLong.less(item1.rightLong));
+		assertCondition(item1, item2, item1.leftDouble.less(item1.rightDouble));
+		assertCondition(item1, item2, item1.leftDate.less(item1.rightDate));
+		assertCondition(item1, item2, item1.leftDay.less(item1.rightDay));
+		assertCondition(item1, item2, item1.leftEnum.less(item1.rightEnum));
+		assertCondition(item1, item2, item1.leftItem.less(item1.rightItem));
+		assertCondition(item1, item2, item1.TYPE.getThis().less(item1.rightItem));
 
 		// lessOrEqual
-		assertIt(item1, item2, item3, item1.leftString.lessOrEqual(item1.rightString));
-		assertIt(item1, item2, item3, item1.leftInt.lessOrEqual(item1.rightInt));
-		assertIt(item1, item2, item3, item1.leftLong.lessOrEqual(item1.rightLong));
-		assertIt(item1, item2, item3, item1.leftDouble.lessOrEqual(item1.rightDouble));
-		assertIt(item1, item2, item3, item1.leftDate.lessOrEqual(item1.rightDate));
-		assertIt(item1, item2, item3, item1.leftDay.lessOrEqual(item1.rightDay));
-		assertIt(item1, item2, item3, item1.leftEnum.lessOrEqual(item1.rightEnum));
-		assertIt(item1, item2, item3, item1.leftItem.lessOrEqual(item1.rightItem));
-		assertIt(item1, item2, item3, item1.TYPE.getThis().lessOrEqual(item1.rightItem));
+		assertCondition(item1, item2, item3, item1.leftString.lessOrEqual(item1.rightString));
+		assertCondition(item1, item2, item3, item1.leftInt.lessOrEqual(item1.rightInt));
+		assertCondition(item1, item2, item3, item1.leftLong.lessOrEqual(item1.rightLong));
+		assertCondition(item1, item2, item3, item1.leftDouble.lessOrEqual(item1.rightDouble));
+		assertCondition(item1, item2, item3, item1.leftDate.lessOrEqual(item1.rightDate));
+		assertCondition(item1, item2, item3, item1.leftDay.lessOrEqual(item1.rightDay));
+		assertCondition(item1, item2, item3, item1.leftEnum.lessOrEqual(item1.rightEnum));
+		assertCondition(item1, item2, item3, item1.leftItem.lessOrEqual(item1.rightItem));
+		assertCondition(item1, item2, item3, item1.TYPE.getThis().lessOrEqual(item1.rightItem));
 
 		// greater
-		assertIt(item4, item5, item1.leftString.greater(item1.rightString));
-		assertIt(item4, item5, item1.leftInt.greater(item1.rightInt));
-		assertIt(item4, item5, item1.leftLong.greater(item1.rightLong));
-		assertIt(item4, item5, item1.leftDouble.greater(item1.rightDouble));
-		assertIt(item4, item5, item1.leftDate.greater(item1.rightDate));
-		assertIt(item4, item5, item1.leftDay.greater(item1.rightDay));
-		assertIt(item4, item5, item1.leftEnum.greater(item1.rightEnum));
-		assertIt(item4, item5, item1.leftItem.greater(item1.rightItem));
-		assertIt(item4, item5, item1.TYPE.getThis().greater(item1.rightItem));
+		assertCondition(item4, item5, item1.leftString.greater(item1.rightString));
+		assertCondition(item4, item5, item1.leftInt.greater(item1.rightInt));
+		assertCondition(item4, item5, item1.leftLong.greater(item1.rightLong));
+		assertCondition(item4, item5, item1.leftDouble.greater(item1.rightDouble));
+		assertCondition(item4, item5, item1.leftDate.greater(item1.rightDate));
+		assertCondition(item4, item5, item1.leftDay.greater(item1.rightDay));
+		assertCondition(item4, item5, item1.leftEnum.greater(item1.rightEnum));
+		assertCondition(item4, item5, item1.leftItem.greater(item1.rightItem));
+		assertCondition(item4, item5, item1.TYPE.getThis().greater(item1.rightItem));
 
 		// greaterOrEqual
-		assertIt(item3, item4, item5, item1.leftString.greaterOrEqual(item1.rightString));
-		assertIt(item3, item4, item5, item1.leftInt.greaterOrEqual(item1.rightInt));
-		assertIt(item3, item4, item5, item1.leftLong.greaterOrEqual(item1.rightLong));
-		assertIt(item3, item4, item5, item1.leftDouble.greaterOrEqual(item1.rightDouble));
-		assertIt(item3, item4, item5, item1.leftDate.greaterOrEqual(item1.rightDate));
-		assertIt(item3, item4, item5, item1.leftDay.greaterOrEqual(item1.rightDay));
-		assertIt(item3, item4, item5, item1.leftEnum.greaterOrEqual(item1.rightEnum));
-		assertIt(item3, item4, item5, item1.leftItem.greaterOrEqual(item1.rightItem));
-		assertIt(item3, item4, item5, item1.TYPE.getThis().greaterOrEqual(item1.rightItem));
+		assertCondition(item3, item4, item5, item1.leftString.greaterOrEqual(item1.rightString));
+		assertCondition(item3, item4, item5, item1.leftInt.greaterOrEqual(item1.rightInt));
+		assertCondition(item3, item4, item5, item1.leftLong.greaterOrEqual(item1.rightLong));
+		assertCondition(item3, item4, item5, item1.leftDouble.greaterOrEqual(item1.rightDouble));
+		assertCondition(item3, item4, item5, item1.leftDate.greaterOrEqual(item1.rightDate));
+		assertCondition(item3, item4, item5, item1.leftDay.greaterOrEqual(item1.rightDay));
+		assertCondition(item3, item4, item5, item1.leftEnum.greaterOrEqual(item1.rightEnum));
+		assertCondition(item3, item4, item5, item1.leftItem.greaterOrEqual(item1.rightItem));
+		assertCondition(item3, item4, item5, item1.TYPE.getThis().greaterOrEqual(item1.rightItem));
 		
 		// Condition.Literal.evaluate
 		assertEquals(true,  Condition.TRUE .evaluate(item1));
@@ -171,38 +171,38 @@ public class CompareFunctionConditionTest extends AbstractRuntimeTest
 		}
 	}
 	
-	private void assertIt(final CompareFunctionConditionItem o1, final CompareFunctionCondition actual)
+	private void assertCondition(final CompareFunctionConditionItem o1, final CompareFunctionCondition actual)
 	{
-		assertIt(Arrays.asList(new CompareFunctionConditionItem[]{o1}), actual);
+		assertCondition(Arrays.asList(new CompareFunctionConditionItem[]{o1}), actual);
 	}
 	
-	private void assertIt(final CompareFunctionConditionItem o1, final CompareFunctionConditionItem o2, final CompareFunctionCondition actual)
+	private void assertCondition(final CompareFunctionConditionItem o1, final CompareFunctionConditionItem o2, final CompareFunctionCondition actual)
 	{
-		assertIt(Arrays.asList(new CompareFunctionConditionItem[]{o1, o2}), actual);
+		assertCondition(Arrays.asList(new CompareFunctionConditionItem[]{o1, o2}), actual);
 	}
 	
-	private void assertIt(final CompareFunctionConditionItem o1, final CompareFunctionConditionItem o2, final CompareFunctionConditionItem o3, final CompareFunctionCondition actual)
+	private void assertCondition(final CompareFunctionConditionItem o1, final CompareFunctionConditionItem o2, final CompareFunctionConditionItem o3, final CompareFunctionCondition actual)
 	{
-		assertIt(Arrays.asList(new CompareFunctionConditionItem[]{o1, o2, o3}), actual);
+		assertCondition(Arrays.asList(new CompareFunctionConditionItem[]{o1, o2, o3}), actual);
 	}
 	
-	private void assertIt(final CompareFunctionConditionItem o1, final CompareFunctionConditionItem o2, final CompareFunctionConditionItem o3, final CompareFunctionConditionItem o4, final CompareFunctionCondition actual)
+	private void assertCondition(final CompareFunctionConditionItem o1, final CompareFunctionConditionItem o2, final CompareFunctionConditionItem o3, final CompareFunctionConditionItem o4, final CompareFunctionCondition actual)
 	{
-		assertIt(Arrays.asList(new CompareFunctionConditionItem[]{o1, o2, o3, o4}), actual);
+		assertCondition(Arrays.asList(new CompareFunctionConditionItem[]{o1, o2, o3, o4}), actual);
 	}
 	
-	private void assertIt(final List<CompareFunctionConditionItem> expected, final CompareFunctionCondition actual)
+	private void assertCondition(final List<CompareFunctionConditionItem> expected, final CompareFunctionCondition actual)
 	{
 		assertContainsList(expected, CompareFunctionConditionItem.TYPE.search(actual));
-		assertIt(expected, actual, item1);
-		assertIt(expected, actual, item2);
-		assertIt(expected, actual, item3);
-		assertIt(expected, actual, item4);
-		assertIt(expected, actual, item5);
-		assertIt(expected, actual, itemX);
+		assertCondition(expected, actual, item1);
+		assertCondition(expected, actual, item2);
+		assertCondition(expected, actual, item3);
+		assertCondition(expected, actual, item4);
+		assertCondition(expected, actual, item5);
+		assertCondition(expected, actual, itemX);
 	}
 	
-	private static void assertIt(final List<CompareFunctionConditionItem> expected, final CompareFunctionCondition actual, final CompareFunctionConditionItem item)
+	private static void assertCondition(final List<CompareFunctionConditionItem> expected, final CompareFunctionCondition actual, final CompareFunctionConditionItem item)
 	{
 		assertEquals(expected.contains(item), actual.evaluate(item));
 	}
