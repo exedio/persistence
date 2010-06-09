0d5889134cc91031065f66f7b46c6e8ae3275fea
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-06-09 09:58:36 +0000

    add CheckConstraint, is not yet obeyd by anyone
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/checkCondition@16526 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/testsrc/com/exedio/cope/CheckConstraintTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+import static com.exedio.cope.CheckConstraintItem.TYPE;
+import static com.exedio.cope.CheckConstraintItem.alpha;
+import static com.exedio.cope.CheckConstraintItem.beta;
+import static com.exedio.cope.CheckConstraintItem.delta;
+import static com.exedio.cope.CheckConstraintItem.gamma;
+import static com.exedio.cope.CheckConstraintItem.alphaLessBeta;;
+
+public class CheckConstraintTest extends AbstractRuntimeTest
+{
+	static final Model MODEL = new Model(CheckConstraintItem.TYPE);
+	
+	static
+	{
+		MODEL.enableSerialization(CheckConstraintTest.class, "MODEL");
+	}
+	
+	public CheckConstraintTest()
+	{
+		super(MODEL);
+	}
+
+	public void testItemWithSingleUnique()
+	{
+		// test model
+		assertEqualsUnmodifiable(
+			list(
+				TYPE.getThis(),
+				alpha, beta, gamma, delta,
+				alphaLessBeta),
+			TYPE.getDeclaredFeatures());
+		assertEqualsUnmodifiable(
+			list(
+				TYPE.getThis(),
+				alpha, beta, gamma, delta,
+				alphaLessBeta),
+			TYPE.getFeatures());
+		
+		assertSerializedSame(alphaLessBeta, 393);
+	}
+}
--- runtime/testsrc/com/exedio/cope/PackageTest.java
+++ runtime/testsrc/com/exedio/cope/PackageTest.java
@@ -90,6 +90,7 @@ public class PackageTest extends TestCase
 		suite.addTestSuite( DataFinalTest.class );
 		suite.addTestSuite( DataMandatoryTest.class );
 		suite.addTestSuite( UniqueTest.class );
+		suite.addTestSuite( CheckConstraintTest.class );
 		suite.addTestSuite( HierarchyTest.class );
 		suite.addTestSuite( HierarchyEmptyTest.class );
 		suite.addTestSuite( SearchTest.class );
