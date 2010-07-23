570a7df103ad1b59b21ea2fcca2da52b4fe8509a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-07-23 19:44:36 +0000

    move MessageDigestAlgorithmTest to correct package
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@16997 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/utiltestsrc/com/exedio/cope/PackageUtilTest.java
+++ runtime/utiltestsrc/com/exedio/cope/PackageUtilTest.java
@@ -22,6 +22,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import com.exedio.cope.pattern.MessageDigestAlgorithmTest;
+
 public class PackageUtilTest extends TestCase
 {
 	public static Test suite()
