914a4aade7c85599c5a8b860d8d4205830b3c691
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-09-29 03:53:30 +0000

    new start with revisions
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/sample@17996 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/sample/HistoryRevisions.java
+++ runtime/servletsrc/com/exedio/cope/sample/HistoryRevisions.java
@@ -18,7 +18,6 @@
 
 package com.exedio.cope.sample;
 
-import com.exedio.cope.Revision;
 import com.exedio.cope.Revisions;
 
 /**
@@ -28,41 +27,7 @@ final class HistoryRevisions
 {
 	static final Revisions REVISIONS =
 		new Revisions(
-			new Revision(4, "add guessedUrl",
-				"alter table `HistoryModel` add column `mediasGuessedUrl` integer",
-				"alter table `HistoryMedia` add column `guessedUrl` integer"),
-			new Revision(3, "add HistoryPurge",
-				"create table `HistoryPurge`(" +
-				"`this` integer," +
-				"`type` varchar(80) character set utf8 binary," +
-				"`limit` bigint,`finished` bigint," +
-				"`rows` integer," +
-				"`elapsed` integer," +
-				"constraint `HistoryPurge_Pk` primary key(`this`)," +
-				"constraint `HistoryPurge_this_CkPk` check((`this`>=0) AND (`this`<=2147483647))," +
-				"constraint `HistoryPurge_type_Ck` check((`type` IS NOT NULL) AND (CHAR_LENGTH(`type`)<=80))," +
-				"constraint `HistoryPurge_limit_Ck` check((`limit` IS NOT NULL) AND ((`limit`>=-9223372036854775808) AND (`limit`<=9223372036854775807)))," +
-				"constraint `HistoryPurge_finished_Ck` check((`finished` IS NOT NULL) AND ((`finished`>=-9223372036854775808) AND (`finished`<=9223372036854775807)))," +
-				"constraint `HistoryPurge_rows_Ck` check((`rows` IS NOT NULL) AND ((`rows`>=0) AND (`rows`<=2147483647)))," +
-				"constraint `HistoryPurge_elapsed_Ck` check((`elapsed` IS NOT NULL) AND ((`elapsed`>=0) AND (`elapsed`<=2147483647))))" +
-				" engine=innodb"),
-			new Revision(2, "add MediaInfo.get[RedirectFrom|Moved]",
-				"alter table `HistoryModel`" +
-					" add column `mediasRedirectFrom` integer," +
-					" add column `mediasMoved` integer",
-				"alter table `HistoryMedia`" +
-					" add column `redirectFrom` integer," +
-					" add column `moved` integer"),
-			new Revision(1, "item cache renames \"cleanup\" to \"replacement\"",
-				"alter table `HistoryModel`" +
-					" change `connectPoolInvaliFromIdle` `connectioPoolInvalidOnGet` integer," +
-					" change `connectPoolInvaliIntoIdle` `connectioPoolInvalidOnPut` integer," +
-					" change `itemCacheNumberOfCleanups` `itemCacheReplacementRuns` integer," +
-					" change `itemCacheItemsCleanedUp` `itemCacheReplacements` integer",
-				"alter table `HistoryItemCache`" +
-					" change `numberOfCleanups` `replacementRuns` integer," +
-					" change `itemsCleanedUp` `replacements` integer," +
-					" change `lastCleanup` `lastReplacementRun` bigint")
+			0
 		);
 
 	private HistoryRevisions()
