48df29f1d59e12521722a231f9b795003fb91957
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-10-08 16:52:10 +0000

    use finally for discard
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@18151 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/ModifiedState.java
+++ runtime/src/com/exedio/cope/ModifiedState.java
@@ -55,21 +55,18 @@ final class ModifiedState extends State
 	@Override
 	State write(final Transaction transaction, final Map<BlobColumn, byte[]> blobs)
 	{
+		boolean discard = true;
 		try
 		{
 			type.getModel().connect().database.store(transaction.getConnection(), this, true, blobs);
-			return new WrittenState(this);
+			discard = false;
 		}
-		catch ( final RuntimeException e )
+		finally
 		{
-			discard( transaction );
-			throw e;
+			if(discard)
+				discard( transaction );
 		}
-		catch ( final Error e )
-		{
-			discard( transaction );
-			throw e;
-		}
+		return new WrittenState(this);
 	}
 
 	@Override
