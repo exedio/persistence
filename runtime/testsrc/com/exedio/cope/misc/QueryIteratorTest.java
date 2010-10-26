f20eef7957caa3f5e97df0fd351f630a422357fb
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-10-26 15:49:44 +0000

    add QueryIterator
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@18255 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/testsrc/com/exedio/cope/misc/QueryIteratorTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.misc;
+
+import static com.exedio.cope.misc.QueryAggregatorItem.TYPE;
+import static com.exedio.cope.misc.QueryIterator.wrap;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import com.exedio.cope.AbstractRuntimeTest;
+import com.exedio.cope.Query;
+
+public class QueryIteratorTest extends AbstractRuntimeTest
+{
+	public QueryIteratorTest()
+	{
+		super(QueryAggregatorTest.MODEL);
+	}
+
+	QueryAggregatorItem item0, item1, item2, item3, item4, item5, item6, item7, item8, item9;
+	Query<QueryAggregatorItem> q;
+
+	@Override
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		item0 = deleteOnTearDown(new QueryAggregatorItem(0));
+		item1 = deleteOnTearDown(new QueryAggregatorItem(1));
+		item2 = deleteOnTearDown(new QueryAggregatorItem(2));
+		item3 = deleteOnTearDown(new QueryAggregatorItem(3));
+		item4 = deleteOnTearDown(new QueryAggregatorItem(4));
+		item5 = deleteOnTearDown(new QueryAggregatorItem(5));
+		item6 = deleteOnTearDown(new QueryAggregatorItem(6));
+		item7 = deleteOnTearDown(new QueryAggregatorItem(7));
+		item8 = deleteOnTearDown(new QueryAggregatorItem(8));
+		item9 = deleteOnTearDown(new QueryAggregatorItem(9));
+
+		q = TYPE.newQuery();
+		q.setOrderBy(TYPE.getThis(), true);
+	}
+
+	public void testIt()
+	{
+		try
+		{
+			wrap(null,  0);
+			fail();
+		}
+		catch(final NullPointerException e)
+		{
+			assertEquals("query", e.getMessage());
+		}
+		try
+		{
+			wrap(q,  0);
+			fail();
+		}
+		catch(final IllegalArgumentException e)
+		{
+			assertEquals("slice must be greater 0, but was 0", e.getMessage());
+		}
+
+		assertDing(listg(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9), q);
+
+		q.setLimit(4);
+		assertDing(listg(item4, item5, item6, item7, item8, item9), q);
+
+		q.setLimit(0, 6);
+		assertDing(listg(item0, item1, item2, item3, item4, item5), q);
+
+		q.setLimit(0, 5);
+		assertDing(listg(item0, item1, item2, item3, item4), q);
+
+		q.setLimit(2, 6);
+		assertDing(listg(item2, item3, item4, item5, item6, item7), q);
+
+		q.setLimit(2, 5);
+		assertDing(listg(item2, item3, item4, item5, item6), q);
+	}
+
+	private void assertDing(final List<QueryAggregatorItem> expected, final Query<QueryAggregatorItem> q)
+	{
+		assertEquals(expected, l(wrap(q,  1)));
+		assertEquals(expected, l(wrap(q,  2)));
+		assertEquals(expected, l(wrap(q,  3)));
+		assertEquals(expected, l(wrap(q,  4)));
+		assertEquals(expected, l(wrap(q,  5)));
+		assertEquals(expected, l(wrap(q,  6)));
+		assertEquals(expected, l(wrap(q,  7)));
+		assertEquals(expected, l(wrap(q,  8)));
+		assertEquals(expected, l(wrap(q,  9)));
+		assertEquals(expected, l(wrap(q, 10)));
+		assertEquals(expected, l(wrap(q, 11)));
+		assertEquals(expected, l(wrap(q, 12)));
+		assertEquals(expected, l(wrap(q, 13)));
+		assertEquals(expected, l(wrap(q, 14)));
+		assertEquals(expected, l(wrap(q, 15)));
+	}
+
+	private static ArrayList<QueryAggregatorItem> l(final Iterator<QueryAggregatorItem> iterator)
+	{
+		final ArrayList<QueryAggregatorItem> result = new ArrayList<QueryAggregatorItem>();
+		while(iterator.hasNext())
+			result.add(iterator.next());
+		try
+		{
+			iterator.next();
+			fail();
+		}
+		catch(final NoSuchElementException e)
+		{
+			assertEquals(null, e.getMessage());
+		}
+		return result;
+	}
+}
