f1ab92a20556dcb778b5f9f0f1a1c1137657db39
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2010-11-23 13:31:48 +0000

    split ChangeListenersTest off ChangeListenerTest
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@18495 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/utiltestsrc/com/exedio/cope/ChangeListenersTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+import com.exedio.cope.junit.CopeAssert;
+
+public class ChangeListenersTest extends CopeAssert
+{
+	final FailListener l = new FailListener();
+
+	public void testIt()
+	{
+		assertEqualsUnmodifiable(list(), model.getChangeListeners());
+		assertInfo(0, 0);
+
+		model.addChangeListener(l);
+		assertEqualsUnmodifiable(list(l), model.getChangeListeners());
+		assertInfo(0, 0);
+
+		try
+		{
+			model.addChangeListener(null);
+			fail();
+		}
+		catch(final NullPointerException e)
+		{
+			assertEquals("listener", e.getMessage());
+		}
+		assertEqualsUnmodifiable(list(l), model.getChangeListeners());
+		assertInfo(0, 0);
+
+		try
+		{
+			model.removeChangeListener(null);
+			fail();
+		}
+		catch(final NullPointerException e)
+		{
+			assertEquals("listener", e.getMessage());
+		}
+		assertEqualsUnmodifiable(list(l), model.getChangeListeners());
+		assertInfo(0, 0);
+
+		assertInfo(0, 0);
+		model.removeChangeListener(l);
+		assertEqualsUnmodifiable(list(), model.getChangeListeners());
+		assertInfo(0, 1);
+
+		// test weakness
+		FailListener l1 = new FailListener();
+		model.addChangeListener(l1);
+		assertEquals(list(l1), model.getChangeListeners());
+		assertInfo(0, 1);
+
+		System.gc();
+		assertEquals(list(l1), model.getChangeListeners());
+		assertInfo(0, 1);
+
+		l1 = null;
+		System.gc();
+		assertInfo(0, 1);
+		assertEquals(list(), model.getChangeListeners());
+		assertInfo(1, 1);
+
+		final FailListener l2 = new FailListener();
+		model.addChangeListener(l2);
+		model.addChangeListener(new FailListener());
+		System.gc();
+		model.removeChangeListener(l2);
+		assertInfo(2, 2);
+		assertEquals(list(), model.getChangeListeners());
+		assertInfo(2, 2);
+	}
+
+	private final class FailListener implements ChangeListener
+	{
+		FailListener()
+		{
+			// make constructor non-private
+		}
+
+		public void onChange(final ChangeEvent event)
+		{
+			System.out.println("ERROR: MockListener.onChange");
+			fail();
+		}
+	}
+
+	private void assertInfo(final int cleared, final int removed)
+	{
+		final ChangeListenerInfo info = model.getChangeListenersInfo();
+		assertEquals(cleared, info.getCleared());
+		assertEquals(removed, info.getRemoved());
+
+		@SuppressWarnings("deprecation")
+		final int clearedDeprecated = model.getChangeListenersCleared();
+		assertEquals(cleared, clearedDeprecated);
+	}
+
+	private static final Type TYPE = TypesBound.newType(AnItem.class);
+	private static final Model model = new Model(TYPE);
+
+	private static class AnItem extends Item
+	{
+		private static final long serialVersionUID = 1l;
+
+		private AnItem(final ActivationParameters ap)
+		{
+			super(ap);
+		}
+	}
+}
