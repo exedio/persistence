54460b8e6d39b1a4b82c184b633abee92994e552
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-02-01 17:47:28 +0000

    add disabled test for archiv
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@18911 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/testsrc/com/exedio/cope/CacheBadReadTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+
+public class CacheBadReadTest extends AbstractRuntimeTest
+{
+	public CacheBadReadTest()
+	{
+		super(CacheIsolationTest.MODEL);
+		skipTransactionManagement();
+	}
+
+	CacheIsolationItem item;
+	ThreadStoppable threadRead;
+
+	@Override
+	public void setUp() throws Exception
+	{
+		super.setUp();
+
+		model.startTransaction("CacheBadReadTest setUp");
+		try
+		{
+			item = deleteOnTearDown(new CacheIsolationItem("itemName"));
+			model.commit();
+		}
+		finally
+		{
+			model.rollbackIfNotCommitted();
+		}
+	}
+
+	@Override
+	public void tearDown() throws Exception
+	{
+		if(threadRead!=null)
+			threadRead.join();
+		threadRead = null;
+
+		super.tearDown();
+	}
+
+	static class ThreadStoppable extends Thread
+	{
+		boolean proceed = true;
+	}
+
+	public void testIt() throws InterruptedException
+	{
+		if(hsqldb||oracle) return; // TODO
+
+		threadRead = new ThreadStoppable(){
+			@Override
+			public void run()
+			{
+				int i;
+				for(i = 0; i<10000000 && proceed; i++)
+				{
+					//if(i%100==0 || i<20) System.out.println("CacheBadReadTest read " + i);
+					Thread.yield();
+					model.startTransaction("CacheBadReadTest  read " + i);
+					final String name = item.getName();
+					assertTrue(name, name.startsWith("itemName"));
+					model.commit();
+				}
+				System.out.println("CacheBadReadTest read fertig " + i);
+			}
+		};
+
+		threadRead.start();
+
+		int i = 0;
+		try
+		{
+			for(; i<10000; i++)
+			{
+				//if(i%100==0 || i<20) System.out.println("CacheBadReadTest write " + i);
+				Thread.yield();
+				model.startTransaction("CacheBadReadTest write " + i);
+				item.setName("itemName" + i);
+				model.commit();
+			}
+		}
+		catch(final TemporaryTransactionException e)
+		{
+			System.out.println("" + i + " " + e.getMessage());
+		}
+
+
+		threadRead.proceed = false;
+		threadRead.join();
+		threadRead = null;
+	}
+}
