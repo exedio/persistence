234e1ea7dc695fa0681f7588e5a1eea89528e425
Frank Finger <frank.finger@exedio.com>
2011-02-11 14:36:09 +0000

    remove '-' from JavaView get method names
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@19025 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/patternsrc/com/exedio/cope/pattern/JavaView.java
+++ runtime/patternsrc/com/exedio/cope/pattern/JavaView.java
@@ -40,7 +40,7 @@ public final class JavaView extends Pattern
 		Mount(final Type<? extends Item> type, final String name)
 		{
 			final String nameUpper =
-				Character.toUpperCase(name.charAt(0)) + name.substring(1);
+				(Character.toUpperCase(name.charAt(0)) + name.substring(1)).replaceAll("-", "");
 
 			final Class<?> javaClass = type.getJavaClass();
 			final Method getter;
