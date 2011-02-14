4e3466ee273660b392e6aa06c58db019ac7a55ac
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-02-14 12:46:43 +0000

    Invalidate is first
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@19038 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/sampler/SamplerClusterNode.java
+++ runtime/servletsrc/com/exedio/cope/sampler/SamplerClusterNode.java
@@ -68,9 +68,9 @@ final class SamplerClusterNode extends Item
 	private static final DateField firstEncounter = new DateField().toFinal();
 	private static final StringField fromAddress = new StringField().toFinal();
 	private static final IntegerField fromPort = new IntegerField().toFinal().range(0, 0xffff);
+	private static final CompositeField<SequenceInfo> invalidate = CompositeField.newComposite(SequenceInfo.class).toFinal();
 	private static final CompositeField<SequenceInfo> ping = CompositeField.newComposite(SequenceInfo.class).toFinal();
 	private static final CompositeField<SequenceInfo> pong = CompositeField.newComposite(SequenceInfo.class).toFinal();
-	private static final CompositeField<SequenceInfo> invalidate = CompositeField.newComposite(SequenceInfo.class).toFinal();
 
 	static List<SetValue> map(final ClusterListenerInfo.Node node)
 	{
@@ -80,9 +80,9 @@ final class SamplerClusterNode extends Item
 				fromAddress   .map(node.getAddress().toString()),
 				fromPort      .map(node.getPort()),
 
+				invalidate.map(new SequenceInfo(node.getInvalidateInfo())),
 				ping      .map(new SequenceInfo(node.getPingInfo())),
-				pong      .map(new SequenceInfo(node.getPingInfo())),
-				invalidate.map(new SequenceInfo(node.getInvalidateInfo())));
+				pong      .map(new SequenceInfo(node.getPingInfo())));
 	}
 
 
