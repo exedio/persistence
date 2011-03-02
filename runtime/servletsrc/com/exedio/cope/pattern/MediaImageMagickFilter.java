bdefbaad72c9961f5140bd3c30419c3d1e649a72
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-03-02 12:47:01 +0000

    format code
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@19172 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/pattern/MediaImageMagickFilter.java
+++ runtime/servletsrc/com/exedio/cope/pattern/MediaImageMagickFilter.java
@@ -346,7 +346,14 @@ public class MediaImageMagickFilter extends MediaFilter
 		final ProcessBuilder processBuilder = new ProcessBuilder(command);
 		final int exitValue;
 		final Process process = processBuilder.start();
-		try { exitValue = process.waitFor(); } catch(final InterruptedException e) { throw new RuntimeException(toString(), e); }
+		try
+		{
+			exitValue = process.waitFor();
+		}
+		catch(final InterruptedException e)
+		{
+			throw new RuntimeException(toString(), e);
+		}
 
 		// IMPLEMENTATION NOTE
 		// Without the following three lines each run of this code will leave
