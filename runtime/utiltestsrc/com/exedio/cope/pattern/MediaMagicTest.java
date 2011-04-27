0f5d740fca50192b64d7a454167e65e376947449
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-04-27 12:33:59 +0000

    add MediaMagicTest
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@19545 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/utiltestsrc/com/exedio/cope/pattern/MediaMagicTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2004-2011  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.pattern;
+
+import com.exedio.cope.DataField;
+import com.exedio.cope.StringField;
+import com.exedio.cope.junit.CopeAssert;
+
+public class MediaMagicTest extends CopeAssert
+{
+	public void testIt()
+	{
+		final Media m = new Media();
+		final DataField b = m.getBody();
+		final StringField c = (StringField)m.getContentType();
+		assertEquals(
+				"(" +
+				"(("+c+"='image/jpeg' OR "+c+"='image/pjpeg') AND !("+b+" startsWith 'ffd8ff')) OR " +
+				"("+c+"='image/gif' AND !("+b+" startsWith '47494638')) OR " +
+				"("+c+"='image/png' AND !("+b+" startsWith '89504e470d0a1a0a')) OR " +
+				"(("+c+"='image/icon' OR "+c+"='image/x-icon' OR "+c+"='image/vnd.microsoft.icon') AND !("+b+" startsWith '00000100')) OR " +
+				"(("+c+"='application/zip' OR "+c+"='application/java-archive') AND !("+b+" startsWith '504b0304')) OR " +
+				"("+c+"='application/pdf' AND !("+b+" startsWith '25504446'))" +
+				")",
+				m.bodyMismatchesContentType().toString());
+	}
+}
