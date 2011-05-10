0365b330cefd71bb6f9196ab610351a173b6b872
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-05-10 12:25:56 +0000

    more type-safe
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/reflectionWrapper@19708 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/instrument/Wrapper.java
+++ runtime/src/com/exedio/cope/instrument/Wrapper.java
@@ -377,7 +377,7 @@ public final class Wrapper
 		return new ExtendsType(rawType, actualTypeArguments);
 	}
 
-	public static List<Wrapper> makeByReflection(final Class<? extends Feature> clazz, final Feature feature, final List<Wrapper> superResult)
+	public static <F extends Feature> List<Wrapper> makeByReflection(final Class<F> clazz, final F feature, final List<Wrapper> superResult)
 	{
 		final WrapperByReflection factory = new WrapperByReflection(clazz, feature);
 		final ArrayList<Wrapper> result = new ArrayList<Wrapper>();
