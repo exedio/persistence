8f16790c49e6426d8eaa4720d8b96b7941dab9df
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-10-05 08:03:49 +0000

    use SetValue.map
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/setValueMap@21161 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/patternsrc/com/exedio/cope/reflect/FeatureField.java
+++ runtime/patternsrc/com/exedio/cope/reflect/FeatureField.java
@@ -181,7 +181,7 @@ public final class FeatureField<E extends Feature> extends Pattern implements Se
 		if(value==null && !optional)
 			throw MandatoryViolationException.create(this, exceptionItem);
 
-		return new SetValue[]{ idField.map(value!=null ? value.getID() : null) };
+		return new SetValue[]{ SetValue.map(idField, value!=null ? value.getID() : null) };
 	}
 
 	public List<E> getValues()
