9f4eaa6a5f2be8c614c7a2026584e0634722f524
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-01-18 11:50:31 +0000

    MxSampler added
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@22379 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/PackageTest.java
+++ runtime/testsrc/com/exedio/cope/PackageTest.java
@@ -172,6 +172,7 @@ public class PackageTest extends TestCase
 		suite.addTest( com.exedio.cope.util.PackageTest.suite() );
 		suite.addTest( com.exedio.cope.misc.PackageTest.suite() );
 		suite.addTest( com.exedio.cope.sampler.PackageTest.suite() );
+		suite.addTest( com.exedio.cope.mxsampler.PackageTest.suite() );
 
 		return suite;
 	}
