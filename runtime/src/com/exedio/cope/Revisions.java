fb0956e552a631730bfece551cfc1d93f43b1bd5
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-01-23 10:09:55 +0000

    fix javadoc
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/revisions-factory@22437 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/Revisions.java
+++ runtime/src/com/exedio/cope/Revisions.java
@@ -297,8 +297,8 @@ public final class Revisions
 
 	/**
 	 * If you supply an instance of {@link Factory} to a {@link Model}
-	 * via {@link Model#Model(Factory, Type...)} etc.
-	 * the model takes care, that {@link #get(EnvironmentInfo)}
+	 * via {@link Model#Model(Revisions.Factory, Type...)} etc.
+	 * the model takes care, that {@link #get(Context)}
 	 * is called only while the model is connected and only once for each connect.
 	 */
 	public static interface Factory
