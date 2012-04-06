5686d472309b37c8764de8044a7ce33f028cba5c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-04-06 19:38:09 +0000

    add copyright header
    git-svn-id: https://svn.exedio.com/cope/persistence/trunk@23099 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/CountSelectable.java
+++ runtime/src/com/exedio/cope/CountSelectable.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2004-2011  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package com.exedio.cope;
 
 /**
@@ -27,7 +45,7 @@ public class CountSelectable implements Selectable<Integer>
 	}
 
 	@Override
-	public void toString( StringBuilder bf, Type defaultType )
+	public void toString( final StringBuilder bf, final Type defaultType )
 	{
 		bf.append( "count(*)" );
 	}
@@ -37,7 +55,7 @@ public class CountSelectable implements Selectable<Integer>
 	 */
 	@Deprecated // OK: for internal use within COPE only
 	@Override
-	public void check( TC tc, Join join )
+	public void check( final TC tc, final Join join )
 	{
 		// TODO
 		// probably: nothing to do here, since there are no sources
@@ -48,7 +66,7 @@ public class CountSelectable implements Selectable<Integer>
 	 */
 	@Deprecated // OK: for internal use within COPE only
 	@Override
-	public void append( Statement bf, Join join )
+	public void append( final Statement bf, final Join join )
 	{
 		bf.append( "count(*)" );
 	}
@@ -58,7 +76,7 @@ public class CountSelectable implements Selectable<Integer>
 	 */
 	@Deprecated // OK: for internal use within COPE only
 	@Override
-	public void appendSelect( Statement bf, Join join )
+	public void appendSelect( final Statement bf, final Join join )
 	{
 		bf.append( "count(*)" );
 	}
