6eab438174601bf3efd2845921af31ffa3af15f0
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-09-13 10:09:17 +0000

    fix findbugs SE_BAD_FIELD from r24308: introduce VolatileLong, encapsulates VO_VOLATILE_INCREMENT, found by findbugs2
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/findbugs2@24310 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/QueryCache.java
+++ runtime/src/com/exedio/cope/QueryCache.java
@@ -259,7 +259,7 @@ final class QueryCache
 		private static final long serialVersionUID = 1l;
 
 		private final int maxSize;
-		final VolatileLong replacements = new VolatileLong();
+		transient final VolatileLong replacements = new VolatileLong();
 
 		LRUMap(final int maxSize)
 		{
