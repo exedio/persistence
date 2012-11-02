6c80628f1ec3fd593b049125dbbf78c2f8aea01c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-11-02 18:42:56 +0000

    allow secrets without context
    git-svn-id: https://svn.exedio.com/cope/persistence/branches/mediaUrlSecretNoContext@24731 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/utiltestsrc/com/exedio/cope/MediaUrlSecretTest.java
+++ runtime/utiltestsrc/com/exedio/cope/MediaUrlSecretTest.java
@@ -79,37 +79,54 @@ public class MediaUrlSecretTest extends TestCase
 				getSource(context, "MediaUrlSecretTestContext"));
 	}
 
-	public void testNoContext()
+	public void testOffNoContext()
 	{
-		final ConnectProperties props = propsNoContext();
+		final ConnectProperties props = propsNoContext(null);
 		model.connect(props);
 
+		assertEquals(false, MediaPath.isUrlGuessingPreventedSecurely(props));
+		assertEquals(false, AnItem.media.isUrlGuessingPreventedSecurely(props));
+	}
+
+	public void testOnNoContext()
+	{
+		final ConnectProperties props = propsNoContext("1234567890");
+		model.connect(props);
+
+		assertEquals(true, MediaPath.isUrlGuessingPreventedSecurely(props));
+		assertEquals(true, AnItem.media.isUrlGuessingPreventedSecurely(props));
+	}
+
+	public void testTooShortNoContext()
+	{
 		try
 		{
-			MediaPath.isUrlGuessingPreventedSecurely(props);
+			propsNoContext("123456789");
 			fail();
 		}
-		catch(final IllegalStateException e)
+		catch(final IllegalArgumentException e)
 		{
-			assertEquals("no context available", e.getMessage());
-		}
-		try
-		{
-			AnItem.media.isUrlGuessingPreventedSecurely(props);
-			fail();
-		}
-		catch(final IllegalStateException e)
-		{
-			assertEquals("no context available", e.getMessage());
+			assertEquals("media.url.secret must be at least 10 characters, but just has 9", e.getMessage());
 		}
 	}
 
-	private static ConnectProperties propsNoContext()
+	public void testEmptyNoContext()
+	{
+		final ConnectProperties props = propsNoContext("");
+		model.connect(props);
+
+		assertEquals(false, MediaPath.isUrlGuessingPreventedSecurely(props));
+		assertEquals(false, AnItem.media.isUrlGuessingPreventedSecurely(props));
+	}
+
+	private static ConnectProperties propsNoContext(final String secret)
 	{
 		final Properties source = new Properties();
 		source.setProperty("connection.url", "jdbc:hsqldb:mem:MediaUrlSecretTest");
 		source.setProperty("connection.user", "sa");
 		source.setProperty("connection.password", "");
+		if(secret!=null)
+			source.setProperty("media.url.secret", secret);
 		return new ConnectProperties(
 				getSource(source , "MediaUrlSecretTestSource"),
 				null);
@@ -118,7 +135,8 @@ public class MediaUrlSecretTest extends TestCase
 	@Override
 	protected void tearDown() throws Exception
 	{
-		model.disconnect();
+		if(model.isConnected())
+			model.disconnect();
 		super.tearDown();
 	}
 
