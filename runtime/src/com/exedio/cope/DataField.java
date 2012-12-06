649228e4c596cd7f8bae361271ffa4cc0d8b5c13
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-12-06 09:44:20 +0000

    hint about ZipEntry#getSize() - format code
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@24909 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/src/com/exedio/cope/DataField.java
+++ runtime/src/com/exedio/cope/DataField.java
@@ -470,7 +470,9 @@ public final class DataField extends Field<DataField.Value>
 		 * an {@link DataLengthViolationException} is thrown nevertheless,
 		 * but only after fetching at least {@link DataField#getMaximumLength()} bytes from the source.
 		 * A typical source of the estimated length is
-		 * {@link File#length()} or {@link HttpURLConnection#getContentLength()} or {@link ZipEntry#getSize()}.
+		 * {@link File#length()} or
+		 * {@link HttpURLConnection#getContentLength()} or
+		 * {@link ZipEntry#getSize()}.
 		 */
 		abstract long estimateLength();
 
