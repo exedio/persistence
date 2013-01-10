48e94655339447112c941d61a1d6ac76977b2ee1
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-01-10 14:11:35 +0000

    use more MediaType
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@25165 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/pattern/MediaImageMagickFilter.java
+++ runtime/servletsrc/com/exedio/cope/pattern/MediaImageMagickFilter.java
@@ -156,7 +156,7 @@ public class MediaImageMagickFilter extends MediaFilter implements MediaTestable
 		if(type==null || !supportedContentTypes.contains(type.getName())) // TODO reuse
 			return null;
 
-		return (constantOutputContentType!=null?constantOutputContentType.getName():type.getName());
+		return (constantOutputContentType!=null?constantOutputContentType:type).getName();
 	}
 
 	@Override
