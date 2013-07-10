ac0ee0a17bad8370757d41354686ff36b99d157d
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-07-10 11:26:17 +0000

    organize imports
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@26278 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/sampler/SamplerProperties.java
+++ runtime/servletsrc/com/exedio/cope/sampler/SamplerProperties.java
@@ -21,7 +21,6 @@ package com.exedio.cope.sampler;
 import com.exedio.cope.ConnectProperties;
 import com.exedio.cope.Model;
 import com.exedio.cope.misc.ConnectToken;
-import com.exedio.cope.sampler.Sampler;
 import com.exedio.cope.util.JobContext;
 import com.exedio.cope.util.Properties;
 
