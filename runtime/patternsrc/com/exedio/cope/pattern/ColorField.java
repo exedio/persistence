dd27d5c09487f0383f8f19d764a5986642fae490
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-09-27 16:57:36 +0000

    add productive code
    git-svn-id: https://svn.exedio.com/cope/cope/branches/colorField@26647 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/patternsrc/com/exedio/cope/pattern/ColorField.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2004-2012  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.pattern;
+
+import com.exedio.cope.IntegerField;
+import com.exedio.cope.IntegerRangeViolationException;
+import com.exedio.cope.Item;
+import com.exedio.cope.MandatoryViolationException;
+import com.exedio.cope.Pattern;
+import com.exedio.cope.SetValue;
+import com.exedio.cope.Settable;
+import com.exedio.cope.instrument.Wrap;
+import com.exedio.cope.misc.instrument.FinalSettableGetter;
+import com.exedio.cope.misc.instrument.InitialExceptionsSettableGetter;
+import java.awt.Color;
+import java.util.Set;
+
+public final class ColorField extends Pattern implements Settable<Color>
+{
+	private final IntegerField rgb;
+
+	public ColorField()
+	{
+		addSource(
+			this.rgb = new IntegerField().range(0, 0xffffff),
+			"rgb");
+	}
+
+	public boolean isInitial()
+	{
+		return rgb.isInitial();
+	}
+
+	public boolean isFinal()
+	{
+		return rgb.isFinal();
+	}
+
+	public boolean isMandatory()
+	{
+		return true;
+	}
+
+	@Deprecated
+	public Class<?> getInitialType()
+	{
+		return Color.class;
+	}
+
+	public Set<Class<? extends Throwable>> getInitialExceptions()
+	{
+		final Set<Class<? extends Throwable>> result = rgb.getInitialExceptions();
+		result.remove(IntegerRangeViolationException.class);
+		return result;
+	}
+
+	@Wrap(order=10,
+			doc="Returns the value of {0}.")
+	public Color get(final Item item)
+	{
+		return new Color(rgb.get(item));
+	}
+
+	@Wrap(order=20,
+			doc="Sets a new value for {0}.",
+			hide=FinalSettableGetter.class,
+			thrownGetter=InitialExceptionsSettableGetter.class)
+	public void set(final Item item, final Color value)
+	{
+		check(value, item);
+		rgb.set(item, value.getRGB());
+	}
+
+	public SetValue<Color> map(final Color value)
+	{
+		return SetValue.map(this, value);
+	}
+
+	public SetValue<?>[] execute(final Color value, final Item exceptionItem)
+	{
+		check(value, exceptionItem);
+		return new SetValue<?>[]{ rgb.map(value.getRGB()) };
+	}
+
+	private void check(final Color value, final Item exceptionItem)
+	{
+		if(value==null)
+			throw MandatoryViolationException.create(this, exceptionItem);
+		if(value.getAlpha()>0)
+			throw new ColorTransparencyViolationException(this, exceptionItem, value);
+	}
+}
