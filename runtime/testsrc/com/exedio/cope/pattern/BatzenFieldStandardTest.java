153c67eae0906f876e2447ada2d2ca77eb616143
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-10-30 16:21:28 +0000

    rename Batzen to Block (fix test)
    git-svn-id: https://svn.exedio.com/cope/cope/branches/batzen@26890 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/pattern/BatzenFieldStandardTest.java
+++ runtime/testsrc/com/exedio/cope/pattern/BatzenFieldStandardTest.java
@@ -113,7 +113,7 @@ public class BatzenFieldStandardTest extends AbstractRuntimeModelTest
 		assertEquals("AnItem.zwei#AnItem-1", b2b.toString());
 
 		// serialization
-		final ABatzen b1aS = reserialize(b1a, 710);
+		final ABatzen b1aS = reserialize(b1a, 708);
 		assertEquals(b1aS, b1a);
 		assertNotSame(b1aS, b1a);
 		assertFalse(b1aS.equals(b1b));
