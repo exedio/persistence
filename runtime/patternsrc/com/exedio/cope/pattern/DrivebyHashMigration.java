b6833521c2593eb67eb4c09a371e25fc7bfa5a53
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-03-19 09:24:21 +0000

    DrivebyHashMigration added
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@28295 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/patternsrc/com/exedio/cope/pattern/DrivebyHashMigration.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright (C) 2004-2012  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.pattern;
+
+import com.exedio.cope.CheckConstraint;
+import com.exedio.cope.Cope;
+import com.exedio.cope.Item;
+import com.exedio.cope.MandatoryViolationException;
+import com.exedio.cope.Pattern;
+import com.exedio.cope.SetValue;
+import com.exedio.cope.instrument.Wrap;
+import com.exedio.cope.misc.ComputedElement;
+import com.exedio.cope.misc.instrument.InitialExceptionsSettableGetter;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.security.SecureRandom;
+import java.util.Set;
+
+public final class DrivebyHashMigration extends Pattern implements HashInterface
+{
+	private static final long serialVersionUID = 1l;
+
+	private final Hash legacyHash;
+	private final Hash targetHash;
+	@SuppressFBWarnings("SE_BAD_FIELD") // OK: writeReplace
+
+	/**
+	 * @param legacy the algorithm the passwords are currently hashed with
+	 * @param target the algorithm the passwords are to be hashed with in the future
+	 */
+	public DrivebyHashMigration(final HashAlgorithm legacy, final HashAlgorithm target)
+	{
+		addSource(legacyHash = hash(legacy), "legacy", ComputedElement.get());
+		addSource(targetHash = hash(target), "target");
+		addSource(new CheckConstraint(
+			Cope.or(
+				legacyHash.isNull().and(targetHash.isNotNull()),
+				legacyHash.isNotNull().and(targetHash.isNull())
+			)),
+			"xor");
+	}
+
+	private static Hash hash(final HashAlgorithm algorithm)
+	{
+		return new Hash(algorithm).optional();
+	}
+
+	public Hash getLegacyHash()
+	{
+		return legacyHash;
+	}
+
+	public Hash getTargetHash()
+	{
+		return targetHash;
+	}
+
+	@Wrap(order=10,
+			doc="Returns whether the given value corresponds to the hash in {0}.")
+	public boolean check(final Item item, final String actualPlainText)
+	{
+		if(!legacyHash.isNull(item))
+		{
+			final boolean result = legacyHash.check(item, actualPlainText);
+			if(result && actualPlainText!=null)
+				set(item, actualPlainText);
+			return result;
+		}
+		else
+		{
+			return targetHash.check(item, actualPlainText);
+		}
+	}
+
+	public boolean isNull(final Item item)
+	{
+		// needs actual implementation if there is optional()
+		return false;
+	}
+
+	public String getHash(final Item item)
+	{
+		final String targetHash = this.targetHash.getHash(item);
+		if(targetHash!=null)
+			return targetHash;
+
+		return legacyHash.getHash(item);
+	}
+
+	@Wrap(order=20,
+			doc={"Wastes (almost) as much cpu cycles, as a call to <tt>check{3}</tt> would have needed.",
+					"Needed to prevent Timing Attacks."})
+	@Override
+	public void blind(final String actualPlainText)
+	{
+		targetHash.blind(actualPlainText);
+	}
+
+	@Override
+	public String newRandomPassword(final SecureRandom random)
+	{
+		return targetHash.newRandomPassword(random);
+	}
+
+	@Wrap(order=30,
+			doc="Sets a new value for {0}.",
+			thrownGetter=InitialExceptionsSettableGetter.class)
+	public void set(final Item item, final String plainText)
+	{
+		if(plainText==null)
+			throw MandatoryViolationException.create(this, item);
+
+		item.set(
+				legacyHash.map(null),
+				targetHash.map(plainText));
+	}
+
+	@Override
+	public SetValue<String> map(final String value)
+	{
+		return SetValue.map(this, value);
+	}
+
+	@Override
+	public SetValue<?>[] execute(final String value, final Item exceptionItem)
+	{
+		if(value==null)
+			throw MandatoryViolationException.create(this, exceptionItem);
+
+		return new SetValue<?>[]{
+				assertSingleton(legacyHash.execute(null,  exceptionItem)),
+				assertSingleton(targetHash.execute(value, exceptionItem))};
+	}
+
+	private static <T> T assertSingleton(final T[] array)
+	{
+		assert array.length == 1;
+		return array[0];
+	}
+
+	@Override
+	public boolean isFinal()
+	{
+		return targetHash.isFinal(); // TODO allow final as well
+	}
+
+	@Override
+	public boolean isMandatory()
+	{
+		return true; // TODO allow optional as well
+	}
+
+	@Override
+	public boolean isInitial()
+	{
+		return isMandatory() || isFinal();
+	}
+
+	@Deprecated
+	@Override
+	public java.lang.reflect.Type getInitialType()
+	{
+		return String.class;
+	}
+
+	@Override
+	public Set<Class<? extends Throwable>> getInitialExceptions()
+	{
+		final Set<Class<? extends Throwable>> result = targetHash.getInitialExceptions();
+		if(isMandatory())
+			result.add(MandatoryViolationException.class);
+		return result;
+	}
+}
---
+++ runtime/testsrc/com/exedio/cope/pattern/DrivebyHashMigrationTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2004-2012  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.pattern;
+
+import static com.exedio.cope.pattern.DrivebyHashMigrationItem.TYPE;
+import static com.exedio.cope.pattern.DrivebyHashMigrationItem.password;
+
+import com.exedio.cope.AbstractRuntimeTest;
+import com.exedio.cope.MandatoryViolationException;
+import com.exedio.cope.Model;
+
+public class DrivebyHashMigrationTest extends AbstractRuntimeTest
+{
+	private static final Model MODEL = new Model(TYPE);
+
+	public DrivebyHashMigrationTest()
+	{
+		super(MODEL);
+	}
+
+	public void testSetNull()
+	{
+		final DrivebyHashMigrationItem item = deleteOnTearDown(new DrivebyHashMigrationItem("111111", 1.1));
+
+		try
+		{
+			item.setPassword(null);
+			fail();
+		}
+		catch(final MandatoryViolationException e)
+		{
+			assertEquals("mandatory violation on " + item + " for DrivebyHashMigrationItem.password", e.getMessage());
+		}
+		try
+		{
+			item.set(password.map(null));
+			fail();
+		}
+		catch(final MandatoryViolationException e)
+		{
+			assertEquals("mandatory violation on " + item + " for DrivebyHashMigrationItem.password", e.getMessage());
+		}
+		try
+		{
+			new DrivebyHashMigrationItem(null);
+			fail();
+		}
+		catch(final MandatoryViolationException e)
+		{
+			assertEquals("mandatory violation for DrivebyHashMigrationItem.password", e.getMessage());
+		}
+	}
+
+	public void testMigratePasswordOnChange()
+	{
+		final DrivebyHashMigrationItem item = deleteOnTearDown(new DrivebyHashMigrationItem("111111", 1.1));
+		assertNotNull(password.getLegacyHash().getHash(item));
+		assertNull(password.getTargetHash().getHash(item));
+		assertEquals(password.getHash(item), password.getLegacyHash().getHash(item));
+
+		item.setPassword("222222");
+		assertNull(password.getLegacyHash().getHash(item));
+		assertNotNull(password.getTargetHash().getHash(item));
+		assertEquals(password.getHash(item), password.getTargetHash().getHash(item));
+		assertTrue(item.checkPassword("222222"));
+		assertFalse(item.checkPassword("333333"));
+		assertFalse(item.checkPassword("111111"));
+	}
+
+	public void testMigratePasswordOnCheck()
+	{
+		final DrivebyHashMigrationItem item = deleteOnTearDown(new DrivebyHashMigrationItem("111111A", 1.1));
+		assertNotNull(password.getLegacyHash().getHash(item));
+		assertNull(password.getTargetHash().getHash(item));
+		assertEquals(password.getHash(item), password.getLegacyHash().getHash(item));
+
+		assertFalse(item.checkPassword("111111Ax"));
+		assertNotNull(password.getLegacyHash().getHash(item));
+		assertNull(password.getTargetHash().getHash(item));
+		assertEquals(password.getHash(item), password.getLegacyHash().getHash(item));
+
+		assertTrue(item.checkPassword("111111A"));
+		assertNull(password.getLegacyHash().getHash(item));
+		assertNotNull(password.getTargetHash().getHash(item));
+		assertEquals(password.getHash(item), password.getTargetHash().getHash(item));
+
+		assertTrue(item.checkPassword("111111A"));
+		assertNull(password.getLegacyHash().getHash(item));
+		assertNotNull(password.getTargetHash().getHash(item));
+		assertEquals(password.getHash(item), password.getTargetHash().getHash(item));
+
+		assertFalse(item.checkPassword("111111Ax"));
+		assertNull(password.getLegacyHash().getHash(item));
+		assertNotNull(password.getTargetHash().getHash(item));
+		assertEquals(password.getHash(item), password.getTargetHash().getHash(item));
+	}
+}
