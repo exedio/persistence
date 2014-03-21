dfacdbc6a93b08c0576708ce2dfb292c6b80555e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-03-21 07:58:54 +0000

    add CopeNameUtilTest
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@28377 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ runtime/utiltestsrc/com/exedio/cope/misc/CopeNameUtilTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2004-2012  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.misc;
+
+import com.exedio.cope.ActivationParameters;
+import com.exedio.cope.CopeName;
+import com.exedio.cope.Item;
+import com.exedio.cope.StringField;
+import com.exedio.cope.Type;
+import com.exedio.cope.TypesBound;
+import com.exedio.cope.junit.CopeAssert;
+
+public class CopeNameUtilTest extends CopeAssert
+{
+	public void testIt()
+	{
+		assertEquals("naked",    AnItem.naked.getName());
+		assertEquals("nameAnno", AnItem.name .getName());
+	}
+
+	static final class AnItem extends Item
+	{
+		static final StringField naked = new StringField();
+
+		@CopeName("nameAnno")
+		static final StringField name = new StringField();
+
+		private static final long serialVersionUID = 1l;
+		static final Type<AnItem> TYPE = TypesBound.newType(AnItem.class);
+		private AnItem(final ActivationParameters ap) { super(ap); }
+	}
+}
