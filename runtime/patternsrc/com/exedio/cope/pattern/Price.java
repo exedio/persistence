337e91bf1b0c43372c2faf4aeef1da6e0876f716
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-04-28 11:13:52 +0000

    order code
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@29063 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/patternsrc/com/exedio/cope/pattern/Price.java
+++ runtime/patternsrc/com/exedio/cope/pattern/Price.java
@@ -29,10 +29,7 @@ public final class Price implements Serializable, Comparable<Price>
 {
 	private static final long serialVersionUID = 1l;
 
-	private static final double FACTOR_D = 100d;
 	private static final int    FACTOR_I = 100;
-	private static final double MIN_VALUE_D = Integer.MIN_VALUE/FACTOR_D;
-	private static final double MAX_VALUE_D = Integer.MAX_VALUE/FACTOR_D;
 	private static final BigDecimal MIN_VALUE_B = BigDecimal.valueOf(Integer.MIN_VALUE, 2);
 	private static final BigDecimal MAX_VALUE_B = BigDecimal.valueOf(Integer.MAX_VALUE, 2);
 
@@ -70,29 +67,9 @@ public final class Price implements Serializable, Comparable<Price>
 		return store!=null ? storeOf(store.intValue()) : null;
 	}
 
-	public static Price valueOf(final double value)
-	{
-		// TODO reuse common small values
-		if(Double.isNaN(value))
-			throw new IllegalArgumentException("NaN not allowed");
-		if(Double.isInfinite(value))
-			throw new IllegalArgumentException("Infinity not allowed");
-		if(value<MIN_VALUE_D)
-			throw new IllegalArgumentException("too small: " + value);
-		if(value>MAX_VALUE_D)
-			throw new IllegalArgumentException("too big: " + value);
-
-		return storeOf((int)Math.rint(100d * value));
-	}
-
 	public int store()
 	{
 		return store;
-	}
-
-	public double doubleValue()
-	{
-		return store / FACTOR_D;
 	}
 
 
@@ -359,6 +336,33 @@ public final class Price implements Serializable, Comparable<Price>
 
 		return result;
 	}
+
+
+	// conversion double
+
+	public static Price valueOf(final double value)
+	{
+		// TODO reuse common small values
+		if(Double.isNaN(value))
+			throw new IllegalArgumentException("NaN not allowed");
+		if(Double.isInfinite(value))
+			throw new IllegalArgumentException("Infinity not allowed");
+		if(value<MIN_VALUE_D)
+			throw new IllegalArgumentException("too small: " + value);
+		if(value>MAX_VALUE_D)
+			throw new IllegalArgumentException("too big: " + value);
+
+		return storeOf((int)Math.rint(100d * value));
+	}
+
+	public double doubleValue()
+	{
+		return store / FACTOR_D;
+	}
+
+	private static final double FACTOR_D = 100d;
+	private static final double MIN_VALUE_D = Integer.MIN_VALUE/FACTOR_D;
+	private static final double MAX_VALUE_D = Integer.MAX_VALUE/FACTOR_D;
 
 
 	// conversion BigDecimal
