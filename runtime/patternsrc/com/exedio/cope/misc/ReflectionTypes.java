c67fe74e5855d368a87d770a87e9dd96ce0e1293
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-05-06 15:24:06 +0000

    Settable#getInitialType() un-deprecated (fix warning about internal proprietary API)
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@29258 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/patternsrc/com/exedio/cope/misc/ReflectionTypes.java
+++ runtime/patternsrc/com/exedio/cope/misc/ReflectionTypes.java
@@ -18,7 +18,15 @@
 
 package com.exedio.cope.misc;
 
+import static com.exedio.cope.misc.Check.requireNonEmptyAndCopy;
+import static java.util.Objects.requireNonNull;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.Arrays;
+import java.util.Objects;
 
 public final class ReflectionTypes
 {
@@ -26,9 +34,98 @@ public final class ReflectionTypes
 			final Class<?> rawType,
 			final Class<?>... actualTypeArguments)
 	{
-		return sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl.
-				make(rawType, actualTypeArguments, null);
+		return new ParameterizedToplevel(actualTypeArguments, rawType);
 	}
+
+	private static final class ParameterizedToplevel implements ParameterizedType
+	{
+		private final Class<?>[] arguments;
+		private final Class<?> rawType;
+
+		ParameterizedToplevel(
+				final Class<?>[] actualTypeArguments,
+				final Class<?> rawType)
+		{
+			this.arguments = requireNonEmptyAndCopy(actualTypeArguments, "actualTypeArguments");
+			this.rawType = requireNonNull(rawType, "rawType");
+
+			{
+				final Type ownerType = rawType.getDeclaringClass();
+				if(ownerType!=null)
+					throw new IllegalArgumentException("ownerType not supported: " + ownerType);
+			}
+			{
+				final TypeVariable<?>[] parameters = rawType.getTypeParameters();
+				if(parameters.length!=actualTypeArguments.length)
+					throw new MalformedParameterizedTypeException();
+			}
+		}
+
+		public Type[] getActualTypeArguments()
+		{
+			return com.exedio.cope.misc.Arrays.copyOf(arguments);
+		}
+
+		public Class<?> getRawType()
+		{
+			return rawType;
+		}
+
+		public Type getOwnerType()
+		{
+			return null;
+		}
+
+		@Override
+		public boolean equals(final Object other)
+		{
+			if(this==other)
+				return true;
+
+			if(!(other instanceof ParameterizedType))
+				return false;
+
+			final ParameterizedType o = (ParameterizedType)other;
+			return
+					null==o.getOwnerType() &&
+					rawType.equals(o.getRawType()) &&
+					Arrays.equals(arguments, o.getActualTypeArguments());
+		}
+
+		@Override
+		public int hashCode()
+		{
+			return
+					Objects.hashCode(rawType) ^
+					Arrays.hashCode(arguments);
+		}
+
+		@Override
+		public String toString()
+		{
+			final StringBuilder bf = new StringBuilder();
+
+			bf.append(rawType.getName());
+
+			bf.append('<');
+
+			boolean first = true;
+			for(final Class<?> argument : arguments)
+			{
+				if(first)
+					first = false;
+				else
+					bf.append(", ");
+
+				bf.append(argument.getName());
+			}
+
+			bf.append('>');
+
+			return bf.toString();
+		}
+	}
+
 
 	private ReflectionTypes()
 	{
