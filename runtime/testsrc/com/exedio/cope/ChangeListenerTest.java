94daaa8eeae956512a19e5a226e4cba18ff93aee
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-06-16 15:57:24 +0000

    fix tests
    git-svn-id: https://svn.exedio.com/cope/cope/branches/changeListenerOverflowLog@29589 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/testsrc/com/exedio/cope/ChangeListenerTest.java
+++ runtime/testsrc/com/exedio/cope/ChangeListenerTest.java
@@ -136,7 +136,7 @@ public class ChangeListenerTest extends AbstractRuntimeTest
 		waitWhilePending();
 		l.assertIt(list(item1), te);
 		assertEquals(false, l.exception);
-		log.assertMessage(Level.ERROR, "change listener [" + item1.toString() + "] " + l.toString());
+		log.assertMessage(Level.ERROR, "change listener [" + item1.toString() + "] " + te.getID() + " CommitListenerTestE " + l.toString());
 
 		assertInfo(1, 0, 0, 1);
 		model.removeChangeListener(l);
@@ -164,7 +164,6 @@ public class ChangeListenerTest extends AbstractRuntimeTest
 			assertNotNull(items);
 			assertTrue(!items.isEmpty());
 			assertUnmodifiable(items);
-			assertEquals(items.toString(), event.toString());
 
 			try
 			{
@@ -207,7 +206,7 @@ public class ChangeListenerTest extends AbstractRuntimeTest
 				assertEquals(expectedTransaction.getID(), event.getTransactionID());
 				assertEquals(expectedTransaction.getName(), event.getTransactionName());
 				assertEquals(expectedTransaction.getStartDate(), event.getTransactionStartDate());
-				assertEquals(event.getItems().toString(), event.toString());
+				assertEquals(event.getItems().toString() + ' ' + expectedTransaction.getID() + ' ' + expectedTransaction.getName(), event.toString());
 				assertNull(expectedTransaction.getBoundThread());
 				assertTrue(expectedTransaction.isClosed());
 			}
