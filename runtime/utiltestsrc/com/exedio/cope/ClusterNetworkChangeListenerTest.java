9a0dfe7dcd3914d7bbe8898f8cadfc922acb5e94
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-06-16 14:13:33 +0000

    test ChangeEvent.toString()
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@29585 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/utiltestsrc/com/exedio/cope/ClusterNetworkChangeListenerTest.java
+++ runtime/utiltestsrc/com/exedio/cope/ClusterNetworkChangeListenerTest.java
@@ -129,6 +129,7 @@ public class ClusterNetworkChangeListenerTest extends ClusterNetworkTest
 			assertEquals(expectedTransaction.getID(), event.getTransactionID());
 			assertEquals(expectedTransaction.getName(), event.getTransactionName());
 			assertEquals(expectedTransaction.getStartDate(), event.getTransactionStartDate());
+			assertEquals(expectedItems + "", event.toString());
 			assertNull(expectedTransaction.getBoundThread());
 			assertTrue(expectedTransaction.isClosed());
 
@@ -178,6 +179,7 @@ public class ClusterNetworkChangeListenerTest extends ClusterNetworkTest
 			{
 				assertEquals("remote", e.getMessage());
 			}
+			assertEquals(expectedItems + "", event.toString());
 
 			event = null;
 		}
