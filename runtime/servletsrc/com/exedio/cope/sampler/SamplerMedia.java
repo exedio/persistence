ed35867dd07fbb8834d9c91b17da11d1418696a0
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-06-19 11:41:03 +0000

    use SetValue.map
    git-svn-id: https://svn.exedio.com/cope/cope/trunk@29632 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- runtime/servletsrc/com/exedio/cope/sampler/SamplerMedia.java
+++ runtime/servletsrc/com/exedio/cope/sampler/SamplerMedia.java
@@ -18,6 +18,7 @@
 
 package com.exedio.cope.sampler;
 
+import static com.exedio.cope.SetValue.map;
 import static com.exedio.cope.sampler.Util.field;
 import static com.exedio.cope.sampler.Util.maD;
 import static com.exedio.cope.sampler.Util.maS;
@@ -50,8 +51,8 @@ final class SamplerMedia extends Item
 	@SuppressWarnings("unchecked") static List<SetValue<?>> mapIt(final SamplerModel m)
 	{
 		return Arrays.asList((SetValue<?>)
-			model         .map(m),
-			date          .map(SamplerModel.date.get(m)));
+			map(model, m),
+			map(date,  SamplerModel.date.get(m)));
 	}
 
 
