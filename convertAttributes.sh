d8b9196545da8dd6bf5b4a7825dd657f743386e9
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-09-25 12:55:18 +0200

    remove convertAttributes.sh, has been done long time ago

--- convertAttributes.sh
+++
@@ -1,9 +0,0 @@
-for i in `find . -name *.java`
-do
-	#echo $i
-	sed -e 's/new *\(com.exedio.cope.\)\?\(Enum\|Item\)Attribute *< *\([0-9,A-Z,a-z,.]*\) *>\( *\)(\( *\)\([A-Z,_]*\)\( *\(, *[A-Z]* *\)\?\))/new\2Attribute\4(\5\6, \3.class\7)/g' \
-		< $i \
-		> $i.sed
-	diff $i $i.sed
-	mv -f $i.sed $i
-done
