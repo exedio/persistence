b75e9d37ab5976ef8028382cce66bec145639158
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-10-01 10:49:49 +0200

    format code

--- runtime/src/com/exedio/cope/CopeCacheWeight.java
+++ runtime/src/com/exedio/cope/CopeCacheWeight.java
@@ -24,7 +24,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({TYPE})
+@Target(TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CopeCacheWeight
 {
