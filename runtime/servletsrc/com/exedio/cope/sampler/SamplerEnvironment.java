0cf0714058a7dbed7b5a312185343cda79fef627
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-11-17 12:03:01 +0100

    write production code, not yet tested

--- runtime/servletsrc/com/exedio/cope/sampler/SamplerEnvironment.java
+++ runtime/servletsrc/com/exedio/cope/sampler/SamplerEnvironment.java
@@ -27,6 +27,7 @@ import com.exedio.cope.EnvironmentInfo;
 import com.exedio.cope.IntegerField;
 import com.exedio.cope.Item;
 import com.exedio.cope.Model;
+import com.exedio.cope.RevisionInfo;
 import com.exedio.cope.SetValue;
 import com.exedio.cope.StringField;
 import com.exedio.cope.Type;
@@ -35,6 +36,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 final class SamplerEnvironment extends Item
 {
@@ -57,6 +60,8 @@ final class SamplerEnvironment extends Item
 		addEnvironmentInfo(sv, model);
 		addBuildTag(sv, buildTag);
 		TYPE.newItem(sv);
+
+		addFromRevisions(model);
 	}
 
 	private static SamplerEnvironment forConnectDate(final Date connectDate)
@@ -79,6 +84,11 @@ final class SamplerEnvironment extends Item
 		}
 	}
 
+	private static void addHostname(final ArrayList<SetValue<?>> sv, final Map<String, String> environment)
+	{
+		sv.add(hostname.map(getFromInfo(environment, "hostname", "hostname")));
+	}
+
 
 	private static final StringField connectionUrl      = new StringField().toFinal().lengthMax(1000);
 	private static final StringField connectionUsername = new StringField().toFinal().lengthMax(1000);
@@ -88,6 +98,12 @@ final class SamplerEnvironment extends Item
 		final ConnectProperties p = model.getConnectProperties();
 		sv.add(connectionUrl     .map(p.getConnectionUrl     ()));
 		sv.add(connectionUsername.map(p.getConnectionUsername()));
+	}
+
+	private static void addConnection(final ArrayList<SetValue<?>> sv, final Map<String, String> environment)
+	{
+		sv.add(connectionUrl     .map(getFromInfo(environment, "connection.url",  "jdbc.url" )));
+		sv.add(connectionUsername.map(getFromInfo(environment, "connection.user", "jdbc.user")));
 	}
 
 
@@ -113,12 +129,80 @@ final class SamplerEnvironment extends Item
 		sv.add(driverVersionMinor    .map(i.getDriverMinorVersion    ()));
 	}
 
+	private static final void addEnvironmentInfo(final ArrayList<SetValue<?>> sv, final Map<String, String> environment)
+	{
+		sv.add(databaseProductName   .map(getFromInfo(environment, "database.name",  "database.name" )));
+		sv.add(databaseProductVersion.map(getFromInfo(environment, "database.version",  "database.version" )));
+		sv.add(databaseVersionMajor  .map(getFromInfoInt(environment, "database.version.major",  "database.version.major" )));
+		sv.add(databaseVersionMinor  .map(getFromInfoInt(environment, "database.version.minor",  "database.version.minor" )));
+		sv.add(driverName            .map(getFromInfo(environment, "driver.name",  "driver.name" )));
+		sv.add(driverVersion         .map(getFromInfo(environment, "driver.version",  "driver.version" )));
+		sv.add(driverVersionMajor    .map(getFromInfoInt(environment, "driver.version.major",  "driver.version.major" )));
+		sv.add(driverVersionMinor    .map(getFromInfoInt(environment, "driver.version.minor",  "driver.version.minor" )));
+	}
+
 
 	private static final StringField buildTag = new StringField().toFinal().optional().lengthMax(1000);
 
 	private static final void addBuildTag(final ArrayList<SetValue<?>> sv, final String buildTag)
 	{
 		sv.add(maC(SamplerEnvironment.buildTag, buildTag));
+	}
+
+
+	private static void addFromRevisions(final Model model)
+	{
+		if(model.getRevisions()==null)
+			return;
+
+		final TreeMap<Date, ArrayList<RevisionInfo>> revisions = new TreeMap<>();
+		final Map<Integer, byte[]> logs = model.getRevisionLogs();
+
+		for(final Map.Entry<Integer, byte[]> entry : logs.entrySet())
+		{
+			final RevisionInfo info = RevisionInfo.read(entry.getValue());
+			if(info!=null)
+			{
+				final Date date = info.getDate();
+				ArrayList<RevisionInfo> list = revisions.get(date);
+				if(list==null)
+				{
+					list = new ArrayList<>();
+					revisions.put(date, list);
+				}
+				list.add(info);
+			}
+		}
+
+		for(final ArrayList<RevisionInfo> list : revisions.values())
+		{
+			final RevisionInfo info = list.get(0);
+			final Date date = info.getDate();
+			final Map<String, String> environment = info.getEnvironment();
+
+			final ArrayList<SetValue<?>> sv = new ArrayList<>();
+			sv.add(SamplerEnvironment.connectDate.map(date));
+			sv.add(initializeDate.map(date));
+			addHostname(sv, environment);
+			addConnection(sv, environment);
+			addEnvironmentInfo(sv, environment);
+			addBuildTag(sv, RevisionInfo.class.getName() + ' ' + info.getNumber() + '-' + list.get(list.size()-1).getNumber());
+			TYPE.newItem(sv);
+		}
+	}
+
+	private static String getFromInfo(final Map<String, String> environment, final String key, final String deprecatedKey)
+	{
+		final String value = environment.get(key);
+		if(value!=null)
+			return value;
+
+		return environment.get(deprecatedKey);
+	}
+
+	private static int getFromInfoInt(final Map<String, String> environment, final String key, final String deprecatedKey)
+	{
+		return Integer.parseInt(getFromInfo(environment, key, deprecatedKey));
 	}
 
 
