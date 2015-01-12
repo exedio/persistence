51c8b36d2a058fbfb7b8779b159a225c94e2d562
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-01-12 20:08:44 +0100

    fix findbugs ITA_INEFFICIENT_TO_ARRAY

--- runtime/testsrc/com/exedio/cope/SchemaTypeIntegerItem.java
+++ runtime/testsrc/com/exedio/cope/SchemaTypeIntegerItem.java
@@ -42,7 +42,7 @@ public final class SchemaTypeIntegerItem extends Item
 
 	SchemaTypeIntegerItem(final ArrayList<SetValue<?>> sv)
 	{
-		super(sv.toArray(new SetValue<?>[0]));
+		super(sv.toArray(new SetValue<?>[sv.size()]));
 	}
 
 	/**
