73b2f71e4dccae6dc2ff83d70c7a5bc6090fb823
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-02-13 14:50:43 +0100

    MediaUrlFingerOffset: prepare more testing

--- runtime/testsrc/com/exedio/cope/pattern/MediaUrlFingerOffsetTest.java
+++ runtime/testsrc/com/exedio/cope/pattern/MediaUrlFingerOffsetTest.java
@@ -39,16 +39,15 @@ public final class MediaUrlFingerOffsetTest extends AbstractRuntimeModelTest
 	{
 		super.setUp();
 		item = new MediaUrlItem(null);
-	}
-
-	public void testIt()
-	{
 		final byte[] bytes  = {-86,122,-8,23};
 		item.setFotoFinger(bytes, "image/jpeg");
 		item.setFileFinger(bytes, "foo/bar");
 		fotoFinger.getLastModified().set(item, new Date(23 + 128)); // XC
 		fileFinger.getLastModified().set(item, new Date(23 + 192)); // XD
+	}
 
+	public void testIt()
+	{
 		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item + ".jpg", item.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item,          item.getFileFingerLocator());
 	}
