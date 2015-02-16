2e72b4a2370defd07507c4bae5a9130e5aebb22d
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-02-16 20:20:19 +0100

    MediaUrlFingerOffset: fix synchronization

--- runtime/src/com/exedio/cope/pattern/MediaFingerprintOffset.java
+++ runtime/src/com/exedio/cope/pattern/MediaFingerprintOffset.java
@@ -25,21 +25,16 @@ import com.exedio.cope.Item;
 
 public final class MediaFingerprintOffset
 {
-	private final int initialValue;
-	private int value;
-	private int ramp;
-	private static final int MAX_RAMP = 999;
+	private volatile State state;
 
 	public MediaFingerprintOffset(final int value)
 	{
-		this.initialValue = requireNonNegative(value, "value");
-		reset();
+		this.state = new State(value);
 	}
 
 	public void reset()
 	{
-		this.value = initialValue;
-		this.ramp = 0;
+		this.state = state.reset();
 	}
 
 	/**
@@ -47,34 +42,17 @@ public final class MediaFingerprintOffset
 	 */
 	public boolean isInitial()
 	{
-		return
-			value==initialValue &&
-			ramp==0;
+		return state.isInitial();
 	}
 
 	public String getInfo()
 	{
-		final StringBuilder bf = new StringBuilder();
-		bf.append(value);
-
-		if(ramp!=0)
-			bf.append(" ramp ").
-				append(ramp).
-				append('/').
-				append(MAX_RAMP+1);
-
-		if(value!=initialValue)
-			bf.append(" initially ").
-				append(initialValue);
-
-		return bf.toString();
+		return state.getInfo();
 	}
 
 	public void setValueAndResetRamp(final int value)
 	{
-		requireNonNegative(value, "value");
-		this.value = value;
-		this.ramp = 0; // TODO do some synchronization
+		state = state.setValueAndResetRamp(value);
 	}
 
 	/**
@@ -82,30 +60,95 @@ public final class MediaFingerprintOffset
 	 */
 	public void setRamp(final double ramp)
 	{
-		if(! (0.0<=ramp && ramp<=1.0) )
-			throw new IllegalArgumentException("ramp must be between 0.0 and 1.0, but was " + String.valueOf(ramp));
-
-		int rampInt = (int)Math.round( ramp*(MAX_RAMP+1) );
-		if(rampInt>MAX_RAMP)
-			rampInt = MAX_RAMP;
-
-		this.ramp = rampInt;
+		state = state.setRamp(ramp);
 	}
 
 	int get(final Item item)
 	{
-		if(ramp==0)
-			return value;
-
-		return
-			(getPrimaryKeyColumnValue(item)<ramp)
-			? (value + 1)
-			: value;
+		return state.get(item);
 	}
 
 	@Override
 	public String toString()
 	{
-		return getInfo();
+		return state.getInfo();
+	}
+
+	private static final class State
+	{
+		private final int initialValue;
+		private final int value;
+		private final int ramp;
+		private static final int MAX_RAMP = 999;
+
+		State(final int value)
+		{
+			this(requireNonNegative(value, "value"), value, 0);
+		}
+
+		private State(final int initialValue, final int value, final int ramp)
+		{
+			this.initialValue = requireNonNegative(initialValue, "value");
+			this.value = requireNonNegative(value, "value");
+			this.ramp = ramp;
+		}
+
+		State reset()
+		{
+			return new State(initialValue, initialValue, 0);
+		}
+
+		boolean isInitial()
+		{
+			return
+				value==initialValue &&
+				ramp==0;
+		}
+
+		String getInfo()
+		{
+			final StringBuilder bf = new StringBuilder();
+			bf.append(value);
+
+			if(ramp!=0)
+				bf.append(" ramp ").
+					append(ramp).
+					append('/').
+					append(MAX_RAMP+1);
+
+			if(value!=initialValue)
+				bf.append(" initially ").
+					append(initialValue);
+
+			return bf.toString();
+		}
+
+		State setValueAndResetRamp(final int value)
+		{
+			return new State(initialValue, value, 0);
+		}
+
+		State setRamp(final double ramp)
+		{
+			if(! (0.0<=ramp && ramp<=1.0) )
+				throw new IllegalArgumentException("ramp must be between 0.0 and 1.0, but was " + String.valueOf(ramp));
+
+			int rampInt = (int)Math.round( ramp*(MAX_RAMP+1) );
+			if(rampInt>MAX_RAMP)
+				rampInt = MAX_RAMP;
+
+			return new State(initialValue, value, rampInt);
+		}
+
+		int get(final Item item)
+		{
+			if(ramp==0)
+				return value;
+
+			return
+				(getPrimaryKeyColumnValue(item)<ramp)
+				? (value + 1)
+				: value;
+		}
 	}
 }
