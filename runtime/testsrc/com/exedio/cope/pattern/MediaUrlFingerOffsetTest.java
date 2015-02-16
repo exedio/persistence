d272e0a13d8be9c2483d5ec6be1ceb957b3c9b2b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-02-16 18:03:38 +0100

    MediaUrlFingerOffset: do not test with fotoFinger anymore

--- runtime/testsrc/com/exedio/cope/pattern/MediaUrlFingerOffsetTest.java
+++ runtime/testsrc/com/exedio/cope/pattern/MediaUrlFingerOffsetTest.java
@@ -20,7 +20,6 @@ package com.exedio.cope.pattern;
 
 import static com.exedio.cope.pattern.MediaLocatorAssert.assertLocator;
 import static com.exedio.cope.pattern.MediaUrlItem.fileFinger;
-import static com.exedio.cope.pattern.MediaUrlItem.fotoFinger;
 
 import com.exedio.cope.AbstractRuntimeModelTest;
 import java.util.Date;
@@ -49,110 +48,67 @@ public final class MediaUrlFingerOffsetTest extends AbstractRuntimeModelTest
 	{
 		final MediaUrlItem item = new MediaUrlItem(null);
 		final byte[] bytes  = {-86,122,-8,23};
-		item.setFotoFinger(bytes, "image/jpeg");
 		item.setFileFinger(bytes, "foo/bar");
-		fotoFinger.getLastModified().set(item, new Date(23 + 128)); // XC
 		fileFinger.getLastModified().set(item, new Date(23 + 192)); // XD
 		return item;
 	}
 
 	public void testTouchLastModified()
 	{
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item1 + ".jpg", item1.getFotoFingerLocator());
-		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item1,          item1.getFileFingerLocator());
-
-		fotoFinger.getLastModified().set(item1, new Date(24 + 128)); // YC
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item1,          item1.getFileFingerLocator());
 
 		fileFinger.getLastModified().set(item1, new Date(24 + 192)); // YD
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item1,          item1.getFileFingerLocator());
 	}
 
 	public void testGlobalOffset()
 	{
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item1,          item1.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item2 + ".jpg", item2.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item2,          item2.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item3 + ".jpg", item3.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item3,          item3.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item4 + ".jpg", item4.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item4,          item4.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item5 + ".jpg", item5.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item5,          item5.getFileFingerLocator());
 
 		setOffset(1, 0);
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item1,          item1.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item2 + ".jpg", item2.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item2,          item2.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item3 + ".jpg", item3.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item3,          item3.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item4 + ".jpg", item4.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item4,          item4.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item5 + ".jpg", item5.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item5,          item5.getFileFingerLocator());
 
 		setOffset(3, 0);
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.faC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.faD/" + item1,          item1.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.faC/" + item2 + ".jpg", item2.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.faD/" + item2,          item2.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.faC/" + item3 + ".jpg", item3.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.faD/" + item3,          item3.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.faC/" + item4 + ".jpg", item4.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.faD/" + item4,          item4.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.faC/" + item5 + ".jpg", item5.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.faD/" + item5,          item5.getFileFingerLocator());
 
 		setOffset(0, 0);
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item1,          item1.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item2 + ".jpg", item2.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item2,          item2.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item3 + ".jpg", item3.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item3,          item3.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item4 + ".jpg", item4.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item4,          item4.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item5 + ".jpg", item5.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item5,          item5.getFileFingerLocator());
 
 		setOffset(0, 1);
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item1,          item1.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item2 + ".jpg", item2.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item2,          item2.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item3 + ".jpg", item3.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item3,          item3.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item4 + ".jpg", item4.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item4,          item4.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item5 + ".jpg", item5.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item5,          item5.getFileFingerLocator());
 
 		setOffset(0, 2);
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item1,          item1.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item2 + ".jpg", item2.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item2,          item2.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item3 + ".jpg", item3.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item3,          item3.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item4 + ".jpg", item4.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item4,          item4.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item5 + ".jpg", item5.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item5,          item5.getFileFingerLocator());
 
 		setOffset(0, 4);
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item1 + ".jpg", item1.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item1,          item1.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item2 + ".jpg", item2.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item2,          item2.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item3 + ".jpg", item3.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item3,          item3.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fYC/" + item4 + ".jpg", item4.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fYD/" + item4,          item4.getFileFingerLocator());
-		assertLocator(fotoFinger, "MediaUrlItem/fotoFinger/.fXC/" + item5 + ".jpg", item5.getFotoFingerLocator());
 		assertLocator(fileFinger, "MediaUrlItem/fileFinger/.fXD/" + item5,          item5.getFileFingerLocator());
 	}
 
