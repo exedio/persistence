29a26034fbd04fbbb91f81158e50aa233ee41ed0
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-02-18 11:54:01 +0100

    MediaUrlFingerOffset: local rename, prepare using array

--- runtime/testsrc/com/exedio/cope/pattern/MediaUrlFingerOffsetTest.java
+++ runtime/testsrc/com/exedio/cope/pattern/MediaUrlFingerOffsetTest.java
@@ -31,17 +31,17 @@ public final class MediaUrlFingerOffsetTest extends AbstractRuntimeModelTest
 		super(MediaUrlModelTest.MODEL);
 	}
 
-	private MediaUrlItem item1, item2, item3, item4, item5;
+	private MediaUrlItem item0, item1, item2, item3, item4;
 
 	@Override
 	public void setUp() throws Exception
 	{
 		super.setUp();
+		item0 = create();
 		item1 = create();
 		item2 = create();
 		item3 = create();
 		item4 = create();
-		item5 = create();
 	}
 
 	private static MediaUrlItem create()
@@ -55,121 +55,121 @@ public final class MediaUrlFingerOffsetTest extends AbstractRuntimeModelTest
 
 	public void testTouchLastModified()
 	{
-		assertIt(".fXD", item1);
+		assertIt(".fXD", item0);
 
-		fileFinger.getLastModified().set(item1, new Date(24 + 192)); // YD
-		assertIt(".fYD", item1);
+		fileFinger.getLastModified().set(item0, new Date(24 + 192)); // YD
+		assertIt(".fYD", item0);
 	}
 
 	public void testGlobalOffsetValue()
 	{
+		assertIt(".fXD", item0);
 		assertIt(".fXD", item1);
 		assertIt(".fXD", item2);
 		assertIt(".fXD", item3);
 		assertIt(".fXD", item4);
-		assertIt(".fXD", item5);
 
 		setValueAndResetRamp(1, "1 (initially 0)");
+		assertIt(".fYD", item0);
 		assertIt(".fYD", item1);
 		assertIt(".fYD", item2);
 		assertIt(".fYD", item3);
 		assertIt(".fYD", item4);
-		assertIt(".fYD", item5);
 
 		setValueAndResetRamp(3, "3 (initially 0)");
+		assertIt(".faD", item0);
 		assertIt(".faD", item1);
 		assertIt(".faD", item2);
 		assertIt(".faD", item3);
 		assertIt(".faD", item4);
-		assertIt(".faD", item5);
 
 		setValueAndResetRamp(0, "0");
+		assertIt(".fXD", item0);
 		assertIt(".fXD", item1);
 		assertIt(".fXD", item2);
 		assertIt(".fXD", item3);
 		assertIt(".fXD", item4);
-		assertIt(".fXD", item5);
 	}
 
 	public void testGlobalOffsetRamp()
 	{
+		assertIt(".fXD", item0);
 		assertIt(".fXD", item1);
 		assertIt(".fXD", item2);
 		assertIt(".fXD", item3);
 		assertIt(".fXD", item4);
-		assertIt(".fXD", item5);
 
 		setRamp(1, "0 ramp 1/1000");
+		assertIt(".fYD", item0);
+		assertIt(".fXD", item1);
+		assertIt(".fXD", item2);
+		assertIt(".fXD", item3);
+		assertIt(".fXD", item4);
+
+		setRamp(2, "0 ramp 2/1000");
+		assertIt(".fYD", item0);
 		assertIt(".fYD", item1);
 		assertIt(".fXD", item2);
 		assertIt(".fXD", item3);
 		assertIt(".fXD", item4);
-		assertIt(".fXD", item5);
-
-		setRamp(2, "0 ramp 2/1000");
-		assertIt(".fYD", item1);
-		assertIt(".fYD", item2);
-		assertIt(".fXD", item3);
-		assertIt(".fXD", item4);
-		assertIt(".fXD", item5);
 
 		setRamp(4, "0 ramp 4/1000");
+		assertIt(".fYD", item0);
 		assertIt(".fYD", item1);
 		assertIt(".fYD", item2);
 		assertIt(".fYD", item3);
-		assertIt(".fYD", item4);
-		assertIt(".fXD", item5);
+		assertIt(".fXD", item4);
 
 		setRamp(0, "0");
+		assertIt(".fXD", item0);
 		assertIt(".fXD", item1);
 		assertIt(".fXD", item2);
 		assertIt(".fXD", item3);
 		assertIt(".fXD", item4);
-		assertIt(".fXD", item5);
 	}
 
 	public void testGlobalOffsetValueAndRamp()
 	{
+		assertIt(".fXD", item0);
 		assertIt(".fXD", item1);
 		assertIt(".fXD", item2);
 		assertIt(".fXD", item3);
 		assertIt(".fXD", item4);
-		assertIt(".fXD", item5);
 
 		setValueAndResetRamp(3, "3 (initially 0)");
+		assertIt(".faD", item0);
 		assertIt(".faD", item1);
 		assertIt(".faD", item2);
 		assertIt(".faD", item3);
 		assertIt(".faD", item4);
-		assertIt(".faD", item5);
 
 		setRamp(1, "3 (initially 0) ramp 1/1000");
+		assertIt(".fbD", item0);
+		assertIt(".faD", item1);
+		assertIt(".faD", item2);
+		assertIt(".faD", item3);
+		assertIt(".faD", item4);
+
+		setRamp(2, "3 (initially 0) ramp 2/1000");
+		assertIt(".fbD", item0);
 		assertIt(".fbD", item1);
 		assertIt(".faD", item2);
 		assertIt(".faD", item3);
 		assertIt(".faD", item4);
-		assertIt(".faD", item5);
-
-		setRamp(2, "3 (initially 0) ramp 2/1000");
-		assertIt(".fbD", item1);
-		assertIt(".fbD", item2);
-		assertIt(".faD", item3);
-		assertIt(".faD", item4);
-		assertIt(".faD", item5);
 
 		setRamp(4, "3 (initially 0) ramp 4/1000");
+		assertIt(".fbD", item0);
 		assertIt(".fbD", item1);
 		assertIt(".fbD", item2);
 		assertIt(".fbD", item3);
-		assertIt(".fbD", item4);
-		assertIt(".faD", item5);
+		assertIt(".faD", item4);
 
 		setRamp(0, "3 (initially 0)");
+		assertIt(".faD", item0);
 		assertIt(".faD", item1);
 		assertIt(".faD", item2);
 		assertIt(".faD", item3);
 		assertIt(".faD", item4);
-		assertIt(".faD", item5);
 	}
 
 	private static void assertIt(final String fingerprint, final MediaUrlItem item)
