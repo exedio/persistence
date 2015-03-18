22960ba30c3588ba732f6fc2bf389e606f7779f2
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-03-18 15:25:03 +0100

    prepare splitting off QueryUtilTest

--- runtime/testsrc/com/exedio/cope/QueryTest.java
+++ runtime/testsrc/com/exedio/cope/QueryTest.java
@@ -75,69 +75,69 @@ public class QueryTest extends AbstractRuntimeTest
 
 	public void testSetSelect()
 	{
-		final Query<DayItem> q = DayItem.TYPE.newQuery(null);
-		assertEquals(DayItem.TYPE.getThis(), q.getSelectSingle());
-		assertEquals("select this from DayItem", q.toString());
+		final Query<AnItem> q = AnItem.TYPE.newQuery(null);
+		assertEquals(AnItem.TYPE.getThis(), q.getSelectSingle());
+		assertEquals("select this from AnItem", q.toString());
 
 		try
 		{
-			q.setSelects(new Selectable<?>[]{DayItem.day});
+			q.setSelects(new Selectable<?>[]{AnItem.day});
 			fail();
 		}
 		catch(final RuntimeException e)
 		{
-			assertEquals("must have at least 2 selects, but was [" + DayItem.day + "]", e.getMessage());
+			assertEquals("must have at least 2 selects, but was [" + AnItem.day + "]", e.getMessage());
 		}
-		assertEquals(DayItem.TYPE.getThis(), q.getSelectSingle());
-		assertEquals("select this from DayItem", q.toString());
+		assertEquals(AnItem.TYPE.getThis(), q.getSelectSingle());
+		assertEquals("select this from AnItem", q.toString());
 
 		try
 		{
-			q.setSelects(new Selectable<?>[]{DayItem.TYPE.getThis(), DayItem.day});
+			q.setSelects(new Selectable<?>[]{AnItem.TYPE.getThis(), AnItem.day});
 			fail();
 		}
 		catch(final RuntimeException e)
 		{
 			assertEquals("use setSelect instead", e.getMessage());
 		}
-		assertEquals(DayItem.TYPE.getThis(), q.getSelectSingle());
-		assertEquals("select this from DayItem", q.toString());
+		assertEquals(AnItem.TYPE.getThis(), q.getSelectSingle());
+		assertEquals("select this from AnItem", q.toString());
 	}
 
 	public void testSetSelects()
 	{
 		try
 		{
-			newQuery(new Selectable<?>[]{DayItem.day}, DayItem.TYPE, null);
+			newQuery(new Selectable<?>[]{AnItem.day}, AnItem.TYPE, null);
 			fail();
 		}
 		catch(final RuntimeException e)
 		{
-			assertEquals("must have at least 2 selects, but was [" + DayItem.day + "]", e.getMessage());
+			assertEquals("must have at least 2 selects, but was [" + AnItem.day + "]", e.getMessage());
 		}
 
-		final Query<List<Object>> q = newQuery(new Selectable<?>[]{DayItem.day, DayItem.optionalDay}, DayItem.TYPE, null);
-		assertEquals("select day,optionalDay from DayItem", q.toString());
+		final Query<List<Object>> q = newQuery(new Selectable<?>[]{AnItem.day, AnItem.optionalDay}, AnItem.TYPE, null);
+		assertEquals("select day,optionalDay from AnItem", q.toString());
 
-		q.setSelects(new Selectable<?>[]{DayItem.TYPE.getThis(), DayItem.day});
-		assertEquals("select this,day from DayItem", q.toString());
+		q.setSelects(new Selectable<?>[]{AnItem.TYPE.getThis(), AnItem.day});
+		assertEquals("select this,day from AnItem", q.toString());
 
 		try
 		{
-			q.setSelects(new Selectable<?>[]{DayItem.day});
+			q.setSelects(new Selectable<?>[]{AnItem.day});
 			fail();
 		}
 		catch(final RuntimeException e)
 		{
-			assertEquals("must have at least 2 selects, but was [" + DayItem.day + "]", e.getMessage());
+			assertEquals("must have at least 2 selects, but was [" + AnItem.day + "]", e.getMessage());
 		}
-		assertEquals("select this,day from DayItem", q.toString());
+		assertEquals("select this,day from AnItem", q.toString());
 	}
 
 	@SuppressWarnings({"unchecked", "rawtypes"}) // OK: test bad api usage
 	public void testSetSelectsUnchecked()
 	{
-		final Query q = newQuery(new Selectable[]{DayItem.day, DayItem.optionalDay}, DayItem.TYPE, null);
+		final Query q = newQuery(new Selectable[]{AnItem.day, AnItem.optionalDay}, AnItem.TYPE, null);
 		try
 		{
 			q.getSelectSingle();
@@ -149,13 +149,27 @@ public class QueryTest extends AbstractRuntimeTest
 		}
 		try
 		{
-			q.setSelect(DayItem.TYPE.getThis());
+			q.setSelect(AnItem.TYPE.getThis());
 			fail();
 		}
 		catch(final RuntimeException e)
 		{
 			assertEquals("use setSelects instead", e.getMessage());
 		}
+	}
+
+	static class AnItem extends Item
+	{
+		static final DayField day = new DayField();
+		static final DayField optionalDay = new DayField();
+		static final Type<AnItem> TYPE = TypesBound.newType(AnItem.class);
+		private static final long serialVersionUID = 1l;
+		private AnItem(final ActivationParameters ap) { super(ap); }
+	}
+
+	static
+	{
+		new Model(AnItem.TYPE);
 	}
 
 	public void testLiterals()
