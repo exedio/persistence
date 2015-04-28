645dc7684eaa0c65e0d1e560b57d5d83398d9079
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-04-28 15:49:32 +0200

    FieldDateDaylightSavingTest: testSpring

--- runtime/testsrc/com/exedio/cope/FieldDateDaylightSavingTest.java
+++ runtime/testsrc/com/exedio/cope/FieldDateDaylightSavingTest.java
@@ -36,6 +36,43 @@ public class FieldDateDaylightSavingTest extends FieldTest
 		natve = SchemaInfo.supportsNativeDate(model);
 	}
 
+	public void testSpring() throws ParseException
+	{
+		final Date cutoff = date("2014/3/30 02:00:00.000 (+0100)");
+		assertDate(cutoff, cutoff);
+
+		{
+			final Date minus1000 = date("2014/3/30 01:59:59.000 (+0100)");
+			assertDiff(-1000, cutoff, minus1000);
+			assertDate(minus1000);
+		}
+		{
+			final Date minus2 = date("2014/3/30 01:59:59.998 (+0100)");
+			assertDiff(-2, cutoff, minus2);
+			assertDate(minus2);
+		}
+		{
+			final Date minus1 = date("2014/3/30 01:59:59.999 (+0100)");
+			assertDiff(-1, cutoff, minus1);
+			assertDate(minus1);
+		}
+		{
+			final Date plus1 = date("2014/3/30 03:00:00.001 (+0200)");
+			assertDiff(1, cutoff, plus1);
+			assertDate(plus1, plus1);
+		}
+		{
+			final Date plus2 = date("2014/3/30 03:00:00.002 (+0200)");
+			assertDiff(2, cutoff, plus2);
+			assertDate(plus2, plus2);
+		}
+		{
+			final Date plus1000 = date("2014/3/30 03:00:01.000 (+0200)");
+			assertDiff(1000, cutoff, plus1000);
+			assertDate(plus1000, plus1000);
+		}
+	}
+
 	public void testAutumnStart() throws ParseException
 	{
 		final Date cutoff = date("2014/10/26 02:00:00.000 (+0200)");
