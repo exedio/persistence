d7005bf0c08016bf37a5a4656b00b45fabfc20f8
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-05-12 09:49:36 +0200

    CharSetApos: fix constraint

--- runtime/mysqlsrc/com/exedio/cope/MysqlDialect.java
+++ runtime/mysqlsrc/com/exedio/cope/MysqlDialect.java
@@ -282,9 +282,8 @@ final class MysqlDialect extends Dialect
 
 		final StringBuilder bf = new StringBuilder();
 		bf.append(column).
-			append(" REGEXP '").
-			append(set.getRegularExpression()).
-			append('\'');
+			append(" REGEXP ").
+			append(StringColumn.cacheToDatabaseStatic(set.getRegularExpression()));
 		return bf.toString();
 	}
 
