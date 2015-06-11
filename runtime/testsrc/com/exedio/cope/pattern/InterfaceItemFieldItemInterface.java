c9d7bce8c42b03502dfca9279ea6666ee14b37c8
Uwe Hausbrand <uwe.hausbrand@exedio.com>
2015-06-11 15:13:49 +0200

    add model tests for InterfaceItemField;

---
+++ runtime/testsrc/com/exedio/cope/pattern/InterfaceItemFieldItemInterface.java
@@ -0,0 +1,8 @@
+package com.exedio.cope.pattern;
+
+import java.io.Serializable;
+
+public interface InterfaceItemFieldItemInterface extends Serializable
+{
+	public String getValue();
+}
---
+++ runtime/testsrc/com/exedio/cope/pattern/InterfaceItemFieldTest.java
@@ -0,0 +1,30 @@
+package com.exedio.cope.pattern;
+
+import com.exedio.cope.AbstractRuntimeModelTest;
+import com.exedio.cope.Model;
+import org.junit.Test;
+
+public class InterfaceItemFieldTest extends AbstractRuntimeModelTest
+{
+	static final Model MODEL = new Model(InterfaceItemFieldItem.TYPE,
+			InterfaceItemFieldItemInterfaceImplementationA.TYPE,
+			InterfaceItemFieldItemInterfaceImplementationB.TYPE);
+
+	static
+	{
+		MODEL.enableSerialization(InterfaceItemFieldTest.class, "MODEL");
+	}
+
+	public InterfaceItemFieldTest()
+	{
+		super(MODEL);
+	}
+
+	@Test
+	public void testGetSource()
+	{
+		final InterfaceItemFieldItemInterfaceImplementationA fieldValue = new InterfaceItemFieldItemInterfaceImplementationA();
+		final InterfaceItemFieldItem expected = new InterfaceItemFieldItem(fieldValue);
+		assertEquals(expected, InterfaceItemFieldItem.field.getSource(InterfaceItemFieldItem.class, fieldValue));
+	}
+}
