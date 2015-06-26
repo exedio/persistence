d945120b1797829eec84e00368de9e572ad1fe9a
Uwe Hausbrand <uwe.hausbrand@exedio.com>
2015-06-26 14:50:09 +0200

    BetterTypingOfJoins: add test for joins with better typing on equalTarget usage;

--- runtime/testsrc/com/exedio/cope/PackageTest.java
+++ runtime/testsrc/com/exedio/cope/PackageTest.java
@@ -185,6 +185,7 @@ public class PackageTest extends TestCase
 		suite.addTestSuite( CheckTypeColumnAbstractLinearTest.class );
 		suite.addTestSuite( SchemaPurgeTest.class );
 		suite.addTestSuite( TransactionTryTest.class );
+		suite.addTestSuite( JoinWithInheritanceTest.class );
 
 		suite.addTestSuite( BadQueryTest.class );
 		suite.addTestSuite( InstanceOfQueryTest.class );
