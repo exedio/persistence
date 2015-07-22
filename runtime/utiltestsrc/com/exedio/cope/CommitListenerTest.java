c8ca77cf6e168e5e04177bd20c04a9c401115e41
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2015-07-22 21:42:07 +0200

    CommitListener: add test

---
+++ runtime/utiltestsrc/com/exedio/cope/CommitListenerTest.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+import com.exedio.cope.junit.CopeAssert;
+import java.awt.geom.IllegalPathStateException;
+import java.io.File;
+
+public class CommitListenerTest extends CopeAssert
+{
+	public void testOne()
+	{
+		final StringBuilder bf = new StringBuilder();
+		model.startTransaction("tx");
+		model.addCommitListener(appender(bf, "one"));
+
+		assertEquals("", bf.toString());
+		model.commit();
+		assertEquals("one,", bf.toString());
+	}
+
+	public void testTwo()
+	{
+		final StringBuilder bf = new StringBuilder();
+		model.startTransaction("tx");
+		model.addCommitListener(appender(bf, "one"));
+		model.addCommitListener(appender(bf, "two"));
+
+		assertEquals("", bf.toString());
+		model.commit();
+		assertEquals("one,two,", bf.toString());
+	}
+
+	public void testThrower()
+	{
+		final StringBuilder bf = new StringBuilder();
+		model.startTransaction("tx");
+		model.addCommitListener(appender(bf, "one"));
+		model.addCommitListener(thrower("thrower"));
+		model.addCommitListener(appender(bf, "two"));
+
+		assertEquals("", bf.toString());
+		assertEquals(true, model.hasCurrentTransaction());
+		try
+		{
+			model.commit();
+			fail();
+		}
+		catch(final IllegalPathStateException e)
+		{
+			assertEquals("thrower", e.getMessage());
+		}
+		assertEquals("one,", bf.toString());
+		assertEquals(false, model.hasCurrentTransaction());
+	}
+
+	public void testRollback()
+	{
+		final StringBuilder bf = new StringBuilder();
+		model.startTransaction("tx");
+		model.addCommitListener(appender(bf, "one"));
+
+		assertEquals("", bf.toString());
+		assertEquals(true, model.hasCurrentTransaction());
+		model.rollback();
+		assertEquals("", bf.toString());
+		assertEquals(false, model.hasCurrentTransaction());
+	}
+
+	public void testNullListener()
+	{
+		model.startTransaction("tx");
+		try
+		{
+			model.addCommitListener(null);
+			fail();
+		}
+		catch(final NullPointerException e)
+		{
+			assertEquals("listener", e.getMessage());
+		}
+	}
+
+	public void testNoTransaction()
+	{
+		try
+		{
+			model.addCommitListener(null);
+			fail();
+		}
+		catch(final IllegalStateException e)
+		{
+			assertEquals("there is no cope transaction bound to this thread, see Model#startTransaction", e.getMessage());
+		}
+	}
+
+
+	private static Runnable appender(final StringBuilder bf, final String value)
+	{
+		return new Runnable(){
+			@Override
+			public void run()
+			{
+				bf.append(value).append(',');
+			}
+		};
+	}
+
+	private static Runnable thrower(final String message)
+	{
+		return new Runnable(){
+			@Override
+			public void run()
+			{
+				throw new IllegalPathStateException(message);
+			}
+		};
+	}
+
+
+
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		model.connect(new ConnectProperties(new File("runtime/utiltest.properties")));
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		model.rollbackIfNotCommitted();
+		model.disconnect();
+		super.tearDown();
+	}
+
+
+	private static final Model model = new Model(AnItem.TYPE);
+
+	static class AnItem extends Item
+	{
+		private static final long serialVersionUID = 1l;
+		static final Type<AnItem> TYPE = TypesBound.newType(AnItem.class);
+		AnItem(final ActivationParameters ap) { super(ap); }
+	}
+}
