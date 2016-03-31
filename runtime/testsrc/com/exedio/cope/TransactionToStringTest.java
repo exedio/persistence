f63d772930bbc578b2cc248786cd092ca15e28ff
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2016-03-31 15:15:59 +0200

    TransactionToStringTest added

---
+++ runtime/testsrc/com/exedio/cope/TransactionToStringTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TransactionToStringTest extends TestWithEnvironment
+{
+	public TransactionToStringTest()
+	{
+		super(CacheIsolationTest.MODEL);
+		copeRule.omitTransaction();
+	}
+
+	@Test public void testWithName()
+	{
+		final Transaction tx = model.startTransaction("txname");
+		final String id = String.valueOf(tx.getID());
+		assertEquals("txname", tx.getName());
+
+		assertEquals("CT."+id, tx.toString());
+		model.commit();
+		assertEquals("CT."+id+"(closed)", tx.toString());
+	}
+
+	@Test public void testWithoutName()
+	{
+		@SuppressWarnings("deprecation") // OK: testing deprecated API
+		final Transaction tx = model.startTransaction();
+		final String id = String.valueOf(tx.getID());
+		assertEquals(null, tx.getName());
+
+		assertEquals("CT."+id, tx.toString());
+		model.commit();
+		assertEquals("CT."+id+"(closed)", tx.toString());
+	}
+}
