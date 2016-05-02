bb48b7a8fc0ecaaa767982a470bf98036bdf1f31
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2016-05-02 14:42:26 +0200

    SchemaMismatchConstraintForeignClauseTest local rename

--- runtime/testsrc/com/exedio/cope/SchemaMismatchConstraintForeignClauseTest.java
+++ runtime/testsrc/com/exedio/cope/SchemaMismatchConstraintForeignClauseTest.java
@@ -48,7 +48,7 @@ public class SchemaMismatchConstraintForeignClauseTest extends SchemaMismatchTes
 		final Table table = schema.getTable(name(ItemA.TYPE));
 		assertIt(null, OK, ERROR, table);
 
-		final Constraint unique;
+		final Constraint fk;
 		assertIt(
 				"different condition in database: " +
 				"expected "  + "---field->" + name(TargetB.TYPE) + "." + name(TargetB.TYPE.getThis()) + "---, " +
@@ -56,9 +56,9 @@ public class SchemaMismatchConstraintForeignClauseTest extends SchemaMismatchTes
 				"normalized to  ---field->" + name(TargetB.TYPE) + "." + name(TargetB.TYPE.getThis()) + "--- " +
 				"and "       + "---field->" + name(TargetA.TYPE) + "." + name(TargetA.TYPE.getThis()) + "---",
 				// TODO report normalized form only if different from non-normalized
-				ERROR, ERROR, ForeignKey, unique = table.getConstraint(nameFk(ItemA.field)));
+				ERROR, ERROR, ForeignKey, fk = table.getConstraint(nameFk(ItemA.field)));
 
-		assertTrue(unique instanceof com.exedio.dsmf.ForeignKeyConstraint);
+		assertTrue(fk instanceof com.exedio.dsmf.ForeignKeyConstraint);
 	}
 
 	@CopeName("ItemAB")
