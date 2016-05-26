bca3f006ad1b714a5f592ba170e7720c223062b1
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2016-05-26 17:53:03 +0200

    DateFieldPrecision: fix DatePrecisionSchemaTest after rebase across: simplify check constraint - multiple constraints per column

--- runtime/testsrc/com/exedio/cope/DatePrecisionSchemaTest.java
+++ runtime/testsrc/com/exedio/cope/DatePrecisionSchemaTest.java
@@ -24,7 +24,6 @@ import static com.exedio.cope.DateField.Precision.Minutes;
 import static com.exedio.cope.DateField.Precision.Seconds;
 import static com.exedio.cope.DatePrecisionItem.TYPE;
 import static com.exedio.cope.DatePrecisionItem.hours;
-import static com.exedio.cope.DatePrecisionItem.millis;
 import static com.exedio.cope.DatePrecisionItem.minutes;
 import static com.exedio.cope.DatePrecisionItem.seconds;
 import static com.exedio.cope.SchemaInfo.getColumnName;
@@ -55,10 +54,10 @@ public class DatePrecisionSchemaTest extends TestWithEnvironment
 		schema.checkUnsupportedConstraints();
 
 		final Table table = schema.getTable(getTableName(TYPE));
-		final Constraint millisC  = table.getConstraint("DatePrecisiItem_millis_Ck");
-		final Constraint secondsC = table.getConstraint("DatePrecisiItem_second_Ck");
-		final Constraint minutesC = table.getConstraint("DatePrecisiItem_minute_Ck");
-		final Constraint hoursC   = table.getConstraint("DatePrecisioItem_hours_Ck");
+		final Constraint millisC  = table.getConstraint("DatePrecisiItem_millis_PR");
+		final Constraint secondsC = table.getConstraint("DatePrecisiItem_second_PR");
+		final Constraint minutesC = table.getConstraint("DatePrecisiItem_minute_PR");
+		final Constraint hoursC   = table.getConstraint("DatePrecisioItem_hours_PR");
 
 		if(supportsNativeDate(model))
 		{
@@ -69,29 +68,22 @@ public class DatePrecisionSchemaTest extends TestWithEnvironment
 		}
 		else
 		{
-			assertEquals(   range(millis ), millisC .getRequiredCondition());
-			assertEquals(hp(range(seconds)) + " AND " + precision(seconds,    1000), secondsC.getRequiredCondition());
-			assertEquals(hp(range(minutes)) + " AND " + precision(minutes,   60000), minutesC.getRequiredCondition());
-			assertEquals(hp(range(hours  )) + " AND " + precision(hours  , 3600000), hoursC  .getRequiredCondition());
+			assertEquals(null, millisC);
+			assertEquals(precision(seconds,    1000), secondsC.getRequiredCondition());
+			assertEquals(precision(minutes,   60000), minutesC.getRequiredCondition());
+			assertEquals(precision(hours  , 3600000), hoursC  .getRequiredCondition());
 		}
 
 		model.startTransaction(DatePrecisionSchemaTest.class.getName());
 		assertSchema();
 	}
 
-	private final String range(final DateField field)
-	{
-		return
-				"(" + q(field) + ">=" + Long.MIN_VALUE + ") AND " +
-				"(" + q(field) + "<=" + Long.MAX_VALUE + ")";
-	}
-
 	private final String precision(final DateField field, final int divisor)
 	{
 		switch(dialect)
 		{
-			case hsqldb    : return "(MOD(" + q(field) + "," + divisor + ")=0)";
-			case mysql     : return "((" + q(field) + " MOD " + divisor + ")=0)";
+			case hsqldb    : return "MOD(" + q(field) + "," + divisor + ")=0";
+			case mysql     : return "(" + q(field) + " MOD " + divisor + ")=0";
 			case oracle    : // TODO
 			case postgresql: // TODO
 			default:
@@ -102,14 +94,6 @@ public class DatePrecisionSchemaTest extends TestWithEnvironment
 	private final String q(final Field<?> f)
 	{
 		return quoteName(model, getColumnName(f));
-	}
-
-	protected final String hp(final String s)
-	{
-		if(hsqldb)
-			return "(" + s + ")";
-		else
-			return s;
 	}
 
 	@Test public void testEnumSchema()
