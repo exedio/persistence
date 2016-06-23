61495c42b7c81cd3090f10a47016bcd406d0bf4c
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2016-06-23 09:52:23 +0200

    CopeNameSpaceLog: do not require imports

--- instrument/src/com/exedio/cope/instrument/CopeNameSpace.java
+++ instrument/src/com/exedio/cope/instrument/CopeNameSpace.java
@@ -48,42 +48,42 @@ class CopeNameSpace extends NameSpace
 
 	/*
 	@Override
-	public Object get(final String name, final Interpreter interpreter) throws UtilEvalError
+	public Object get(final String name, final bsh.Interpreter interpreter) throws bsh.UtilEvalError
 	{
 		System.out.println("++++++++++++++++get(\""+name+"\",Interpreter)");
 		return report(super.get(name, interpreter));
 	}
 
 	@Override
-	public Class<?> getClass(final String name) throws UtilEvalError
+	public Class<?> getClass(final String name) throws bsh.UtilEvalError
 	{
 		System.out.println("++++++++++++++++getClass(\""+name+"\")");
 		return report(super.getClass(name));
 	}
 
 	@Override
-	public Object getVariable(final String name) throws UtilEvalError
+	public Object getVariable(final String name) throws bsh.UtilEvalError
 	{
 		System.out.println("++++++++++++++++getVariable(\""+name+"\")");
 		return report(super.getVariable(name));
 	}
 
 	@Override
-	public Object getVariable(final String name, final boolean recurse) throws UtilEvalError
+	public Object getVariable(final String name, final boolean recurse) throws bsh.UtilEvalError
 	{
 		System.out.println("++++++++++++++++getVariable(\""+name+"\","+recurse+")");
 		return report(super.getVariable(name, recurse));
 	}
 
 	@Override
-	protected Variable getVariableImpl(final String name, final boolean recurse) throws UtilEvalError
+	protected bsh.Variable getVariableImpl(final String name, final boolean recurse) throws bsh.UtilEvalError
 	{
 		System.out.println("++++++++++++++++getVariableImpl(\""+name+"\","+recurse+")");
 		return report(super.getVariableImpl(name, recurse));
 	}
 
 	@Override
-	protected Variable getImportedVar(final String name) throws bsh.UtilEvalError
+	protected bsh.Variable getImportedVar(final String name) throws bsh.UtilEvalError
 	{
 		System.out.println("++++++++++++++++getImportedVar(\""+name+"\")");
 		return report(super.getImportedVar(name));
@@ -97,7 +97,7 @@ class CopeNameSpace extends NameSpace
 	}
 
 	@Override
-	public Variable[] getDeclaredVariables()
+	public bsh.Variable[] getDeclaredVariables()
 	{
 		System.out.println("++++++++++++++++getDeclaredVariables()");
 		return report(super.getDeclaredVariables());
