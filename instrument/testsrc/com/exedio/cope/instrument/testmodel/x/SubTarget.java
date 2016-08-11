e383b4464860c174a3a306ddabfbf1a8c36f7bb6
Frank Finger <frank.finger@exedio.com>
2016-08-11 10:31:07 +0200

    test case for class resolving problem in instrumentor

---
+++ instrument/testsrc/com/exedio/cope/instrument/testmodel/x/SubTarget.java
@@ -0,0 +1,8 @@
+package com.exedio.cope.instrument.testmodel.x;
+
+/**
+ * This class is used only to illustrate a name collision with {@link com.exedio.cope.instrument.testmodel.sub.SubTarget}.
+ */
+class SubTarget
+{
+}
