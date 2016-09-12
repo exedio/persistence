ef3b0f02b2a2f718521cc8a109f14ba528c5c96b
Frank Finger <frank.finger@exedio.com>
2016-09-12 12:56:26 +0200

    test for instrument/ignore

---
+++ instrument/testsrc/com/exedio/cope/instrument/testmodel/DontInstrument.java
@@ -0,0 +1,14 @@
+package com.exedio.cope.instrument.testmodel;
+
+import com.exedio.cope.Item;
+import com.exedio.cope.instrument.testfeature.SimpleSettable;
+
+/**
+ * This class tests that the 'ignore' element at the 'instrument' ant task works.
+ */
+class DontInstrument extends Item
+{
+	private static final long serialVersionUID=1L;
+
+	static final SimpleSettable makeInstrumentorFail = null;
+}
