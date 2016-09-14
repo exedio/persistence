eccf389bee4f0d58c750a96eee1122e05fb9981a
Frank Finger <frank.finger@exedio.com>
2016-09-14 10:04:14 +0200

    trigger Jenkins warning

--- instrument/testsrc/com/exedio/cope/instrument/testmodel/DontInstrument.java
+++ instrument/testsrc/com/exedio/cope/instrument/testmodel/DontInstrument.java
@@ -20,6 +20,7 @@ package com.exedio.cope.instrument.testmodel;
 
 import com.exedio.cope.Item;
 import com.exedio.cope.instrument.testfeature.SimpleSettable;
+import javax.annotation.Generated;
 
 /**
  * This class tests that the 'ignore' element at the 'instrument' ant task works.
@@ -29,4 +30,10 @@ class DontInstrument extends Item
 	private static final long serialVersionUID=1L;
 
 	static final SimpleSettable makeInstrumentorFail = null;
+
+	@Generated("com.exedio.cope.instrument")
+	int generatedVariable;
+
+	@Generated("com.exedio.cope.instrument")
+	void generatedMethod(){}
 }
