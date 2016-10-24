d9619a28191192f5c5059f30f1b343eb743dfbaa
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2016-10-24 18:01:28 +0200

    add UniqueManyTest

---
+++ runtime/testsrc/com/exedio/cope/UniqueManyTest.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+import static com.exedio.cope.UniqueManyTest.MyItem.constraint;
+import static com.exedio.cope.UniqueManyTest.MyItem.field;
+import static com.exedio.cope.instrument.Visibility.NONE;
+import static com.exedio.cope.instrument.Visibility.PACKAGE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import org.junit.Test;
+
+public class UniqueManyTest extends TestWithEnvironment
+{
+	public UniqueManyTest()
+	{
+		super(MODEL);
+	}
+
+	@Test public void test()
+	{
+		final int maximum = 6;
+
+		for(int index = 2; index<=maximum; index++)
+		{
+			final ArrayList<StringField> fields = new ArrayList<>();
+			for(int i = 1; i<=index; i++)
+				fields.add(field(i));
+
+			assertEquals("constraint" + index, fields, constraint(index).getFields());
+		}
+
+		final ArrayList<SetValue<?>> sv = new ArrayList<>();
+		for(int index = 1; index<=maximum; index++)
+			sv.add(field(index).map("value" + index));
+
+		final MyItem i = new MyItem(sv.toArray(new SetValue<?>[sv.size()]));
+		for(int index = 1; index<=maximum; index++)
+			assertEquals("value" + index, field(index).get(i));
+
+		restartTransaction();
+		for(int index = 1; index<=maximum; index++)
+			assertEquals("value" + index, field(index).get(i));
+	}
+
+	@com.exedio.cope.instrument.WrapperType(indent=2, constructor=NONE, genericConstructor=PACKAGE, comments=false) // TODO use import, but this is not accepted by javac
+	static final class MyItem extends Item
+	{
+		static final StringField field1 = new StringField();
+		static final StringField field2 = new StringField();
+		static final StringField field3 = new StringField();
+		static final StringField field4 = new StringField();
+		static final StringField field5 = new StringField();
+		static final StringField field6 = new StringField();
+
+		static final UniqueConstraint constraint2 = new UniqueConstraint(field1, field2);
+		static final UniqueConstraint constraint3 = new UniqueConstraint(field1, field2, field3);
+		static final UniqueConstraint constraint4 = new UniqueConstraint(field1, field2, field3, field4);
+		static final UniqueConstraint constraint5 = new UniqueConstraint(field1, field2, field3, field4, field5);
+		static final UniqueConstraint constraint6 = new UniqueConstraint(field1, field2, field3, field4, field5, field6);
+
+		static StringField field(final int index)
+		{
+			final String name = "field" + index;
+			final StringField result = (StringField)TYPE.getFeature(name);
+			assertNotNull(name, result);
+			return result;
+		}
+
+		static UniqueConstraint constraint(final int index)
+		{
+			final String name = "constraint" + index;
+			final UniqueConstraint result = (UniqueConstraint)TYPE.getFeature(name);
+			assertNotNull(name, result);
+			return result;
+		}
+
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		MyItem(final com.exedio.cope.SetValue<?>... setValues)
+		{
+			super(setValues);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nonnull
+		final java.lang.String getField1()
+		{
+			return MyItem.field1.get(this);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		final void setField1(@javax.annotation.Nonnull final java.lang.String field1)
+				throws
+					com.exedio.cope.MandatoryViolationException,
+					com.exedio.cope.UniqueViolationException,
+					com.exedio.cope.StringLengthViolationException
+		{
+			MyItem.field1.set(this,field1);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nonnull
+		final java.lang.String getField2()
+		{
+			return MyItem.field2.get(this);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		final void setField2(@javax.annotation.Nonnull final java.lang.String field2)
+				throws
+					com.exedio.cope.MandatoryViolationException,
+					com.exedio.cope.UniqueViolationException,
+					com.exedio.cope.StringLengthViolationException
+		{
+			MyItem.field2.set(this,field2);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nonnull
+		final java.lang.String getField3()
+		{
+			return MyItem.field3.get(this);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		final void setField3(@javax.annotation.Nonnull final java.lang.String field3)
+				throws
+					com.exedio.cope.MandatoryViolationException,
+					com.exedio.cope.UniqueViolationException,
+					com.exedio.cope.StringLengthViolationException
+		{
+			MyItem.field3.set(this,field3);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nonnull
+		final java.lang.String getField4()
+		{
+			return MyItem.field4.get(this);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		final void setField4(@javax.annotation.Nonnull final java.lang.String field4)
+				throws
+					com.exedio.cope.MandatoryViolationException,
+					com.exedio.cope.UniqueViolationException,
+					com.exedio.cope.StringLengthViolationException
+		{
+			MyItem.field4.set(this,field4);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nonnull
+		final java.lang.String getField5()
+		{
+			return MyItem.field5.get(this);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		final void setField5(@javax.annotation.Nonnull final java.lang.String field5)
+				throws
+					com.exedio.cope.MandatoryViolationException,
+					com.exedio.cope.UniqueViolationException,
+					com.exedio.cope.StringLengthViolationException
+		{
+			MyItem.field5.set(this,field5);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nonnull
+		final java.lang.String getField6()
+		{
+			return MyItem.field6.get(this);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		final void setField6(@javax.annotation.Nonnull final java.lang.String field6)
+				throws
+					com.exedio.cope.MandatoryViolationException,
+					com.exedio.cope.UniqueViolationException,
+					com.exedio.cope.StringLengthViolationException
+		{
+			MyItem.field6.set(this,field6);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nullable
+		static final MyItem forConstraint2(@javax.annotation.Nonnull final java.lang.String field1,@javax.annotation.Nonnull final java.lang.String field2)
+		{
+			return MyItem.constraint2.search(MyItem.class,field1,field2);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nullable
+		static final MyItem forConstraint3(@javax.annotation.Nonnull final java.lang.String field1,@javax.annotation.Nonnull final java.lang.String field2,@javax.annotation.Nonnull final java.lang.String field3)
+		{
+			return MyItem.constraint3.search(MyItem.class,field1,field2,field3);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nullable
+		static final MyItem forConstraint4(@javax.annotation.Nonnull final java.lang.String field1,@javax.annotation.Nonnull final java.lang.String field2,@javax.annotation.Nonnull final java.lang.String field3,@javax.annotation.Nonnull final java.lang.String field4)
+		{
+			return MyItem.constraint4.search(MyItem.class,field1,field2,field3,field4);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nullable
+		static final MyItem forConstraint5(@javax.annotation.Nonnull final java.lang.String field1,@javax.annotation.Nonnull final java.lang.String field2,@javax.annotation.Nonnull final java.lang.String field3,@javax.annotation.Nonnull final java.lang.String field4,@javax.annotation.Nonnull final java.lang.String field5)
+		{
+			return MyItem.constraint5.search(MyItem.class,field1,field2,field3,field4,field5);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@javax.annotation.Nullable
+		static final MyItem forConstraint6(@javax.annotation.Nonnull final java.lang.String field1,@javax.annotation.Nonnull final java.lang.String field2,@javax.annotation.Nonnull final java.lang.String field3,@javax.annotation.Nonnull final java.lang.String field4,@javax.annotation.Nonnull final java.lang.String field5,@javax.annotation.Nonnull final java.lang.String field6)
+		{
+			return MyItem.constraint6.search(MyItem.class,field1,field2,field3,field4,field5,field6);
+		}
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		private static final long serialVersionUID = 1l;
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		static final com.exedio.cope.Type<MyItem> TYPE = com.exedio.cope.TypesBound.newType(MyItem.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@SuppressWarnings("unused") private MyItem(final com.exedio.cope.ActivationParameters ap){super(ap);}
+	}
+
+	private static final Model MODEL = new Model(MyItem.TYPE);
+}
