ae656e8d127fe5f7eb582cf6ccba180dbb475d32
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2016-11-07 09:39:17 +0100

    SerializationCheckTest: use intrumentor

--- runtime/testsrc/com/exedio/cope/misc/SerializationCheckTest.java
+++ runtime/testsrc/com/exedio/cope/misc/SerializationCheckTest.java
@@ -18,19 +18,18 @@
 
 package com.exedio.cope.misc;
 
+import static com.exedio.cope.instrument.Visibility.NONE;
 import static com.exedio.cope.misc.SerializationCheck.check;
 import static com.exedio.cope.tojunit.Assert.assertEqualsUnmodifiable;
 import static com.exedio.cope.tojunit.Assert.list;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import com.exedio.cope.ActivationParameters;
 import com.exedio.cope.Item;
 import com.exedio.cope.Model;
 import com.exedio.cope.StringField;
-import com.exedio.cope.Type;
-import com.exedio.cope.TypesBound;
-import com.exedio.cope.instrument.WrapperIgnore;
+import com.exedio.cope.instrument.Wrapper;
+import com.exedio.cope.instrument.WrapperType;
 import java.lang.reflect.Field;
 import org.junit.Test;
 
@@ -62,26 +61,34 @@ public class SerializationCheckTest
 	}
 
 
-	@WrapperIgnore
+	@WrapperType(constructor=NONE, genericConstructor=NONE, indent=2, comments=false)
 	static class ItemOk extends Item
 	{
+		@Wrapper(wrap="*", visibility=NONE)
 		static final StringField f2 = new StringField();
 
 		static int staticField2;
 		transient int transientField2;
 		static transient int staticTransientField2;
 
-		private ItemOk(final ActivationParameters ap) { super(ap); }
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
 		private static final long serialVersionUID = 1l;
-		static final Type<ItemOk> TYPE = TypesBound.newType(ItemOk.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		static final com.exedio.cope.Type<ItemOk> TYPE = com.exedio.cope.TypesBound.newType(ItemOk.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		protected ItemOk(final com.exedio.cope.ActivationParameters ap){super(ap);}
 	}
 
 	private static final Model MODEL_OK = new Model(ItemOk.TYPE);
 
 
-	@WrapperIgnore
+	@WrapperType(constructor=NONE, genericConstructor=NONE, indent=2, comments=false)
 	static class Item1 extends Item
 	{
+		@Wrapper(wrap="*", visibility=NONE)
 		static final StringField f1 = new StringField();
 
 		static int staticField1;
@@ -89,14 +96,21 @@ public class SerializationCheckTest
 		static transient int staticTransientField1;
 		int serializedField1;
 
-		private Item1(final ActivationParameters ap) { super(ap); }
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
 		private static final long serialVersionUID = 1l;
-		static final Type<Item1> TYPE = TypesBound.newType(Item1.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		static final com.exedio.cope.Type<Item1> TYPE = com.exedio.cope.TypesBound.newType(Item1.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		protected Item1(final com.exedio.cope.ActivationParameters ap){super(ap);}
 	}
 
-	@WrapperIgnore
+	@WrapperType(constructor=NONE, genericConstructor=NONE, indent=2, comments=false)
 	static class Item2 extends Item
 	{
+		@Wrapper(wrap="*", visibility=NONE)
 		static final StringField f2 = new StringField();
 
 		static int staticField2;
@@ -104,9 +118,15 @@ public class SerializationCheckTest
 		static transient int staticTransientField2;
 		int serializedField2;
 
-		private Item2(final ActivationParameters ap) { super(ap); }
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
 		private static final long serialVersionUID = 1l;
-		static final Type<Item2> TYPE = TypesBound.newType(Item2.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		static final com.exedio.cope.Type<Item2> TYPE = com.exedio.cope.TypesBound.newType(Item2.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		protected Item2(final com.exedio.cope.ActivationParameters ap){super(ap);}
 	}
 
 	private static final Model MODEL = new Model(Item1.TYPE, Item2.TYPE);
