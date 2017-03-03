3a7f0dc6f4e78fb7a03336f51ce447ec6993b32d
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2017-03-03 13:46:33 +0100

    Enum[Set|Map]Field supports @CopeName - add test

---
+++ runtime/testsrc/com/exedio/cope/pattern/EnumCopeNameTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.pattern;
+
+import static com.exedio.cope.instrument.Visibility.NONE;
+import static org.junit.Assert.assertEquals;
+
+import com.exedio.cope.CopeName;
+import com.exedio.cope.Item;
+import com.exedio.cope.StringField;
+import com.exedio.cope.instrument.WrapperIgnore;
+import com.exedio.cope.instrument.WrapperType;
+import org.junit.Test;
+
+public class EnumCopeNameTest
+{
+	@Test public void test()
+	{
+		assertEquals("set-normal", MyItem.set.getField(MyEnum.normal).getName());
+		assertEquals("map-normal", MyItem.map.getField(MyEnum.normal).getName());
+		assertEquals("set-pure",   MyItem.set.getField(MyEnum.pure  ).getName()); // TODO actual
+		assertEquals("map-pure",   MyItem.map.getField(MyEnum.pure  ).getName()); // TODO actual
+	}
+
+	enum MyEnum
+	{
+		normal,
+		@CopeName("actual") pure,
+	}
+
+	@WrapperType(constructor=NONE, genericConstructor=NONE, indent=2, comments=false)
+	static final class MyItem extends Item
+	{
+		@WrapperIgnore static final EnumSetField<MyEnum       > set = EnumSetField.create(MyEnum.class);
+		@WrapperIgnore static final EnumMapField<MyEnum,String> map = EnumMapField.create(MyEnum.class, new StringField());
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		private static final long serialVersionUID = 1l;
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		static final com.exedio.cope.Type<MyItem> TYPE = com.exedio.cope.TypesBound.newType(MyItem.class);
+
+		@javax.annotation.Generated("com.exedio.cope.instrument")
+		@SuppressWarnings("unused") private MyItem(final com.exedio.cope.ActivationParameters ap){super(ap);}
+	}
+}
