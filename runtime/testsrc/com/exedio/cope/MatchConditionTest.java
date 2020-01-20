3b48404dd8cd31c841ec3d93b6b70371a0ad7412
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2020-01-20 18:05:33 +0100

    MatchConditionTest added

---
+++ runtime/testsrc/com/exedio/cope/MatchConditionTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope;
+
+import static com.exedio.cope.tojunit.Assert.assertFails;
+import static com.exedio.cope.tojunit.EqualsAssert.assertEqualsAndHash;
+import static com.exedio.cope.tojunit.EqualsAssert.assertNotEqualsAndHash;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.junit.jupiter.api.Test;
+
+@SuppressFBWarnings("NP_NULL_PARAM_DEREF_NONVIRTUAL")
+public class MatchConditionTest
+{
+	@Test void testEquals()
+	{
+		assertEqualsAndHash(
+				new MatchCondition(FIELD1, "one"),
+				new MatchCondition(FIELD1, "one"));
+		assertNotEqualsAndHash(
+				new MatchCondition(FIELD1, "one"),
+				new MatchCondition(FIELD2, "one"),
+				new MatchCondition(FIELD1, "two"));
+	}
+	@Test void testToString()
+	{
+		assertEquals(FIELD1 + " matches 'one'", new MatchCondition(FIELD1, "one").toString());
+		assertEquals(FIELD2 + " matches 'two'", new MatchCondition(FIELD2, "two").toString());
+	}
+	@Test void testCreateFunctionNull()
+	{
+		assertFails(
+				() -> new MatchCondition(null, null),
+				NullPointerException.class,
+				"function");
+	}
+	@Test void testCreateValueNull()
+	{
+		assertFails(
+				() -> new MatchCondition(FIELD1, null),
+				NullPointerException.class,
+				"value");
+	}
+
+	private static final StringField FIELD1 = new StringField();
+	private static final StringField FIELD2 = new StringField();
+}
