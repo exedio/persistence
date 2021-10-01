4bec20e1b8d284227c3abbd9be7d582d158d6766
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2021-10-01 15:31:44 +0200

    bugfix: instrumentor fails on semicolon after a top-level class

---
+++ instrument/testsrc/com/exedio/cope/instrument/testmodel/ClassWithSemicolon.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.instrument.testmodel;
+
+import com.exedio.cope.Item;
+
+/**
+ * This class tests what happens, if there is a semicolon after a top-level class.
+ * Look at the very end of this file.
+ */
+final class ClassWithSemicolon extends Item
+{
+	/**
+	 * Creates a new ClassWithSemicolon with all the fields initially needed.
+	 */
+	@com.exedio.cope.instrument.Generated // customize with @WrapperType(constructor=...) and @WrapperInitial
+	@java.lang.SuppressWarnings({"RedundantSuppression","TypeParameterExtendsFinalClass","UnnecessarilyQualifiedInnerClassAccess"})
+	ClassWithSemicolon()
+	{
+		this(new com.exedio.cope.SetValue<?>[]{
+		});
+	}
+
+	/**
+	 * Creates a new ClassWithSemicolon and sets the given fields initially.
+	 */
+	@com.exedio.cope.instrument.Generated // customize with @WrapperType(genericConstructor=...)
+	private ClassWithSemicolon(final com.exedio.cope.SetValue<?>... setValues){super(setValues);}
+
+	@com.exedio.cope.instrument.Generated
+	private static final long serialVersionUID = 1l;
+
+	/**
+	 * The persistent type information for classWithSemicolon.
+	 */
+	@com.exedio.cope.instrument.Generated // customize with @WrapperType(type=...)
+	static final com.exedio.cope.Type<ClassWithSemicolon> TYPE = com.exedio.cope.TypesBound.newType(ClassWithSemicolon.class);
+
+	/**
+	 * Activation constructor. Used for internal purposes only.
+	 * @see com.exedio.cope.Item#Item(com.exedio.cope.ActivationParameters)
+	 */
+	@com.exedio.cope.instrument.Generated
+	private ClassWithSemicolon(final com.exedio.cope.ActivationParameters ap){super(ap);}
+}; // this semicolon is what this class is about
