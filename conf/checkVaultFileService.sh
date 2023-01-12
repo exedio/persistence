6ccf2054540bc91502c6f90e6d6ea6f286be1cb6
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2023-01-12 16:46:26 +0100

    checkVaultFileService.sh added

---
+++ conf/checkVaultFileService.sh
@@ -0,0 +1,10 @@
+# VaultFileService hash checker
+# Generates a file suitable for
+#   sha512sum --check --quiet --strict
+find . \
+	-mindepth 2 \
+	-maxdepth 2 \
+	-not -path "./.tempVaultFileService/*" \
+	-not -path "./VaultGenuineServiceKey/*" \
+	-type f \
+	| sed -e 's|\(\./\([0-9a-f]*\)/\([0-9a-f]*\)\)|\2\3  \1|'
