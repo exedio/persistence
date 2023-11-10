52186ee15ec6072d6a7c80adf163a27f08e693f9
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2023-11-10 09:17:07 +0100

    StringConditionTest gets its own model

--- runtime/testsrc/com/exedio/cope/StringConditionTest.java
+++ runtime/testsrc/com/exedio/cope/StringConditionTest.java
@@ -18,12 +18,15 @@
 
 package com.exedio.cope;
 
-import static com.exedio.cope.testmodel.StringItem.TYPE;
-import static com.exedio.cope.testmodel.StringItem.any;
+import static com.exedio.cope.StringConditionTest.MyItem.TYPE;
+import static com.exedio.cope.StringConditionTest.MyItem.field;
+import static com.exedio.cope.instrument.Visibility.NONE;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.exedio.cope.testmodel.StringItem;
+import com.exedio.cope.instrument.Wrapper;
+import com.exedio.cope.instrument.WrapperInitial;
+import com.exedio.cope.instrument.WrapperType;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -31,122 +34,156 @@ public class StringConditionTest extends TestWithEnvironment
 {
 	public StringConditionTest()
 	{
-		super(StringModelTest.MODEL);
+		super(MODEL);
 	}
 
 	@Test void testLikeEmpty()
 	{
-		final StringItem some = new StringItem("startEnd", true);
-		final StringItem other = new StringItem("other", true);
-		final StringItem empty = new StringItem("", true);
-		new StringItem(null, true);
+		final MyItem some = new MyItem("startEnd");
+		final MyItem other = new MyItem("other");
+		final MyItem empty = new MyItem("");
+		new MyItem((String)null);
 
-		assertEquals(asList(some), search(any.startsWith("start")));
-		assertEquals(asList(some), search(any.  endsWith("End")));
-		assertEquals(asList(some), search(any.  contains("tE")));
-		assertEquals(asList(some), search(any.startsWithIgnoreCase("START")));
-		assertEquals(asList(some), search(any.  endsWithIgnoreCase("END")));
-		assertEquals(asList(some), search(any.  containsIgnoreCase("TE")));
-		assertEquals(asList(some, other, empty), search(any.startsWith("")));
-		assertEquals(asList(some, other, empty), search(any.  endsWith("")));
-		assertEquals(asList(some, other, empty), search(any.  contains("")));
-		assertEquals(asList(some, other, empty), search(any.startsWithIgnoreCase("")));
-		assertEquals(asList(some, other, empty), search(any.  endsWithIgnoreCase("")));
-		assertEquals(asList(some, other, empty), search(any.  containsIgnoreCase("")));
+		assertEquals(asList(some), search(field.startsWith("start")));
+		assertEquals(asList(some), search(field.  endsWith("End")));
+		assertEquals(asList(some), search(field.  contains("tE")));
+		assertEquals(asList(some), search(field.startsWithIgnoreCase("START")));
+		assertEquals(asList(some), search(field.  endsWithIgnoreCase("END")));
+		assertEquals(asList(some), search(field.  containsIgnoreCase("TE")));
+		assertEquals(asList(some, other, empty), search(field.startsWith("")));
+		assertEquals(asList(some, other, empty), search(field.  endsWith("")));
+		assertEquals(asList(some, other, empty), search(field.  contains("")));
+		assertEquals(asList(some, other, empty), search(field.startsWithIgnoreCase("")));
+		assertEquals(asList(some, other, empty), search(field.  endsWithIgnoreCase("")));
+		assertEquals(asList(some, other, empty), search(field.  containsIgnoreCase("")));
 	}
 
 	@Test void testLikeWildcard()
 	{
-		final StringItem some = new StringItem("startEnd", true);
-		final StringItem someP = new StringItem("start%End", true);
-		final StringItem someU = new StringItem("start_End", true);
-		final StringItem someB = new StringItem("start\\End", true);
-		final StringItem someX = new StringItem("startXEnd", true);
-		final StringItem someXY = new StringItem("startXYEnd", true);
-		new StringItem("other", true);
-		new StringItem("", true);
-		new StringItem(null, true);
+		final MyItem some = new MyItem("startEnd");
+		final MyItem someP = new MyItem("start%End");
+		final MyItem someU = new MyItem("start_End");
+		final MyItem someB = new MyItem("start\\End");
+		final MyItem someX = new MyItem("startXEnd");
+		final MyItem someXY = new MyItem("startXYEnd");
+		new MyItem("other");
+		new MyItem("");
+		new MyItem((String)null);
 
 		// https://hsqldb.org/doc/2.0/guide/dataaccess-chapt.html#dac_sql_predicates
 		// https://dev.mysql.com/doc/refman/8.0/en/string-comparison-functions.html#operator_like
 		// https://www.postgresql.org/docs/11/functions-matching.html#FUNCTIONS-LIKE
-		assertEquals(asList(some                                    ), search(any.like("sta%tEnd")));
-		assertEquals(asList(some                                    ), search(any.like("sta_tEnd")));
-		assertEquals(asList(some                                    ), search(any.like("startEnd")));
-		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(any.like("start%End")));
-		assertEquals(asList(      someP, someU, someB, someX        ), search(any.like("start_End")));
-		assertEquals(asList(some ), search(any.like("start\\End")));
-		assertEquals(asList(someB), search(any.like("start\\\\End")));
-		assertEquals(asList(someP), search(any.like("start\\%End")));
-		assertEquals(asList(someU), search(any.like("start\\_End")));
+		assertEquals(asList(some                                    ), search(field.like("sta%tEnd")));
+		assertEquals(asList(some                                    ), search(field.like("sta_tEnd")));
+		assertEquals(asList(some                                    ), search(field.like("startEnd")));
+		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(field.like("start%End")));
+		assertEquals(asList(      someP, someU, someB, someX        ), search(field.like("start_End")));
+		assertEquals(asList(some ), search(field.like("start\\End")));
+		assertEquals(asList(someB), search(field.like("start\\\\End")));
+		assertEquals(asList(someP), search(field.like("start\\%End")));
+		assertEquals(asList(someU), search(field.like("start\\_End")));
 
-		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(any.startsWith("start")));
-		assertEquals(asList(                                        ), search(any.startsWith("st%t")));
-		assertEquals(asList(                                        ), search(any.startsWith("st_rt")));
-		assertEquals(asList(some                                    ), search(any.startsWith("startEn")));
-		assertEquals(asList(      someP                             ), search(any.startsWith("start%E")));
-		assertEquals(asList(             someU                      ), search(any.startsWith("start_E")));
+		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(field.startsWith("start")));
+		assertEquals(asList(                                        ), search(field.startsWith("st%t")));
+		assertEquals(asList(                                        ), search(field.startsWith("st_rt")));
+		assertEquals(asList(some                                    ), search(field.startsWith("startEn")));
+		assertEquals(asList(      someP                             ), search(field.startsWith("start%E")));
+		assertEquals(asList(             someU                      ), search(field.startsWith("start_E")));
 
-		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(any.endsWith("End")));
-		assertEquals(asList(                                        ), search(any.endsWith("E%d")));
-		assertEquals(asList(                                        ), search(any.endsWith("E_d")));
-		assertEquals(asList(some                                    ), search(any.endsWith("rtEnd")));
-		assertEquals(asList(      someP                             ), search(any.endsWith("rt%End")));
-		assertEquals(asList(             someU                      ), search(any.endsWith("rt_End")));
+		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(field.endsWith("End")));
+		assertEquals(asList(                                        ), search(field.endsWith("E%d")));
+		assertEquals(asList(                                        ), search(field.endsWith("E_d")));
+		assertEquals(asList(some                                    ), search(field.endsWith("rtEnd")));
+		assertEquals(asList(      someP                             ), search(field.endsWith("rt%End")));
+		assertEquals(asList(             someU                      ), search(field.endsWith("rt_End")));
 
-		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(any.contains("tart")));
-		assertEquals(asList(                                        ), search(any.contains("ta%t")));
-		assertEquals(asList(                                        ), search(any.contains("ta_t")));
-		assertEquals(asList(some                                    ), search(any.contains("tartEn")));
-		assertEquals(asList(      someP                             ), search(any.contains("tart%En")));
-		assertEquals(asList(             someU                      ), search(any.contains("tart_En")));
+		assertEquals(asList(some, someP, someU, someB, someX, someXY), search(field.contains("tart")));
+		assertEquals(asList(                                        ), search(field.contains("ta%t")));
+		assertEquals(asList(                                        ), search(field.contains("ta_t")));
+		assertEquals(asList(some                                    ), search(field.contains("tartEn")));
+		assertEquals(asList(      someP                             ), search(field.contains("tart%En")));
+		assertEquals(asList(             someU                      ), search(field.contains("tart_En")));
 
-		assertEquals(asList(), search(any.startsWith("start\\%E")));
-		assertEquals(asList(), search(any.startsWith("start\\_E")));
-		assertEquals(asList(), search(any.startsWith("start\\\\E")));
-		assertEquals(asList(), search(any.endsWith("art\\%End")));
-		assertEquals(asList(), search(any.endsWith("art\\_End")));
-		assertEquals(asList(), search(any.endsWith("art\\\\End")));
-		assertEquals(asList(), search(any.contains("art\\%En")));
-		assertEquals(asList(), search(any.contains("art\\_En")));
-		assertEquals(asList(), search(any.contains("art\\\\En")));
+		assertEquals(asList(), search(field.startsWith("start\\%E")));
+		assertEquals(asList(), search(field.startsWith("start\\_E")));
+		assertEquals(asList(), search(field.startsWith("start\\\\E")));
+		assertEquals(asList(), search(field.endsWith("art\\%End")));
+		assertEquals(asList(), search(field.endsWith("art\\_End")));
+		assertEquals(asList(), search(field.endsWith("art\\\\End")));
+		assertEquals(asList(), search(field.contains("art\\%En")));
+		assertEquals(asList(), search(field.contains("art\\_En")));
+		assertEquals(asList(), search(field.contains("art\\\\En")));
 	}
 
 	@Test void testIgnoreCase()
 	{
-		final StringItem mixed = new StringItem("lowerUPPER", true);
-		final StringItem lower = new StringItem("lowerupper", true);
-		final StringItem upper = new StringItem("LOWERUPPER", true);
-		new StringItem(null, true);
+		final MyItem mixed = new MyItem("lowerUPPER");
+		final MyItem lower = new MyItem("lowerupper");
+		final MyItem upper = new MyItem("LOWERUPPER");
+		new MyItem((String)null);
 
-		assertEquals(asList(mixed, lower, upper), search(any.     equalIgnoreCase("lowerUPPER" )));
-		assertEquals(asList(mixed, lower, upper), search(any.      likeIgnoreCase("lowerUPPER%")));
-		assertEquals(asList(mixed, lower, upper), search(any.startsWithIgnoreCase("lowerUPPER" )));
-		assertEquals(asList(mixed, lower, upper), search(any.  endsWithIgnoreCase("lowerUPPER" )));
-		assertEquals(asList(mixed, lower, upper), search(any.  containsIgnoreCase("lowerUPPER" )));
+		assertEquals(asList(mixed, lower, upper), search(field.     equalIgnoreCase("lowerUPPER" )));
+		assertEquals(asList(mixed, lower, upper), search(field.      likeIgnoreCase("lowerUPPER%")));
+		assertEquals(asList(mixed, lower, upper), search(field.startsWithIgnoreCase("lowerUPPER" )));
+		assertEquals(asList(mixed, lower, upper), search(field.  endsWithIgnoreCase("lowerUPPER" )));
+		assertEquals(asList(mixed, lower, upper), search(field.  containsIgnoreCase("lowerUPPER" )));
 
-		assertEquals(asList(), search(any.     equalIgnoreCase("lowerUPPEX" )));
-		assertEquals(asList(), search(any.      likeIgnoreCase("lowerUPPEX%")));
-		assertEquals(asList(), search(any.startsWithIgnoreCase("lowerUPPEX" )));
-		assertEquals(asList(), search(any.  endsWithIgnoreCase("lowerUPPEX" )));
-		assertEquals(asList(), search(any.  containsIgnoreCase("lowerUPPEX" )));
+		assertEquals(asList(), search(field.     equalIgnoreCase("lowerUPPEX" )));
+		assertEquals(asList(), search(field.      likeIgnoreCase("lowerUPPEX%")));
+		assertEquals(asList(), search(field.startsWithIgnoreCase("lowerUPPEX" )));
+		assertEquals(asList(), search(field.  endsWithIgnoreCase("lowerUPPEX" )));
+		assertEquals(asList(), search(field.  containsIgnoreCase("lowerUPPEX" )));
 	}
 
 	@Test void testIgnoreCaseSZ()
 	{
-		final StringItem mixed = new StringItem("lower\u00dfUPPER", true);
-		final StringItem lower = new StringItem("lower\u00dfupper", true);
-		final StringItem upper = new StringItem("LOWER\u00dfUPPER", true);
-		new StringItem("lowerUPPER", true);
-		new StringItem(null, true);
-		assertEquals(asList(mixed, lower, upper), search(any.equalIgnoreCase("lower\u00dfUPPER")));
-		assertEquals(asList(mixed, lower, upper), search(any. likeIgnoreCase("lower\u00dfUPPER")));
+		final MyItem mixed = new MyItem("lower\u00dfUPPER");
+		final MyItem lower = new MyItem("lower\u00dfupper");
+		final MyItem upper = new MyItem("LOWER\u00dfUPPER");
+		new MyItem("lowerUPPER");
+		new MyItem((String)null);
+		assertEquals(asList(mixed, lower, upper), search(field.equalIgnoreCase("lower\u00dfUPPER")));
+		assertEquals(asList(mixed, lower, upper), search(field. likeIgnoreCase("lower\u00dfUPPER")));
 	}
 
-	private static List<StringItem> search(final Condition condition)
+	private static List<MyItem> search(final Condition condition)
 	{
-		final Query<StringItem> q = TYPE.newQuery(condition);
+		final Query<MyItem> q = TYPE.newQuery(condition);
 		q.setOrderByThis(true);
 		return q.search();
 	}
+
+	@WrapperType(indent=2, comments=false)
+	static final class MyItem extends Item
+	{
+		@WrapperInitial
+		@Wrapper(wrap=Wrapper.ALL_WRAPS, visibility=NONE)
+		static final StringField field = new StringField().optional().lengthMin(0);
+
+		@com.exedio.cope.instrument.Generated
+		@java.lang.SuppressWarnings({"RedundantSuppression","TypeParameterExtendsFinalClass","UnnecessarilyQualifiedInnerClassAccess"})
+		MyItem(
+					@javax.annotation.Nullable final java.lang.String field)
+				throws
+					com.exedio.cope.StringLengthViolationException
+		{
+			this(new com.exedio.cope.SetValue<?>[]{
+				com.exedio.cope.SetValue.map(MyItem.field,field),
+			});
+		}
+
+		@com.exedio.cope.instrument.Generated
+		private MyItem(final com.exedio.cope.SetValue<?>... setValues){super(setValues);}
+
+		@com.exedio.cope.instrument.Generated
+		private static final long serialVersionUID = 1l;
+
+		@com.exedio.cope.instrument.Generated
+		static final com.exedio.cope.Type<MyItem> TYPE = com.exedio.cope.TypesBound.newType(MyItem.class,MyItem::new);
+
+		@com.exedio.cope.instrument.Generated
+		private MyItem(final com.exedio.cope.ActivationParameters ap){super(ap);}
+	}
+
+	private static final Model MODEL = new Model(TYPE);
 }
