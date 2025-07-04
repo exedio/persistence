ef6a6a26ff292b92eeeb7d91d2ec3bf298e42233
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2025-07-04 17:43:31 +0200

    Function#isNotCasted replaces Cope#notEqualAndCast - prepare production

--- runtime/src/com/exedio/cope/Cope.java
+++ runtime/src/com/exedio/cope/Cope.java
@@ -129,10 +129,11 @@ public final class Cope
 	 * {@code value} to {@code X} before calling
 	 * {@link Function#isNot(Object)}
 	 * @throws ClassCastException if {@code value} is not assignable to {@code X}
+	 * To be deprecated Use {@link Function#isNotCasted(Object)} instead.
 	 */
 	public static <X> Condition notEqualAndCast(final Function<X> function, final Object value)
 	{
-		return function.isNot(function.getValueClass().cast(value));
+		return function.isNotCasted(value);
 	}
 
 	/**
