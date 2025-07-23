03e762fdafe2b650d1739b6c77bb7b7d5cd4a494
Frank Finger <frank.finger@exedio.com>
2025-07-23 10:33:47 +0200

    prepare test case for @Wrapper.wrap is String[]

---
+++ instrument/testparamssrc/com/exedio/cope/instrument/parameters/CombinableWrappersItem.java
@@ -0,0 +1,46 @@
+package com.exedio.cope.instrument.parameters;
+
+import static com.exedio.cope.instrument.Visibility.*;
+
+import com.exedio.cope.Item;
+import com.exedio.cope.StringField;
+import com.exedio.cope.instrument.Wrapper;
+import com.exedio.cope.instrument.WrapperType;
+
+@SuppressWarnings("UnnecessarilyQualifiedStaticUsage")
+@WrapperType(constructor=NONE, genericConstructor=NONE, comments=false)
+class CombinableWrappersItem extends Item
+{
+	@Wrapper(wrap="get", visibility=DEFAULT)
+	@Wrapper(wrap="set", visibility=DEFAULT)
+	public static final StringField string = new StringField();
+
+	@com.exedio.cope.instrument.GeneratedClass
+	@java.lang.SuppressWarnings("wrapper-single")
+	@javax.annotation.Nonnull
+	public final java.lang.String getString()
+	{
+		return CombinableWrappersItem.string.get(this);
+	}
+
+	@com.exedio.cope.instrument.GeneratedClass
+	@java.lang.SuppressWarnings("wrapper-single")
+	public final void setString(@javax.annotation.Nonnull final java.lang.String string)
+			throws
+				com.exedio.cope.MandatoryViolationException,
+				com.exedio.cope.StringLengthViolationException
+	{
+		CombinableWrappersItem.string.set(this,string);
+	}
+
+	@com.exedio.cope.instrument.GeneratedClass
+	@java.io.Serial
+	private static final long serialVersionUID = 1;
+
+	@com.exedio.cope.instrument.GeneratedClass
+	@java.lang.SuppressWarnings("type-single")
+	static final com.exedio.cope.Type<CombinableWrappersItem> TYPE = com.exedio.cope.TypesBound.newType(CombinableWrappersItem.class,CombinableWrappersItem::new);
+
+	@com.exedio.cope.instrument.GeneratedClass
+	protected CombinableWrappersItem(final com.exedio.cope.ActivationParameters ap){super(ap);}
+}
